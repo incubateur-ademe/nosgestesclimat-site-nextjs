a578a79ced38038cc1c649fb47661671535f6d1f
Benjamin Arias <benjamin.j.arias@gmail.com>
2023-08-24 12:52:39 +0200

    feat: Ajoute les pages guides

---
+++ src/app/(pages-statiques)/guide/layout.tsx
@@ -0,0 +1,11 @@
+import PageLayout from '@/components/layout/PageLayout'
+import Main from '@/design-system/layout/Main'
+import { PropsWithChildren } from 'react'
+
+export default function Layout({ children }: PropsWithChildren) {
+	return (
+		<PageLayout shouldShowMenu>
+			<Main className="max-w-[800px] p-8">{children}</Main>
+		</PageLayout>
+	)
+}
