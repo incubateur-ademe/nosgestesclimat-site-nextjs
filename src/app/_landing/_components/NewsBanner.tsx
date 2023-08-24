1ad4525628bb5829ffb0ca0e9e469ff6a24e985f
Benjamin Arias <benjamin.j.arias@gmail.com>
2023-08-24 18:11:22 +0200

    feat: Ajoute localstorage release

--- src/app/_landing/_components/NewsBanner.tsx
+++ src/app/_landing/_components/NewsBanner.tsx
@@ -7,6 +7,7 @@ import { getCurrentLangInfos } from '@/locales/translation'
 import { capitaliseString } from '@/utils/capitaliseString'
 import { sortReleases } from '@/utils/sortReleases'
 import Link from 'next/link'
+import { ReactNode, useEffect, useState } from 'react'
 
 export const localStorageKey = 'last-viewed-release'
 
@@ -15,31 +16,36 @@ export const determinant = (word: string) =>
   /^[aeiouy]/i.exec(word) ? 'd’' : 'de '
 
 export default function NewsBanner() {
+  const [lastViewedRelease, setLastViewedRelease] = useState(
+    localStorage.getItem(localStorageKey)
+  )
+
   const { t, i18n } = useClientTranslation()
   const currentLangInfos = getCurrentLangInfos(i18n)
 
   const releases = sortReleases(currentLangInfos.releases)
   const lastRelease = releases && releases[0]
 
-  /*
-	const [lastViewedRelease, setLastViewedRelease] = usePersistingState(
-		localStorageKey,
-		null
-	)
-	*/
+  console.log({ lastRelease, lastViewedRelease })
+
+  const handleUpdateViewedRelease = () => {
+    localStorage.setItem(localStorageKey, lastRelease.name)
+    setLastViewedRelease(lastRelease.name)
+  }
+
+  useEffect(() => {
+    if (!lastViewedRelease) {
+      localStorage.setItem(localStorageKey, lastRelease.name)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   if (!lastRelease) return null // Probably a problem fetching releases in the compilation step. It shouldn't happen, the build should fail, but just in case, this potential failure should not put the whole web site down for a side feature
 
   // We only want to show the banner to returning visitors, so we initiate the
   // local storage value with the last release.
-  /*
-	if (lastViewedRelease === undefined) {
-		setLastViewedRelease(lastRelease.name)
-		return null
-	}
-	*/
-  console.log('TODO : replace persisting state here')
-  const showBanner = lastRelease.name // && lastViewedRelease !== lastRelease.name
+  const shouldShowBanner =
+    lastRelease.name && lastViewedRelease !== lastRelease.name
 
   const date = new Date(lastRelease.published_at).toLocaleDateString(
     currentLangInfos.abrvLocale,
@@ -51,7 +57,9 @@ export default function NewsBanner() {
     }
   )
 
-  return showBanner ? (
+  if (!shouldShowBanner) return null
+
+  return (
     <Card className='relative min-w-[20rem] p-8 text-left'>
       <div>
         <h2 className='m-0 flex items-center'>
@@ -61,7 +69,7 @@ export default function NewsBanner() {
         <div>
           <small className='max-w-[12rem]'>
             <TransClient i18nKey={'components.NewsBanner.miseAJourDate'}>
-              Dernière mise à jour {{ date } as unknown as React.ReactNode}
+              Dernière mise à jour {{ date } as unknown as ReactNode}
             </TransClient>
           </small>
         </div>
@@ -70,12 +78,11 @@ export default function NewsBanner() {
         </div>
       </div>
       <button
-        // onClick={() => setLastViewedRelease(lastRelease.name)}
+        onClick={handleUpdateViewedRelease}
         className='absolute right-2 top-2 h-8 w-8 border-none bg-transparent p-0 text-lg text-primaryDark'
-        title={t('Fermer la notification de nouveautés')}
-      >
+        title={t('Fermer la notification de nouveautés')}>
         &times;
       </button>
     </Card>
-  ) : null
+  )
 }
