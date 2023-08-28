491b3f7bfc8b2fc8130a572ef8bb802e179ab881
Florian <hi@florianpanchout.com>
2023-08-28 14:50:01 +0200

    fix: stop rendering form every question

--- src/app/(simulation)/simulateur/[...dottedName]/_components/Form.tsx
+++ src/app/(simulation)/simulateur/[...dottedName]/_components/Form.tsx
@@ -1,16 +1,40 @@
 import { useForm } from '@/publicodes-state'
-import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
 import Navigation from './form/Navigation'
 import Question from './form/Question'
 
 export default function Form() {
-  const { currentQuestion } = useForm()
+  const { currentQuestion, setCurrentQuestion } = useForm()
   const router = useRouter()
-  useEffect(() => {
-    router.push('/simulateur/bilan/' + currentQuestion.split(' . ').join('/'))
-  }, [currentQuestion])
 
+  const searchParams = useSearchParams()
+  const questionInQueryParams = searchParams.get('question')
+
+  const [isInitialized, setIsInitialized] = useState(false)
+
+  useEffect(() => {
+    if (!isInitialized && questionInQueryParams) {
+      setCurrentQuestion(
+        decodeURI(
+          questionInQueryParams.replaceAll('.', ' . ').replaceAll('_', ' ')
+        )
+      )
+    }
+    setIsInitialized(true)
+  }, [questionInQueryParams])
+
+  useEffect(() => {
+    if (isInitialized) {
+      router.push(
+        '/simulateur/bilan?question=' +
+          currentQuestion.replaceAll(' . ', '.').replaceAll(' ', '_'),
+        { scroll: false }
+      )
+    }
+  }, [currentQuestion, isInitialized])
+
+  if (!isInitialized) return
   return (
     <div className="rounded-lg bg-primaryLight p-4 mb-4">
       <Question question={currentQuestion} key={currentQuestion} />
--- src/app/(simulation)/simulateur/[...dottedName]/page.tsx
+++ src/app/(simulation)/simulateur/[...dottedName]/page.tsx
@@ -1,16 +1,21 @@
 'use client'
+
 import Title from '@/design-system/layout/Title'
 import Charts from './_components/Charts'
 import Debug from './_components/Debug'
 import Form from './_components/Form'
 import Total from './_components/Total'
 
-export default function Simulateur() {
+export default function Simulateur({
+  params,
+}: {
+  params: { dottedName: string[] }
+}) {
   return (
     <>
       <Title title={'Votre bilan climat personnel'} />
       <Total />
-      <Form />
+      <Form params={params} />
       <Charts />
       <Debug />
     </>
