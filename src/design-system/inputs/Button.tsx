e8b9248635003a19fa8392df7700960c58d75482
Benjamin Arias <benjamin.j.arias@gmail.com>
2023-08-28 11:53:36 +0200

    fix: Style Button

--- src/design-system/inputs/Button.tsx
+++ src/design-system/inputs/Button.tsx
@@ -13,9 +13,9 @@ type Props = {
 
 export const colorClassNames = {
   primary:
-    'transition-colors text-white bg-primary shadow-sm hover:bg-primaryDark',
+    'transition-colors text-white bg-primary shadow-sm hover:text-white hover:bg-primaryDark',
   secondary:
-    'border-solid  border-primary text-primary shadow-sm bg-transparent hover:bg-primary hover:text-white',
+    'border-solid border-primary border-2 text-primary shadow-sm bg-transparent hover:bg-primaryLight hover:border-primaryDark',
   text: 'text-primary bg-transparent shadow-none hover:bg-primaryLight hover:text-primary hover:border-primary',
 }
 
