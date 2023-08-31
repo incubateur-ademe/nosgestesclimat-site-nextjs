edb04d2f9d760e2109f96258777687df0b2db579
Florian <hi@florianpanchout.com>
2023-08-31 12:40:05 +0200

    feat: again and again

--- src/app/(pages-statiques)/cgu/_components/Content.tsx
+++ src/app/(pages-statiques)/cgu/_components/Content.tsx
@@ -1,9 +1,5 @@
 'use client'
 
-import MDXContent from '@/components/mdx/MDXContent'
-import ContentEn from '@/locales/pages/en-us/CGU.mdx'
-import ContentFr from '@/locales/pages/fr/CGU.mdx'
-
 export default function Content() {
-	return <MDXContent contentFr={ContentFr} contentEn={ContentEn} />
+  return <div />
 }
