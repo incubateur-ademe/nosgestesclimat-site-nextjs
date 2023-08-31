5e63bf15ef7e01e6fc7290bd40158e1c8d6c26c6
Florian <hi@florianpanchout.com>
2023-08-31 16:27:46 +0200

    fix: unused

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,7 @@ export default async function RootLayout({ children }: PropsWithChildren) {
         }
       }) => res.country
     )
-
+  console.log(region)
   return (
     <html lang={lang ?? ''}>
       <head>
