e60e7a82cfef0c9401df9baad3411daf74ee5d8f
Florian <hi@florianpanchout.com>
2023-08-31 17:26:30 +0200

    fsgrzgrefqe

--- src/app/(simulation)/simulateur/[...dottedName]/page.tsx
+++ src/app/(simulation)/simulateur/[...dottedName]/page.tsx
@@ -1,11 +1,5 @@
 'use client'
 
-import Title from '@/design-system/layout/Title'
-
 export default function Simulateur() {
-  return (
-    <>
-      <Title title={'Votre bilan climat personnel'} />
-    </>
-  )
+  return <>Simulateur</>
 }
---
+++ src/app/(simulation)/simulateur/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import Title from '@/design-system/layout/Title'
+
+export default function Simulateur() {
+  return (
+    <>
+      <Title title={'Votre bilan climat personnel'} />
+    </>
+  )
+}
