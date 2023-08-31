b4e963dfb48b78660af64385aa8892c755b3a963
Florian <hi@florianpanchout.com>
2023-08-31 15:57:26 +0200

    Revert "feat: delete utils"
    This reverts commit cd4a3ddad67cfb510498b823c57f71946e640dfe.

--- src/design-system/utils/Markdown.tsx
+++ src/design-system/utils/Markdown.tsx
@@ -1,3 +1,32 @@
-export default function TransClient({ children }: any) {
-  return <div>{children}</div>
+'use client'
+
+import MarkdownToJsx, { MarkdownToJSX } from 'markdown-to-jsx'
+import Link from 'next/link'
+import { ComponentProps } from 'react'
+
+type MarkdownProps = ComponentProps<typeof MarkdownToJsx> & {
+  className?: string
+  components?: MarkdownToJSX.Overrides
+  renderers?: Record<string, unknown>
+}
+
+export default function Markdown({
+  children,
+  components = {},
+  ...otherProps
+}: MarkdownProps) {
+  return (
+    <MarkdownToJsx
+      {...otherProps}
+      options={{
+        ...otherProps.options,
+        forceBlock: true,
+        overrides: {
+          a: Link,
+          ...components,
+        },
+      }}>
+      {children}
+    </MarkdownToJsx>
+  )
 }
