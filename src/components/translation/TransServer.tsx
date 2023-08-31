06cb0058a3af9dc971a8f088c61930722190ea33
Florian <hi@florianpanchout.com>
2023-08-31 14:25:39 +0200

    feat: remove translations

--- src/components/translation/TransServer.tsx
+++ src/components/translation/TransServer.tsx
@@ -1,16 +1,3 @@
-import { getServerTranslation } from '@/helpers/getServerTranslation'
-import { PropsWithChildren } from 'react'
-import { Trans } from 'react-i18next/TransWithoutContext'
-
-export default async function TransServer({
-  children,
-  i18nKey,
-}: PropsWithChildren<{ i18nKey?: string }>) {
-  const { t } = await getServerTranslation()
-
-  return (
-    <Trans i18nKey={i18nKey} t={t}>
-      {children}
-    </Trans>
-  )
+export default function TransClient({ children }: any) {
+  return <div>{children}</div>
 }
