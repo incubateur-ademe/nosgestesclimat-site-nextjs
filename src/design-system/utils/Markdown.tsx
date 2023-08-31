cd4a3ddad67cfb510498b823c57f71946e640dfe
Florian <hi@florianpanchout.com>
2023-08-31 14:39:25 +0200

    feat: delete utils

--- src/design-system/utils/Markdown.tsx
+++ src/design-system/utils/Markdown.tsx
@@ -1,32 +1,3 @@
-'use client'
-
-import MarkdownToJsx, { MarkdownToJSX } from 'markdown-to-jsx'
-import Link from 'next/link'
-import { ComponentProps } from 'react'
-
-type MarkdownProps = ComponentProps<typeof MarkdownToJsx> & {
-  className?: string
-  components?: MarkdownToJSX.Overrides
-  renderers?: Record<string, unknown>
-}
-
-export default function Markdown({
-  children,
-  components = {},
-  ...otherProps
-}: MarkdownProps) {
-  return (
-    <MarkdownToJsx
-      {...otherProps}
-      options={{
-        ...otherProps.options,
-        forceBlock: true,
-        overrides: {
-          a: Link,
-          ...components,
-        },
-      }}>
-      {children}
-    </MarkdownToJsx>
-  )
+export default function TransClient({ children }: any) {
+  return <div>{children}</div>
 }
