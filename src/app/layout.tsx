e1c6b4176e6c3802505379d88fd5db490422373e
Florian <hi@florianpanchout.com>
2023-08-31 15:50:38 +0200

    Revert "feat: remove font ?"
    This reverts commit a41e2edfda3c404d7f8110045fb0e660a510a582.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,43 @@
 // Initialise react-i18next
 
+import localFont from 'next/font/local'
 import { PropsWithChildren } from 'react'
+
+const marianne = localFont({
+  src: [
+    {
+      path: '_fonts/Marianne-Thin.woff2',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '_fonts/Marianne-Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '_fonts/Marianne-Regular.woff2',
+      weight: 'normal',
+      style: 'normal',
+    },
+    {
+      path: '_fonts/Marianne-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '_fonts/Marianne-Bold.woff2',
+      weight: 'bold',
+      style: 'normal',
+    },
+    {
+      path: '_fonts/Marianne-ExtraBold.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-marianne',
+})
 
 export default async function RootLayout({ children }: PropsWithChildren) {
   return (
@@ -65,7 +102,7 @@ export default async function RootLayout({ children }: PropsWithChildren) {
         <meta name="theme-color" content="#5758BB" />
       </head>
 
-      <body>{children}</body>
+      <body className={marianne.className}>{children}</body>
     </html>
   )
 }
