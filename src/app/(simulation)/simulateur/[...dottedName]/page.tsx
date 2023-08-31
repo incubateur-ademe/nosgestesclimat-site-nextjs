0ffaa65ee77aad3d2daec8ec434f62d8275d86dd
Florian <hi@florianpanchout.com>
2023-08-31 17:23:20 +0200

    feat: trim simulation further

--- src/app/(simulation)/simulateur/[...dottedName]/page.tsx
+++ src/app/(simulation)/simulateur/[...dottedName]/page.tsx
@@ -1,13 +1,11 @@
 'use client'
 
 import Title from '@/design-system/layout/Title'
-import Total from './_components/Total'
 
 export default function Simulateur() {
   return (
     <>
       <Title title={'Votre bilan climat personnel'} />
-      <Total />
     </>
   )
 }
