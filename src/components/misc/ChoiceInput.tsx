576ee720d9032eb01eb23d400aeeedd7d560762a
Florian <hi@florianpanchout.com>
2023-09-04 13:59:56 +0200

    fix: markdown shouldn't be inside p

--- src/components/misc/ChoiceInput.tsx
+++ src/components/misc/ChoiceInput.tsx
@@ -38,9 +38,9 @@ export default function ChoiceInput({
         ) : null}
       </div>
       {description && isOpen ? (
-        <p className="mb-4 rounded-md bg-grey-100 p-2 w-1/2 text-sm">
-          <Markdown>{description}</Markdown>
-        </p>
+        <Markdown className="mb-4 rounded-md bg-grey-100 p-2 w-1/2 text-sm">
+          {description}
+        </Markdown>
       ) : null}
     </>
   )
