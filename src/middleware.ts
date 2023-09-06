d401cd069403b150e44c9f3565b3a0fb68b96c27
Benjamin Arias <benjamin.j.arias@gmail.com>
2023-09-06 14:07:20 +0200

    Revert "Delete middleware.ts"
    This reverts commit 217a0f7e8c5c92cfad6b39fb40d08db9e4d13dee.

---
+++ src/middleware.ts
@@ -0,0 +1,7 @@
+import { i18nRouter } from 'next-i18n-router'
+import { NextRequest } from 'next/server'
+import { i18nConfig } from './constants/i18n'
+
+export function middleware(request: NextRequest) {
+  return i18nRouter(request, i18nConfig)
+}
