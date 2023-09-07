d317d53c39f73b05627b116065d9259359d020d3
Florian <hi@florianpanchout.com>
2023-09-07 19:14:53 +0200

    feat: add button "Voir toutes les actions"

--- src/app/(layout-with-navigation)/(simulation)/fin/_components/Actions.tsx
+++ src/app/(layout-with-navigation)/(simulation)/fin/_components/Actions.tsx
@@ -1,4 +1,5 @@
 import TransClient from '@/components/translation/TransClient'
+import ButtonLink from '@/design-system/inputs/ButtonLink'
 import { useActions, useEngine } from '@/publicodes-state'
 import { useMemo } from 'react'
 import Action from './actions/Action'
@@ -31,10 +32,15 @@ export default function Actions() {
           Les 3 actions au plus fort impact pour vous&nbsp;:
         </TransClient>
       </p>
-      <div className="flex flex-col gap-8 ">
+      <div className="mb-8 flex flex-col gap-8">
         {filteredSameCategoriesActions.map((action, index) =>
           index < 3 ? <Action key={action} action={action} /> : null
         )}
+      </div>
+      <div className="flex justify-center">
+        <ButtonLink href="/actions" size="lg">
+          Voir toutes les actions
+        </ButtonLink>
       </div>
     </div>
   )
