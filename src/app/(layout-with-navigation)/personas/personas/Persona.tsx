4541f5b8a76310c71afe4d0a5bee6b5d942c1eb5
Florian <hi@florianpanchout.com>
2023-09-07 19:47:29 +0200

    feat: page personas

---
+++ src/app/(layout-with-navigation)/personas/personas/Persona.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import Button from '@/design-system/inputs/Button'
+import Card from '@/design-system/layout/Card'
+import { useUser } from '@/publicodes-state'
+
+type Props = {
+  persona: any
+  dottedName: string
+}
+
+export default function Persona({ persona, dottedName }: Props) {
+  const { initSimulation } = useUser()
+
+  return (
+    <Card>
+      <div className="flex flex-col items-center justify-between">
+        <div className="text-lg">{persona['icônes']}</div>
+        <h3>{persona.nom}</h3>
+        <p className="text-center text-sm">
+          {persona['résumé'] || persona['description']}
+        </p>
+        <Button
+          size="sm"
+          onClick={() =>
+            initSimulation({
+              situation: persona.situation,
+              persona: dottedName,
+            })
+          }>
+          Selectionner
+        </Button>
+      </div>
+    </Card>
+  )
+}
