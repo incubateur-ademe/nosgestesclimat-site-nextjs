0ab934dea479ec490f92c8a95f91161b4dcaed55
Emile Rolley <emile.rolley@tuta.io>
2023-09-11 12:47:41 +0200

    test(e2e): update the cypress config to the new cloud project

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 const testLangURL = process.env.CYPRESS_testLangURL ?? "en-US";
 
 export default defineConfig({
-  projectId: "dbxhpr",
+  projectId: "bkkrae",
   env: {
     // This is the URL of the local server that will be used for testing
     personas_fr_url: "https://data.nosgestesclimat.fr/personas-fr.json",
@@ -11,8 +11,11 @@ export default defineConfig({
     language_param: "fr",
   },
   e2e: {
-    baseUrl: `${process.env.CYPRESS_baseUrl ?? "http://localhost:3000"}/${testLangURL}`,
+    baseUrl: `${
+      process.env.CYPRESS_baseUrl ?? "http://localhost:3000"
+    }/${testLangURL}`,
     setupNodeEvents(on, config) {},
     experimentalRunAllSpecs: true,
+    specPattern: "cypress/e2e/integration/**/*.cy.js",
   },
 });
