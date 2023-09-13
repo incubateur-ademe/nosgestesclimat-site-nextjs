5a270d29c47be0b09a94fb8dbccdb18022c6bb91
Florian <hi@florianpanchout.com>
2023-09-13 08:10:02 +0200

    fix: hide mobile logo on desktop

--- src/app/(layout-with-navigation)/_components/MobileHeader.tsx
+++ src/app/(layout-with-navigation)/_components/MobileHeader.tsx
@@ -2,7 +2,7 @@ import Logo from '@/components/misc/Logo'
 
 export default function MobileHeader() {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center lg:hidden">
       <Logo size="xs" className="" />
     </div>
   )
