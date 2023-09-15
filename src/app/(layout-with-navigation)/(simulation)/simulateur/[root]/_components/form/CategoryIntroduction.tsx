f988c51f016a3b3cbba5d911c772ff5ed9b9115f
Florian <hi@florianpanchout.com>
2023-09-15 18:26:18 +0200

    feat: add correct icons to category introduction

--- src/app/(layout-with-navigation)/(simulation)/simulateur/[root]/_components/form/CategoryIntroduction.tsx
+++ src/app/(layout-with-navigation)/(simulation)/simulateur/[root]/_components/form/CategoryIntroduction.tsx
@@ -1,6 +1,7 @@
 import Trans from '@/components/translation/Trans'
 import Button from '@/design-system/inputs/Button'
 import { useRule } from '@/publicodes-state'
+import Image from 'next/image'
 
 type Props = {
   category: string
@@ -14,12 +15,17 @@ export default function CategoryIntroduction({
   const { title, icons, color } = useRule(category)
   return (
     <div
-      className="mb-4 flex flex-col items-center gap-8 rounded-lg p-8"
+      className="mb-4 flex flex-col items-center gap-4 rounded-lg p-8"
       style={{ backgroundColor: color }}>
       <h2 className="mb-0 text-center text-3xl text-white">{title}</h2>
-      <div className="flex h-20 w-20 items-center justify-center rounded-full bg-white text-6xl leading-none">
-        {icons}
-      </div>
+      <Image
+        style={{ filter: 'grayscale(1) invert(1) brightness(1.8)' }}
+        src={`/images/model/${category}.svg`}
+        alt={title || category}
+        width={32}
+        height={32}
+        className="flex h-24 w-24 items-center justify-center rounded-full  leading-none"
+      />
       <Button onClick={startCategory}>
         <Trans>Commencer</Trans>
       </Button>
