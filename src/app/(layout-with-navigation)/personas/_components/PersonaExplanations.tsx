d764ca3f00a3a5fb61c23ba6bc652261f99e1d6d
Benjamin Arias <benjamin.j.arias@gmail.com>
2023-09-21 14:41:02 +0200

    chore : cleaning

--- src/app/(layout-with-navigation)/personas/_components/PersonaExplanations.tsx
+++ src/app/(layout-with-navigation)/personas/_components/PersonaExplanations.tsx
@@ -9,14 +9,15 @@ export default function PersonaExplanations() {
             <Trans>Qui sont-ils ?</Trans>
           </h2>
         </summary>
+
         <div className="mt-4">
           <Trans i18nKey={'publicodes.Personas.description'}>
             Nous les avons définis pour qu’ils représentent la diversité des cas
-            d’usage du simulateur.{' '}
+            d’usage du simulateur.
             <i>
               Toute ressemblance avec une personne existant ou ayant existé
               serait purement fortuite !
-            </i>{' '}
+            </i>
             En aucune mesure, on ne peut dire qu’ils sont représentatifs de la
             distribution de la population française : il ne s’agit pas de coller
             aux statistiques de la population, mais de retrouver parmi nos dix
@@ -27,49 +28,60 @@ export default function PersonaExplanations() {
               <li>
                 Leur genre : même s’il n’influe pas sur l’empreinte, il serait
                 étonnant de n’avoir que des personas “femmes”
-              </li>{' '}
+              </li>
+
               <li>
                 Leur âge et situation personnelle / professionnelle : au moins
                 un étudiant, un retraité, un adulte au foyer, un actif
-              </li>{' '}
+              </li>
+
               <li>
                 La taille de leur foyer : de 1 personne à famille nombreuse
-              </li>{' '}
+              </li>
+
               <li>
                 Leur lieu de vie : de l’urbain, du rural et du péri-urbain, dans
                 le nord, dans le sud, les plaines, la montagne et sur une île
-              </li>{' '}
+              </li>
+
               <li>
                 Leur logement : de l’appartement à la maison, du neuf à l’ancien
               </li>
+
               <li>
                 Leurs modes de transport : de la marche à la voiture en passant
                 par le ferry et l’avion
-              </li>{' '}
+              </li>
+
               <li>
                 Leur régime alimentaire : au moins un végétalien, un végétarien,
                 une personne ne mangeant que du poisson, et un amateur de viande
                 rouge
-              </li>{' '}
+              </li>
+
               <li>
                 Leur tendance à l’achat : du tout occasion au tout neuf, de
                 l’acheteur compulsif à celui ou celle qui n’achète presque rien
-              </li>{' '}
+              </li>
+
               <li>
                 Leur façon de partir en vacances : mode de transport,
                 hébergement, on trouve de tout
-              </li>{' '}
+              </li>
+
               <li>Leurs loisirs : de la culture, du sport, du bien-être…</li>
             </ul>
           </Trans>
         </div>
       </details>
+
       <details>
         <summary>
           <h2 className="inline">
             <Trans>Comment les mettons-nous à jour ?</Trans>
           </h2>
         </summary>
+
         <div className="mt-4">
           <Trans i18nKey={'publicodes.Personas.maj'}>
             Pour qu’ils ou elles continuent de représenter la diversité des cas
@@ -79,7 +91,8 @@ export default function PersonaExplanations() {
             <ul className="list-disc pl-4">
               <li>
                 Chaque réponse possible est attribuée à au moins un persona
-              </li>{' '}
+              </li>
+
               <li>
                 Au moins un persona ne répond rien à la question (il lui est
                 donc attribué la valeur par défaut donnée dans le simulateur).
