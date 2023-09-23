b3b6185a3869029e8371e98026596fe621550242
Florian <hi@florianpanchout.com>
2023-09-23 09:55:52 +0200

    fix: stop infinite update

--- src/publicodes-state/formProvider/useCurrent.ts
+++ src/publicodes-state/formProvider/useCurrent.ts
@@ -1,18 +1,23 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 export default function useCurrent() {
   const [currentQuestion, setCurrentQuestion] = useState<null | string>(null)
 
   const [currentCategory, setCurrentCategory] = useState<null | string>(null)
 
-  return {
-    currentQuestion,
-    currentCategory,
-    setCurrentQuestion: (newCurrentQuestion: string | null) => {
+  const exportedSetCurrentQuestion = useCallback(
+    (newCurrentQuestion: string | null) => {
       setCurrentQuestion(newCurrentQuestion)
       if (newCurrentQuestion)
         setCurrentCategory(newCurrentQuestion.split(' . ')[0])
     },
+    [setCurrentQuestion, setCurrentCategory]
+  )
+
+  return {
+    currentQuestion,
+    currentCategory,
+    setCurrentQuestion: exportedSetCurrentQuestion,
     setCurrentCategory,
   }
 }
