97f5dcd9ca017e6d634a8c39e7ee4850779e56bb
Benjamin Arias <benjamin.j.arias@gmail.com>
2023-09-25 18:12:40 +0200

    fix: Style hauteur du contenu principal

--- src/design-system/layout/Main.tsx
+++ src/design-system/layout/Main.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { getIsIframe } from '@/utils/getIsIframe'
 import { PropsWithChildren } from 'react'
 
 export default function Main({
@@ -6,8 +9,14 @@ export default function Main({
   className,
 }: PropsWithChildren<{ maxWidth?: string; className?: string }>) {
   const maxWidthClass = maxWidth ? `max-w-${maxWidth} mx-auto` : ''
+
+  const isIframe = getIsIframe()
+
   return (
-    <main className={`flex flex-col ${maxWidthClass} ${className}`}>
+    <main
+      className={`flex flex-col ${maxWidthClass} ${className} ${
+        isIframe ? '' : 'min-h-[calc(100vh-2rem)]'
+      }`}>
       {children}
     </main>
   )
