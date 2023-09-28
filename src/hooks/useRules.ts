e8f50a4cc54360fa5331ab5ed6498bb4b9fac76f
Florian <hi@florianpanchout.com>
2023-09-28 08:39:26 +0200

    feat: add refetch in background

--- src/hooks/useRules.ts
+++ src/hooks/useRules.ts
@@ -26,11 +26,12 @@ export function useRules({ lang, region }: Props) {
         .then((res) => res.data as unknown),
     {
       keepPreviousData: true,
-      // When we work locally on the model we want the rules to be updated as much as possible (on window focus and every 3 seconds)
+      // When we work locally on the model we want the rules to be updated as much as possible (on window focus and every second)
       refetchOnWindowFocus: process.env.NEXT_PUBLIC_LOCAL_DATA_SERVER
         ? true
         : false,
-      refetchInterval: process.env.NEXT_PUBLIC_LOCAL_DATA_SERVER ? 3000 : false,
+      refetchInterval: process.env.NEXT_PUBLIC_LOCAL_DATA_SERVER ? 1000 : false,
+      refetchIntervalInBackground: true,
     }
   )
 }
