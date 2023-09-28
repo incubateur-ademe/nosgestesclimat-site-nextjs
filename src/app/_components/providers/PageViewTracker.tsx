7abc06b00903b84555348b33aaf9df446b41e8b8
Benjamin Arias <benjamin.j.arias@gmail.com>
2023-09-28 18:14:03 +0200

    fix: Double tracking pageviews

--- src/app/_components/providers/PageViewTracker.tsx
+++ src/app/_components/providers/PageViewTracker.tsx
@@ -1,14 +1,37 @@
 import { trackPageView } from '@/utils/matomo/trackEvent'
 import { usePathname, useSearchParams } from 'next/navigation'
-import { PropsWithChildren, useEffect } from 'react'
+import { PropsWithChildren, useEffect, useRef } from 'react'
 
 export default function PageViewTracker({ children }: PropsWithChildren) {
   const pathname = usePathname()
   const searchParams = useSearchParams()
+
+  const trackedPathnameAndSearchParamsRef = useRef({
+    pathname: '',
+    searchParams: '',
+  })
+
   useEffect(() => {
+    const searchParamsChecked = (
+      [...searchParams.keys()].length ? searchParams : ''
+    ) as string
+    if (
+      trackedPathnameAndSearchParamsRef.current.pathname === pathname &&
+      trackedPathnameAndSearchParamsRef.current.searchParams ===
+        searchParamsChecked
+    ) {
+      return
+    }
+
     const url = `${pathname}${
       [...searchParams.keys()].length ? '?' + searchParams : ''
     }`
+
+    trackedPathnameAndSearchParamsRef.current = {
+      pathname,
+      searchParams: searchParamsChecked,
+    }
+
     trackPageView(url)
   }, [pathname, searchParams])
 
