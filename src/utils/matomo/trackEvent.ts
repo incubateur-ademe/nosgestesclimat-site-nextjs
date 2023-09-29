c49a1da01d25fa7beb0bb4433d4f4c9de0f6d7b0
Florian <hi@florianpanchout.com>
2023-09-29 08:33:16 +0200

    feat: disable tracking in dev (local) mode

--- src/utils/matomo/trackEvent.ts
+++ src/utils/matomo/trackEvent.ts
@@ -1,3 +1,5 @@
+const shouldUseDevTracker = process.env.NODE_ENV === 'development'
+
 const groupExclusionRegexp = /\/(sondage|conférence)\//
 
 declare global {
@@ -9,6 +11,10 @@ declare global {
 export const trackEvent = (args: (string | null)[]) => {
   if (window.location.pathname.match(groupExclusionRegexp)) return
 
+  if (shouldUseDevTracker) {
+    console.debug(args.join(' => '))
+    return
+  }
   // Pass a copy of the array to avoid mutation
   window?._paq?.push([...args])
 }
@@ -16,12 +22,17 @@ export const trackEvent = (args: (string | null)[]) => {
 export const trackPageView = (url: string) => {
   if (window.location.pathname.match(groupExclusionRegexp)) return
 
+  if (shouldUseDevTracker) {
+    console.debug('trackPageView => ' + url)
+    return
+  }
+
   window?._paq.push(['setCustomUrl', url])
   window?._paq.push(['setDocumentTitle', document?.title])
 
   // remove all previously assigned custom variables, requires Matomo (formerly Piwik) 3.0.2
   window?._paq.push(['deleteCustomVariables', 'page'])
-  window?._paq.push(['setGenerationTimeMs', 0])
+  window?._paq.push(['setPagePerformanceTiming', 0])
 
   window?._paq.push(['trackPageView'])
 }
