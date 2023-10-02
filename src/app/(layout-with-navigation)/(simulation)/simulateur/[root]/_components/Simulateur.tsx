4605360e0520c2fa3967bf25182e662e5e83340e
Benjamin Arias <benjamin.j.arias@gmail.com>
2023-10-02 15:12:53 +0200

    chore: Move files

--- src/app/(layout-with-navigation)/(simulation)/simulateur/[root]/_components/Simulateur.tsx
+++ src/app/(layout-with-navigation)/(simulation)/simulateur/[root]/_components/Simulateur.tsx
@@ -3,9 +3,9 @@
 import Total from '@/components/total/Total'
 import { useDebug } from '@/hooks/useDebug'
 import { useState } from 'react'
-import Form from './Form'
-import Summary from './Summary'
 import Charts from './charts/Charts'
+import Form from './form/Form'
+import Summary from './summary/Summary'
 
 export default function Simulateur() {
   const [isQuestionListOpen, setIsQuestionListOpen] = useState(false)
