df060954891e61b42c391749fd8aebdc5be3d0a6
Emile Rolley <emile.rolley@tuta.io>
2023-10-06 11:08:47 +0200

    scripts(i18n): use the default i18next-parser config file name

--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -54,12 +54,12 @@ module.exports = {
   // Namespace separator used in your translation keys
   // If you want to use plain english keys, separators such as `.` and `:` will conflict. You might want to set `keySeparator: false` and `namespaceSeparator: false`. That way, `t('Status: Loading...')` will not think that there are a namespace and three separator dots for instance.
 
-  output: 'source/locales/static-analysis-$LOCALE.json',
+  output: 'src/locales/static-analysis-$LOCALE.json',
   // Supports $LOCALE and $NAMESPACE injection
   // Supports JSON (.json) and YAML (.yml) file formats
   // Where to write the locale files relative to process.cwd()
 
-  input: '/**/*.{jsx,tsx,js,ts}',
+  input: ['src/**/*.{jsx,tsx,js,ts}'],
   // An array of globs that describe where to look for source files
   // relative to the location of the configuration file
 
