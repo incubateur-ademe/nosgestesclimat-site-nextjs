792ed91097e00e447e98aea77547e819e8ce1917
Benjamin Arias <bjlaa@proton.me>
2023-10-16 11:51:26 +0200

    feat: Ajoute l'ErrorBoundary Sentry + ErrorFallback

---
+++ src/app/_components/ErrorFallback.tsx
@@ -0,0 +1,26 @@
+import Logo from '@/components/misc/Logo'
+import Trans from '@/components/translation/Trans'
+import Button from '@/design-system/inputs/Button'
+
+export const ErrorFallback = () => {
+  return (
+    <div className="text-center">
+      <Logo className="mb-4" size="sm" />
+      <h1>
+        <Trans>Une erreur s'est produite</Trans>
+      </h1>
+      <p className="mb-8">
+        <Trans>
+          Notre équipe a été notifiée, nous allons résoudre le problème au plus
+          vite.
+        </Trans>
+      </p>
+      <Button
+        onClick={() => {
+          window.location.reload()
+        }}>
+        <Trans>Recharger la page</Trans>
+      </Button>
+    </div>
+  )
+}
