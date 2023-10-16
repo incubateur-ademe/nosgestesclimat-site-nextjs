404bd17064edf34d942fccd60541f865e8c6e980
Benjamin Arias <bjlaa@proton.me>
2023-10-16 10:20:59 +0200

    Revert "[Hot fix]: Ajoute redirection manquante pour `/mon-empreinte-carbone` (#132)"
    This reverts commit f44a3a5ac3f26225fced5b68a67eb5ef80608e55.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -98,7 +98,9 @@ export default async function RootLayout({ children }: PropsWithChildren) {
             (function() {
               var u="https://matomo-incubateur-ademe.osc-fr1.scalingo.io/";
               _paq.push(['setTrackerUrl', u+'matomo.php']);
-              _paq.push(['setSiteId', ${process.env.NEXT_PUBLIC_MATOMO_ID}]);
+              _paq.push(['setSiteId', ${
+                process.env.NODE_ENV !== 'production' ? '2' : '1'
+              }]);
               var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
               g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
             })();
