27c966b098f83a9474661d361d549236c0100006
Florian <hi@florianpanchout.com>
2023-10-17 15:15:52 +0200

    fix: fix typo in matomo script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -101,7 +101,7 @@ export default async function RootLayout({ children }: PropsWithChildren) {
             (function() {
               var u="https://stats.data.gouv.fr/";
               _paq.push(['setTrackerUrl', u+'matomo.php']);
-              _paq.push(['setSiteId', '153');
+              _paq.push(['setSiteId', '153']);
               var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
               g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
             })();
