5a362afa145f67b0debc6bfd342456cd43771de3
Clement AUGER <clem.auger@hotmail.fr>
2023-10-19 15:11:38 +0200

    fix: disable FAQ link if iframeOnlySimulation param

--- src/app/(layout-with-navigation)/(simulation)/simulateur/[root]/_components/Faq.tsx
+++ src/app/(layout-with-navigation)/(simulation)/simulateur/[root]/_components/Faq.tsx
@@ -1,15 +1,23 @@
+'use client'
+
 import Link from '@/components/Link'
 import Trans from '@/components/translation/Trans'
 
+import { useIframe } from '@/hooks/useIframe'
+
 export default function Faq() {
+  const { iframeOnlySimulation } = useIframe()
+
   return (
-    <div className="mx-auto mb-4 rounded-lg bg-primaryLight p-4 text-center">
-      <p>
-        <Trans>Une question, un problème ?</Trans>
-      </p>
-      <Link href="/questions-frequentes">
-        <Trans>Découvrez la FAQ !</Trans>
-      </Link>
-    </div>
+    !iframeOnlySimulation && (
+      <div className="mx-auto mb-4 rounded-lg bg-primaryLight p-4 text-center">
+        <p>
+          <Trans>Une question, un problème ?</Trans>
+        </p>
+        <Link href="/questions-frequentes">
+          <Trans>Découvrez la FAQ !</Trans>
+        </Link>
+      </div>
+    )
   )
 }
