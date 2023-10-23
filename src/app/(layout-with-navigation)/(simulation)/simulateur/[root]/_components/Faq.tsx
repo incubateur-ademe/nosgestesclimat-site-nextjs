70bf6dad8de938cacd7cb911ad8c355a354db5af
Clement AUGER <clem.auger@hotmail.fr>
2023-10-23 10:22:06 +0200

    fix: early return for FAQ banner

--- src/app/(layout-with-navigation)/(simulation)/simulateur/[root]/_components/Faq.tsx
+++ src/app/(layout-with-navigation)/(simulation)/simulateur/[root]/_components/Faq.tsx
@@ -8,16 +8,16 @@ import { useIframe } from '@/hooks/useIframe'
 export default function Faq() {
   const { iframeOnlySimulation } = useIframe()
 
+  if (iframeOnlySimulation) return null
+
   return (
-    !iframeOnlySimulation && (
-      <div className="mx-auto mb-4 rounded-lg bg-primaryLight p-4 text-center">
-        <p>
-          <Trans>Une question, un problème ?</Trans>
-        </p>
-        <Link href="/questions-frequentes">
-          <Trans>Découvrez la FAQ !</Trans>
-        </Link>
-      </div>
-    )
+    <div className="mx-auto mb-4 rounded-lg bg-primaryLight p-4 text-center">
+      <p>
+        <Trans>Une question, un problème ?</Trans>
+      </p>
+      <Link href="/questions-frequentes">
+        <Trans>Découvrez la FAQ !</Trans>
+      </Link>
+    </div>
   )
 }
