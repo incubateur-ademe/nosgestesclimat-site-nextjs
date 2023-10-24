6f87595baf965211285d6ae50d65b8c12eb12a87
Florian <hi@florianpanchout.com>
2023-10-24 10:53:54 +0200

    chore: merge artefacts

--- src/app/(layout-with-navigation)/(simulation)/profil/_components/Localisation.tsx
+++ src/app/(layout-with-navigation)/(simulation)/profil/_components/Localisation.tsx
@@ -4,6 +4,7 @@ import CountryFlag from '@/components/misc/CountryFlag'
 import Trans from '@/components/translation/Trans'
 import Button from '@/design-system/inputs/Button'
 import { useClientTranslation } from '@/hooks/useClientTranslation'
+import { useIframe } from '@/hooks/useIframe'
 import { useLocale } from '@/hooks/useLocale'
 import { useUser } from '@/publicodes-state'
 import { SuppportedRegions } from '@/types/international'
@@ -29,6 +30,10 @@ export default function Localisation({
   const isRegionSupported = Object.keys(supportedRegions)?.some(
     (supportedRegion: string) => supportedRegion === region?.code
   )
+
+  const { iframeRegion } = useIframe()
+
+  if (iframeRegion) return null
 
   return (
     <div className="mb-8 mt-4 sm:mt-8">
