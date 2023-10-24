a81e143b7f3bb282ece9ab0b3f96d3a041bc101e
Emile Rolley <emile.rolley@tuta.io>
2023-10-24 10:53:10 +0200

    fix(t9n): use \u202f with t function instead of &nbsp; with Trans component

--- src/app/(layout-with-navigation)/(simulation)/fin/_components/Actions.tsx
+++ src/app/(layout-with-navigation)/(simulation)/fin/_components/Actions.tsx
@@ -1,10 +1,11 @@
-import Trans from '@/components/translation/Trans'
 import ButtonLink from '@/design-system/inputs/ButtonLink'
 import { useActions, useEngine } from '@/publicodes-state'
 import { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
 import Action from './actions/Action'
 
 export default function Actions() {
+  const { t } = useTranslation()
   const { orderedActions } = useActions()
   const { getCategory } = useEngine()
 
@@ -26,11 +27,11 @@ export default function Actions() {
     <div>
       <div id="shareImage">
         <h3 className="mb-2 text-center text-lg md:mb-4 md:text-xl">
-          <Trans>Comment réduire mon empreinte ?</Trans>
+          {t('Comment réduire mon empreinte\u202f?')}
         </h3>
 
         <p className="text-center text-sm italic md:text-base">
-          <Trans>Les 3 actions au plus fort impact pour vous :</Trans>
+          {t('Les 3 actions au plus fort impact pour vous\u202f:')}
         </p>
 
         <div className="mb-4 flex flex-col gap-4">
