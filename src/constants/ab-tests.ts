88106581a2140c2dde8fc1ff15547ea00a61b0d1
Benjamin Arias <bjlaa@proton.me>
2023-10-25 16:58:55 +0200

    clean: Retire tout ce qui a trait à l'AB Testing

--- src/constants/ab-tests.ts
+++
@@ -1,9 +0,0 @@
-export const AB_TESTS = [
-  {
-    name: 'fin',
-    stringToMatch: '/fin',
-    newPath: '/fin-v2',
-    oldPath: '/fin',
-    threshold: 0.5,
-  },
-]
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,8 @@
 import i18nConfig from '@/i18nConfig'
 import { i18nRouter } from 'next-i18n-router'
 import { NextRequest } from 'next/server'
-import { ABTest } from './helpers/ab-testing/ab-test'
 
 export function middleware(request: NextRequest) {
-  const ABTestingResponse = ABTest(request)
-
-  if (ABTestingResponse) {
-    return ABTestingResponse
-  }
-
   return i18nRouter(request, i18nConfig)
 }
 
