3fd07af0d89dee0497383be30f59d8adf2be5875
Florian <hi@florianpanchout.com>
2023-10-27 14:35:00 +0200

    fix: up failure treshold

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,7 @@
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command'
 
 addMatchImageSnapshotCommand({
-  failureThreshold: 0.0,
+  failureThreshold: 0.01,
   failureThresholdType: 'percent',
   customDiffConfig: { threshold: 0.0 },
   capture: 'viewport',
