d17f037571cd437bb7ad48e6bcaf5b08d51a8a74
Florian <hi@florianpanchout.com>
2023-10-30 07:29:08 +0100

    feat: up treshold and update references images

--- cypress/e2e/snapshots/static-pages.cy.js
+++ cypress/e2e/snapshots/static-pages.cy.js
@@ -4,13 +4,8 @@ describe('Visual regression tests of static pages', () => {
     cy.wait(1000)
     cy.matchImageSnapshot()
   })
-  it('Should match previous screenshot "Tutoriel"', () => {
-    cy.visit('/tutoriel')
-    cy.wait(1000)
-    cy.matchImageSnapshot()
-  })
-  it('Should match previous screenshot "À propos"', () => {
-    cy.visit('/a-propos')
+  it('Should match previous screenshot "Plan du site"', () => {
+    cy.visit('/plan-du-site')
     cy.wait(1000)
     cy.matchImageSnapshot()
   })
@@ -19,13 +14,8 @@ describe('Visual regression tests of static pages', () => {
     cy.wait(1000)
     cy.matchImageSnapshot()
   })
-  it('Should match previous screenshot "Amis"', () => {
-    cy.visit('/amis')
-    cy.wait(1000)
-    cy.matchImageSnapshot()
-  })
-  it('Should match previous screenshot "Nouveautes"', () => {
-    cy.visit('/nouveautes')
+  it('Should match previous screenshot "À propos"', () => {
+    cy.visit('/a-propos')
     cy.wait(1000)
     cy.matchImageSnapshot()
   })
@@ -34,18 +24,8 @@ describe('Visual regression tests of static pages', () => {
     cy.wait(1000)
     cy.matchImageSnapshot()
   })
-  it('Should match previous screenshot "Actions"', () => {
-    cy.visit('/actions')
-    cy.wait(1000)
-    cy.matchImageSnapshot()
-  })
   it('Should match previous screenshot "FAQ"', () => {
     cy.visit('/questions-frequentes')
-    cy.wait(1000)
-    cy.matchImageSnapshot()
-  })
-  it('Should match previous screenshot "Profil"', () => {
-    cy.visit('/profil')
     cy.wait(1000)
     cy.matchImageSnapshot()
   })
@@ -56,22 +36,32 @@ describe('Visual regression tests of static pages', () => {
   })
   it('Should match previous screenshot "Blog"', () => {
     cy.visit('/blog')
-    cy.wait(1000)
+    cy.wait(5000)
     cy.matchImageSnapshot()
   })
   it('Should match previous screenshot "Ambassadeurs"', () => {
     cy.visit('/ambassadeurs')
-    cy.wait(1000)
+    cy.wait(5000)
     cy.matchImageSnapshot()
   })
-  it('Should match previous screenshot "Plan du site"', () => {
-    cy.visit('/plan-du-site')
-    cy.wait(1000)
+  it('Should match previous screenshot "Nouveautes"', () => {
+    cy.visit('/nouveautes')
+    cy.wait(5000)
     cy.matchImageSnapshot()
   })
-  it('Should match previous screenshot "Accessibilite"', () => {
-    cy.visit('/accessibilite')
-    cy.wait(1000)
+  it('Should match previous screenshot "Amis"', () => {
+    cy.visit('/amis')
+    cy.wait(3000)
+    cy.matchImageSnapshot()
+  })
+  it('Should match previous screenshot "Actions"', () => {
+    cy.visit('/actions')
+    cy.wait(3000)
+    cy.matchImageSnapshot()
+  })
+  it('Should match previous screenshot "Tutoriel"', () => {
+    cy.visit('/tutoriel')
+    cy.wait(3000)
     cy.matchImageSnapshot()
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,9 @@
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command'
 
 addMatchImageSnapshotCommand({
-  failureThreshold: 0.01,
+  failureThreshold: 0.03,
   failureThresholdType: 'percent',
-  customDiffConfig: { threshold: 0.0 },
+  customDiffConfig: { threshold: 0.1 },
   capture: 'viewport',
 })
 
