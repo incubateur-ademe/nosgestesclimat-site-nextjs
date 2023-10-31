e559bc99a4ae5b523506dcf5d214a5cb0cc0c53e
Florian <hi@florianpanchout.com>
2023-10-31 11:31:05 +0100

    feat: disable i18n middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,10 @@
-import i18nConfig from '@/i18nConfig'
-import { i18nRouter } from 'next-i18n-router'
 import { middleware as withSplit } from 'next-with-split'
 import { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const resWithSplit = withSplit(request)
-
-  if (resWithSplit) return resWithSplit
-
-  return i18nRouter(request, i18nConfig)
+  const res = withSplit(request)
+  return res
+  // return i18nRouter(request, i18nConfig)
 }
 
 // only applies this middleware to files in the app directory
