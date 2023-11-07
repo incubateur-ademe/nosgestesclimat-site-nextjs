586bcd98821800a67a2171f51d52169a81f7ee29
Florian <hi@florianpanchout.com>
2023-11-07 14:04:17 +0100

    fix: stop testing snapshot and remove hydration errors

--- src/components/posts/List.tsx
+++ src/components/posts/List.tsx
@@ -7,7 +7,9 @@ type Props = {
 }
 export default function PostList({ items, path }: Props) {
   return (
-    <ul className="grid list-none grid-cols-1 justify-center gap-4 pl-0 sm:grid-cols-2">
+    <ul
+      className="grid list-none grid-cols-1 justify-center gap-4 pl-0 sm:grid-cols-2"
+      suppressHydrationWarning>
       {items
         .sort((itemA, itemB) =>
           new Date(itemA.data.date || '') > new Date(itemB.data.date || '')
