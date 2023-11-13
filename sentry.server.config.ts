42a5da08cf93775fb4f2e0f2890ac8b277ae0388
Clement AUGER <clem.auger@hotmail.fr>
2023-11-13 14:24:24 +0100

    ix: change NEXT_PUBLIC_LOCAL_DATA_SERVER to NEXT_PUBLIC_LOCAL_DATA again

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -13,6 +13,6 @@ Sentry.init({
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
-  // Disable sentry for development based on local data server
-  enabled: !process.env.NEXT_PUBLIC_LOCAL_DATA_SERVER,
+  // Disable sentry for development based on local data
+  enabled: !process.env.NEXT_PUBLIC_LOCAL_DATA,
 })
