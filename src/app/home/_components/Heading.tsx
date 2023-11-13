9194b1953c7eb87438c533a323083869ed26c7f2
Florian <hi@florianpanchout.com>
2023-11-13 22:28:16 +0100

    feat: add suspense for icons

--- src/app/home/_components/Heading.tsx
+++ src/app/home/_components/Heading.tsx
@@ -1,5 +1,6 @@
 import Link from '@/components/Link'
 import Trans from '@/components/translation/Trans'
+import { Suspense } from 'react'
 import ButtonStart from './heading/ButtonStart'
 import Icons from './heading/Icons'
 import Partners from './heading/Partners'
@@ -8,7 +9,9 @@ export default function Heading() {
   return (
     <>
       <div className="relative flex h-[36rem] items-center justify-center overflow-hidden bg-grey-100 p-4">
-        <Icons />
+        <Suspense fallback={<div />}>
+          <Icons />
+        </Suspense>
         <div className="relative max-w-sm text-center md:max-w-xl">
           <h1 className="font-medium md:text-5xl">
             <Trans>Connaissez-vous votre empreinte sur le climat&#8239;?</Trans>
