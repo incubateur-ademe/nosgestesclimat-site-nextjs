271a995d7ae481f1a427e8738d297915931f2229
Florian <hi@florianpanchout.com>
2023-11-14 17:55:10 +0100

    feat: add back debug and pr indicators

--- src/components/layout/header/headerDesktop/Navigation.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react'
-
-export default function Navigation({ children }: PropsWithChildren) {
-  return (
-    <nav className="h-full">
-      <ul className="flex h-full ">{children}</ul>
-    </nav>
-  )
-}
