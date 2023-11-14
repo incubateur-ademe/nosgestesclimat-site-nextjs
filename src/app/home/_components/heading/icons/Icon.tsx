c0ea77edaaf825e3aa2af664ab02ed31c2deef2f
Florian <hi@florianpanchout.com>
2023-11-14 07:36:53 +0100

    feat: add animation

--- src/app/home/_components/heading/icons/Icon.tsx
+++ src/app/home/_components/heading/icons/Icon.tsx
@@ -3,6 +3,7 @@
 import { everyIcons } from '@/components/icons'
 import { useIsClient } from '@/hooks/useIsClient'
 import { useWindowSize } from '@/hooks/useWindowSize'
+import { useEffect, useState } from 'react'
 
 type Props = {
   iconIndex: any
@@ -17,13 +18,22 @@ export default function Icon({ iconIndex, x, y, rotation, delay }: Props) {
   const { windowWidth } = useWindowSize()
   const IconComponent = everyIcons[iconIndex]
 
+  const [isHidden, setIsHidden] = useState(true)
+  useEffect(() => {
+    let timer: any = null
+    if (isClient) {
+      timer = setTimeout(() => setIsHidden(false), 500)
+    }
+    return () => timer && clearTimeout(timer)
+  })
   if (y < -300 || y > 300) return null
   if (Math.abs(x) > windowWidth / 2 + 32) return null
+  if (!isClient) return null
 
   return (
     <div
       className={`absolute transition-opacity delay-500 duration-500 ${
-        isClient ? 'opacity-100' : 'opacity-0'
+        isHidden ? 'opacity-0' : 'opacity-100'
       } motion-reduce:transition-none`}
       style={{
         transform: `translate(${x}px, ${y}px) rotate(${rotation}deg)`,
