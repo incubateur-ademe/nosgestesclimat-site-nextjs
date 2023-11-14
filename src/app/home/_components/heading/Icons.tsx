a19db24164b12748c87f808da80cd3ac7da87c31
Florian <hi@florianpanchout.com>
2023-11-14 07:21:07 +0100

    fix: remove unused var

--- src/app/home/_components/heading/Icons.tsx
+++ src/app/home/_components/heading/Icons.tsx
@@ -1,27 +1,30 @@
-import generateCircleOfSvg from '@/helpers/generateCircleOfSvg'
+// import generateCircleOfSvg from '@/helpers/generateCircleOfSvg'
+// // import Icon from './icons/Icon'
 
-const circles = [
-  generateCircleOfSvg({ numPieces: 30, radius: 450 }),
-  generateCircleOfSvg({ numPieces: 35, radius: 535 }),
-  generateCircleOfSvg({ numPieces: 40, radius: 620 }),
-  generateCircleOfSvg({ numPieces: 45, radius: 705 }),
-  generateCircleOfSvg({ numPieces: 50, radius: 790 }),
-]
+// // const circles = [
+// //   generateCircleOfSvg({ numPieces: 30, radius: 450 }),
+// //   generateCircleOfSvg({ numPieces: 35, radius: 535 }),
+// //   generateCircleOfSvg({ numPieces: 40, radius: 620 }),
+// //   generateCircleOfSvg({ numPieces: 45, radius: 705 }),
+// //   generateCircleOfSvg({ numPieces: 50, radius: 790 }),
+// // ]
 
 export default async function Icons() {
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
       {/* {circles.map((circle) =>
-        circle.map(({ iconIndex, x, y, rotation, delay }) => (
-          <Icon
-            key={String(x) + String(y)}
-            iconIndex={iconIndex}
-            x={x}
-            y={y}
-            rotation={rotation}
-            delay={delay}
-          />
-        ))
+        circle
+          .filter(({ y }) => y > -300 || y < 300)
+          .map(({ iconIndex, x, y, rotation, delay }) => (
+            <Icon
+              key={String(x) + String(y)}
+              iconIndex={iconIndex}
+              x={x}
+              y={y}
+              rotation={rotation}
+              delay={delay}
+            />
+          ))
       )} */}
     </div>
   )
