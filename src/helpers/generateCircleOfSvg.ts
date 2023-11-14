9d5f1b133fadb57f312e0b3d042a2e94f41bff9e
Florian <hi@florianpanchout.com>
2023-11-14 07:39:37 +0100

    chore: add some comments

--- src/helpers/generateCircleOfSvg.ts
+++ src/helpers/generateCircleOfSvg.ts
@@ -6,6 +6,7 @@ type Props = {
   centerX?: number
   centerY?: number
   maxDelay?: number
+  iconSize?: number
 }
 
 const generateCircleOfSvg = ({
@@ -14,24 +15,29 @@ const generateCircleOfSvg = ({
   centerX = 0,
   centerY = 0,
   maxDelay = 1000,
+  iconSize = 32,
 }: Props) => {
   const icons = []
   for (let i = 0; i < numPieces; i++) {
+    // Choose a random icon from the list
     const iconIndex = Math.floor(Math.random() * everyIcons.length)
 
     const angle = (i / numPieces) * 2 * Math.PI
-    const x = centerX + radius * Math.cos(angle) - 16
-    const y = centerY + radius * Math.sin(angle) - 16
-    let rotation = angle * (180 / Math.PI) // Convert angle to degrees
+    const x = centerX + radius * Math.cos(angle) - iconSize / 2
+    const y = centerY + radius * Math.sin(angle) - iconSize / 2
 
+    let rotation = angle * (180 / Math.PI) // Convert angle to degrees
     // If the piece is on the left side, add 180 degrees to the rotation
     if (x < centerX) {
       rotation += 180
     }
-    const delay = Math.random() * maxDelay
     // Add a random rotation between -10% and 10% of the initial rotation
     const randomRotation = rotation * (Math.random() * 0.2 - 0.1)
     rotation += randomRotation
+
+    // Add a random delay before displaying the icon
+    const delay = Math.random() * maxDelay
+
     icons.push({
       iconIndex,
       x,
