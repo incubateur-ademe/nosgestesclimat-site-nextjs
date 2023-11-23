4e9d710f3f1fb8d7989b6a8bdd6e9abe3c61a69d
Benjamin Arias <bjlaa@proton.me>
2023-11-23 11:03:34 +0100

    refacto: Affichage des titres de liens pour rétablir la traduction

--- src/app/(layout-with-navigation)/(simulation)/plan-du-site/page.tsx
+++ src/app/(layout-with-navigation)/(simulation)/plan-du-site/page.tsx
@@ -1,5 +1,6 @@
 import Trans from '@/components/translation/Trans'
 import Title from '@/design-system/layout/Title'
+import { getServerTranslation } from '@/helpers/getServerTranslation'
 import { getMetadataObject } from '@/helpers/metadata/getMetadataObject'
 import Actions from './_components/Actions'
 import LinkList from './_components/LinkList'
@@ -12,80 +13,85 @@ export async function generateMetadata() {
   })
 }
 
-const links = {
-  'Nos outils': {
-    bilan: {
-      title: 'Le test',
-      href: '/simulateur/bilan',
-    },
-    profil: {
-      title: 'Votre profil',
-      href: '/profil',
-    },
-    personas: {
-      title: 'Nos utilisateurs types',
-      href: '/personas',
-    },
-    actions: {
-      title: 'Nos actions pour réduire votre empreinte',
-      href: '/actions',
-    },
-    actionsPlus: {
-      title: 'Les actions phares',
-      href: '/actions-plus',
-    },
-  },
-  Informations: {
-    nouveautes: {
-      title: 'Nouveautés',
-      href: '/nouveautes',
-    },
-    aPropos: {
-      title: 'À propos',
-      href: '/a-propos',
-    },
-    contact: {
-      title: 'Contact',
-      href: '/contact',
-    },
-    viePrivee: {
-      title: 'Vie privée',
-      href: '/vie-privee',
-    },
-    partenaires: {
-      title: 'Partenaires',
-      href: '/partenaires',
-    },
-    faq: {
-      title: 'FAQ',
-      href: '/faq',
-    },
-    stats: {
-      title: 'Statistiques',
-      href: '/stats',
-    },
-    Blog: {
-      title: 'Blog',
-      href: '/blog',
-    },
-  },
-  Documentations: {
-    guide: {
-      title: 'Nos guides thématiques',
-      href: '/documentation/guide',
-    },
-    modele: {
-      title: 'Le modèle Nos Gestes Climat',
-      href: '/documentation/modele',
-    },
-    documentation: {
-      title: 'Documentation',
-      href: '/documentation',
-    },
-  },
-}
+export default async function PlanDuSitePage() {
+  const { t } = await getServerTranslation()
 
-export default function PlanDuSitePage() {
+  const links = {
+    'Nos outils': {
+      bilan: {
+        title: t('Le test', 'publicodes.planDuSite.bilan'),
+        href: '/simulateur/bilan',
+      },
+      profil: {
+        title: t('Votre profil', 'publicodes.planDuSite.profil'),
+        href: '/profil',
+      },
+      personas: {
+        title: t('Nos utilisateurs types', 'publicodes.planDuSite.personas'),
+        href: '/personas',
+      },
+      actions: {
+        title: t(
+          'Nos actions pour réduire votre empreinte',
+          'publicodes.planDuSite.actions'
+        ),
+        href: '/actions',
+      },
+      actionsPlus: {
+        title: t('Les actions phares', 'publicodes.planDuSite.actionsPlus'),
+        href: '/actions-plus',
+      },
+    },
+    Informations: {
+      nouveautes: {
+        title: t('Nouveautés', 'publicodes.planDuSite.nouveautes'),
+        href: '/nouveautes',
+      },
+      aPropos: {
+        title: t('À propos', 'publicodes.planDuSite.aPropos'),
+        href: '/a-propos',
+      },
+      contact: {
+        title: t('Contact', 'publicodes.planDuSite.contact'),
+        href: '/contact',
+      },
+      viePrivee: {
+        title: t('Vie privée', 'publicodes.planDuSite.viePrivee'),
+        href: '/vie-privee',
+      },
+      partenaires: {
+        title: t('Partenaires', 'publicodes.planDuSite.partenaires'),
+        href: '/partenaires',
+      },
+      faq: {
+        title: t('FAQ'),
+        href: '/faq',
+      },
+      stats: {
+        title: t('Statistiques', 'publicodes.planDuSite.stats'),
+        href: '/stats',
+      },
+      Blog: {
+        title: t('Blog', 'publicodes.planDuSite.blog'),
+        href: '/blog',
+      },
+    },
+    Documentations: {
+      guide: {
+        title: t('Nos guides thématiques', 'publicodes.planDuSite.guide'),
+        href: '/documentation/guide',
+      },
+      modele: {
+        title: t('Le modèle Nos Gestes Climat', 'publicodes.planDuSite.modele'),
+        href: '/documentation/modele',
+      },
+      documentation: {
+        title: t('Documentation', 'publicodes.planDuSite.documentation'),
+        href: '/documentation',
+      },
+    },
+  }
+
   return (
     <div data-cypress-id="plan-links">
       <Title
