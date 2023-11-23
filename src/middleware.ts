0f46a2e2762ffae7809635de02387905b777a89c
Benjamin Arias <bjlaa@proton.me>
2023-11-23 14:30:47 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,18 @@
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import i18nMiddleware from './middlewares/i18nMiddleware'
 import splitTestingMiddleware from './middlewares/splitTestingMiddleware'
 
 export const config = {
-  matchers: ['/((?!api|_next/image|favicon.ico|images).*)'],
+  matchers: ['/((?!api|_next/static|_next/image|favicon.ico|images).*)'],
 }
 
 export async function middleware(request: NextRequest) {
+  if (
+    request.nextUrl.pathname?.match('/((api|_next/image|favicon.ico|images).*)')
+  ) {
+    return NextResponse.next()
+  }
+
   // If we are on the main branch (production)
   if (!process.env.NEXT_PUBLIC_SPLIT_TESTING_BRANCH) {
     return i18nMiddleware(request)
