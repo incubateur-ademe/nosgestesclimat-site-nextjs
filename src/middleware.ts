eca8e4467b0155c12de77ed549e648d293dac886
Benjamin Arias <bjlaa@proton.me>
2023-11-23 13:11:45 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import i18nMiddleware from './middlewares/i18nMiddleware'
 import splitTestingMiddleware from './middlewares/splitTestingMiddleware'
 
 export const config = {
-  matchers: ['/((?!api|_next/static|_next/image|favicon.ico|images).*)'],
+  matchers: ['/((?!api|_next/image|favicon.ico|images).*)'],
 }
 
 export async function middleware(request: NextRequest) {
