067124e193836d802e35e959734bc37b48eae4a1
Clement AUGER <clem.auger@hotmail.fr>
2023-11-23 18:29:19 +0100

    TEMP fix: mosaic child need to includes keys

--- src/publicodes-state/helpers/getQuestionsOfMosaic.ts
+++ src/publicodes-state/helpers/getQuestionsOfMosaic.ts
@@ -7,9 +7,16 @@ export default function getQuestionsOfMosaic({
   dottedName,
   everyMosaicChildWhoIsReallyInMosaic,
 }: Props): string[] {
+  const tempKeyTable = {
+    'logement . vacances': 'présent',
+    'logement . vacances . résidence secondaire . durée': 'nombre',
+  } as Record<string, string>
   return (
-    everyMosaicChildWhoIsReallyInMosaic.filter((mosaicChild) =>
-      mosaicChild.includes(dottedName)
-    ) || []
+    everyMosaicChildWhoIsReallyInMosaic.filter((mosaicChild) => {
+      const conditionFromKey =
+        tempKeyTable[dottedName] &&
+        mosaicChild.includes(tempKeyTable[dottedName])
+      return mosaicChild.includes(dottedName) && conditionFromKey
+    }) || []
   )
 }
