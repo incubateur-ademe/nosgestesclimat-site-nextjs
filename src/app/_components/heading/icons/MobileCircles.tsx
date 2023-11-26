9cd0363808a47605ceeb260ad9f7e3dbd7dbd6f5
Florian <hi@florianpanchout.com>
2023-11-26 11:10:26 +0100

    fix: typo

--- src/app/_components/heading/icons/MobileCircles.tsx
+++ src/app/_components/heading/icons/MobileCircles.tsx
@@ -14,7 +14,7 @@ export default function MobileCircles({ circles }: Props) {
   return (
     <div className="absolute left-1/2 top-1/2 block -translate-x-1/2 -translate-y-1/2 md:hidden">
       {circles.map((circle) =>
-        circle.map(({ iconIndex, x, y, rotation, delay }) => (
+        circle.map(({ iconIndex, x, y, rotation }) => (
           <MobileIcon
             key={String(x) + String(y)}
             iconIndex={iconIndex}
