28334aed046cf6315cc2ae2b410b3012fc4786c1
Florian <hi@florianpanchout.com>
2023-11-26 11:10:15 +0100

    feat: pass mobile icons to server

--- src/app/_components/heading/icons/MobileCircles.tsx
+++ src/app/_components/heading/icons/MobileCircles.tsx
@@ -1,4 +1,4 @@
-import Icon from './desktopCircles/Icon'
+import MobileIcon from './mobileCircles/MobileIcon'
 
 type Props = {
   circles: {
@@ -15,13 +15,12 @@ export default function MobileCircles({ circles }: Props) {
     <div className="absolute left-1/2 top-1/2 block -translate-x-1/2 -translate-y-1/2 md:hidden">
       {circles.map((circle) =>
         circle.map(({ iconIndex, x, y, rotation, delay }) => (
-          <Icon
+          <MobileIcon
             key={String(x) + String(y)}
             iconIndex={iconIndex}
             x={x}
             y={y}
             rotation={rotation}
-            delay={delay}
             scale={0.75}
           />
         ))
