5d0a6e67e1b782efdd79e66eef476ef4b31f399f
Florian <hi@florianpanchout.com>
2023-11-28 11:52:33 +0100

    feat: use new og image

--- src/helpers/metadata/getMetadataObject.ts
+++ src/helpers/metadata/getMetadataObject.ts
@@ -88,7 +88,7 @@ export function getMetadataObject({
           url.includes(urlPart)
         ) && !noImage
           ? generateOGImageURL(url)
-          : 'https://nosgestesclimat.vercel.app/images/misc/dessin-nosgestesclimat_thumbnail.png',
+          : 'https://nosgestesclimat-git-154-nos-gestes-climat.vercel.app/images/misc/dessin-nosgestesclimat_thumbnail.png',
     },
     ...props,
   }
