ec4fc7f830a6b1ffbb1e1fc911ee4d51b44edba8
Florian <hi@florianpanchout.com>
2023-11-29 08:05:44 +0100

    fix: padding on group creation form

--- src/app/(wide-layout)/(simulation)/amis/creer/page.tsx
+++ src/app/(wide-layout)/(simulation)/amis/creer/page.tsx
@@ -18,7 +18,7 @@ export default async function CreerGroupePage() {
   const { t } = await getServerTranslation()
 
   return (
-    <div className="p-4 md:p-8">
+    <div className="md:p-8">
       <AutoCanonicalTag />
 
       <GoBackLink className="mb-4 font-bold" />
