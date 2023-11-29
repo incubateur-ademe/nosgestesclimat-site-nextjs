1a209472a0b9ae99c1bf7a19018dbffc50d356c2
Benjamin Arias <bjlaa@proton.me>
2023-11-29 13:54:10 +0100

    Update HowToAct.tsx

--- src/components/actions/HowToAct.tsx
+++ src/components/actions/HowToAct.tsx
@@ -8,13 +8,16 @@ export default function HowToAct() {
       <h2>
         <Trans>Comment agir ?</Trans>
       </h2>
+
       <p className="text-gray-600">
         <Trans>
           Découvre nos pistes pour agir dès aujourd’hui pour le climat, ou passe
           le test pour obtenir des recommandations personnalisées.
         </Trans>
       </p>
+
       <RecommendedActions />
+
       <div className="mt-4">
         <ButtonLink href="/actions" color="secondary">
           <Trans>Voir toutes les actions</Trans>
