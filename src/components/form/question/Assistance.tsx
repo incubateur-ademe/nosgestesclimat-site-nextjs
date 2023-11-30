a2d336f3af2f25d1448d8ee43bc8c6b616217e3e
Benjamin Arias <bjlaa@proton.me>
2023-11-30 12:13:42 +0100

    feat: Corrige le style page d'accueil et assistance mobile

--- src/components/form/question/Assistance.tsx
+++ src/components/form/question/Assistance.tsx
@@ -26,12 +26,13 @@ export default function Assistance({ question, assistance }: Props) {
   const [inputValue, setInputValue] = useState(0)
 
   return (
-    <div className="mb-2 w-2/3 rounded-lg bg-white p-4">
+    <div className="mb-4 w-[20rem] max-w-full rounded-lg bg-white p-4">
       <Label
         question={question}
         size="sm"
         label={label}
         description={description}
+        titleClassName="text-sm md:text-base"
       />
       {type === 'number' && (
         <NumberInput
