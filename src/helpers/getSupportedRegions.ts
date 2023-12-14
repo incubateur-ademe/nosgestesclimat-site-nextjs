11adb4476f54bfc0e14697d5f3ca128823ff0ca5
Clément <clem.auger@hotmail.fr>
2023-12-14 08:53:16 +0100

    fix: get supported regions from nightly

--- src/helpers/getSupportedRegions.ts
+++ src/helpers/getSupportedRegions.ts
@@ -1,11 +1,11 @@
 //TODO: should check if PR is in query params but it is kind of difficult to do in a server component
 
-import { DEFAULT_MODEL_VERSION } from '@/constants/modelAPI'
+import { NIGHTLY_MODEL_VERSION } from '@/constants/modelAPI'
 import { NGC_MODEL_API_URL } from '@/constants/urls'
 import { SuppportedRegions } from '@/types/international'
 
 export const getSupportedRegions = async (): Promise<SuppportedRegions> => {
-  return fetch(`${NGC_MODEL_API_URL}/${DEFAULT_MODEL_VERSION}`)
+  return fetch(`${NGC_MODEL_API_URL}/${NIGHTLY_MODEL_VERSION}`)
     .then((response) => {
       return response.json()
     })
