d72624c07a9f7ce6cf901e9023d92de77275c2a5
Benjamin Arias <bjlaa@proton.me>
2023-12-17 17:39:58 +0100

    feat: Fetch côté client

--- src/app/(layout-with-navigation)/(pages-statiques)/modele/_components/ModeleIssuePreviews.tsx
+++ src/app/(layout-with-navigation)/(pages-statiques)/modele/_components/ModeleIssuePreviews.tsx
@@ -1,7 +1,8 @@
 import Link from '@/components/Link'
-import Trans from '@/components/translation/Trans'
 import Card from '@/design-system/layout/Card'
 import Markdown from '@/design-system/utils/Markdown'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
 const labelString = ['🖼 exposé'].join(',')
 
@@ -12,44 +13,39 @@ type IssueType = {
   title: string
 }
 
-export default async function ModeleIssuePreviews() {
-  const issues: IssueType[] = await fetch(
-    `https://api.github.com/repos/incubateur-ademe/nosgestesclimat/issues?labels=${labelString}`
-  )
-    .then((res) => res.json())
-    .catch(() => {
-      return null
-    })
+export default function ModeleIssuePreviews() {
+  const { data: issues } = useQuery({
+    queryKey: ['modele-issue-previews'],
+    queryFn: () =>
+      axios
+        .get(
+          `https://api.github.com/repos/incubateur-ademe/nosgestesclimat/issues?labels=${labelString}`
+        )
+        .then((res) => res.data())
+        .catch(() => {
+          return null
+        }),
+  })
 
   if (!issues) return null
 
   return (
-    <>
-      <p>
-        <Trans i18nKey={'model.active'}>
-          Voici à titre d'exemple quelques <em>issues</em> importantes du moment
-          parmi la centaine de sujets documentés.
-        </Trans>
-      </p>
+    <ul className="grid list-none grid-cols-1 gap-4 md:grid-cols-2">
+      {(issues as IssueType[])?.map(({ body, id, html_url: url, title }) => (
+        <Card key={id}>
+          <h3>{title}</h3>
 
-      <ul className="grid list-none grid-cols-1 gap-4 md:grid-cols-2">
-        {issues?.map(({ body, id, html_url: url, title }) => (
-          <Card key={id}>
-            <h3>{title}</h3>
+          <div
+            className="h-[12rem] overflow-hidden"
+            style={{
+              WebkitMaskImage: 'linear-gradient(180deg, #000 60%, transparent)',
+            }}>
+            <Markdown>{body}</Markdown>
+          </div>
 
-            <div
-              className="h-[12rem] overflow-hidden"
-              style={{
-                WebkitMaskImage:
-                  'linear-gradient(180deg, #000 60%, transparent)',
-              }}>
-              <Markdown>{body}</Markdown>
-            </div>
-
-            <Link href={url}>En savoir plus</Link>
-          </Card>
-        ))}
-      </ul>
-    </>
+          <Link href={url}>En savoir plus</Link>
+        </Card>
+      ))}
+    </ul>
   )
 }
