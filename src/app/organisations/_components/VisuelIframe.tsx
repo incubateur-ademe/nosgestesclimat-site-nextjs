b3de7f02f4c9802cbb96e52704fbf55c7e53dc14
Benjamin Arias <bjlaa@proton.me>
2023-12-20 21:19:31 +0100

    chore: Supprime fichiers inutiles

--- src/app/organisations/_components/VisuelIframe.tsx
+++ src/app/organisations/_components/VisuelIframe.tsx
@@ -6,7 +6,7 @@ export default function VisuelIframe() {
       <div className="absolute -bottom-[8%] left-[-16%] flex w-full -rotate-[15deg] items-end rounded-t-md bg-white p-2 pb-0 shadow-md">
         <Image
           className="rounded-sm"
-          src="/images/organisations/orga-visuel-3-2.png"
+          src="/images/organisations/orga-visuel-3.png"
           width="300"
           height="200"
           alt=""
@@ -16,7 +16,7 @@ export default function VisuelIframe() {
       <div className="absolute -bottom-[8%] right-[-16%] flex w-full rotate-[15deg] items-end rounded-t-md bg-white p-2 pb-0 shadow-md">
         <Image
           className="rounded-sm"
-          src="/images/organisations/orga-visuel-3-2.png"
+          src="/images/organisations/orga-visuel-3.png"
           width="300"
           height="200"
           alt=""
@@ -26,7 +26,7 @@ export default function VisuelIframe() {
       <div className="absolute bottom-0 left-1/2 flex w-full -translate-x-1/2 items-end rounded-t-md bg-white p-2 pb-0 shadow-md">
         <Image
           className="rounded-sm"
-          src="/images/organisations/orga-visuel-3-2.png"
+          src="/images/organisations/orga-visuel-3.png"
           width="260"
           height="200"
           alt=""
