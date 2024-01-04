4bb1d25cff0bd12ac99898370f80ffafe8de6122
Clément <clem.auger@hotmail.fr>
2024-01-04 17:27:49 +0100

    feat: add migration script to clean situation

---
+++ src/publicodes-state/helpers/dottedNamesMigration.ts
@@ -0,0 +1,14 @@
+type migrationType = {
+  key: Record<string, string>
+  value: Record<string, Record<string, string>>
+}
+
+export const dottedNamesMigration: migrationType = {
+  key: {
+    'logement . chauffage . bois . type . bûche . consommation':
+      'logement . chauffage . bois . type . bûches . consommation',
+  },
+  value: {
+    'logement . chauffage . bois . type': { "'bûche'": 'bûches' },
+  },
+}
--- src/publicodes-state/helpers/safeGetSituation.ts
+++ src/publicodes-state/helpers/safeGetSituation.ts
@@ -1,4 +1,5 @@
-import { NodeValue, Situation } from '../types'
+import { Situation } from '../types'
+import { dottedNamesMigration } from './dottedNamesMigration'
 
 export const safeGetSituation = ({
   situation,
@@ -6,13 +7,47 @@ export const safeGetSituation = ({
 }: {
   situation: Situation
   everyRules: string[]
-}): any =>
-  everyRules
-    .filter((rule: string) => situation[rule] || situation[rule] === 0)
-    .reduce(
-      (accumulator: Record<string, NodeValue>, currentValue: string) => ({
-        ...accumulator,
-        [currentValue]: situation[currentValue],
-      }),
-      {}
-    )
+}): any => {
+  const unsupportedDottedNamesFromSituation = Object.keys(situation).filter(
+    (dottedName) => {
+      // We check if the dotteName is a rule of the model and migrate the rule if it's possible
+      if (!everyRules.includes(dottedName)) {
+        return true
+      }
+      // We check if the value of a dottedName waiting for an answer from "une possiblité"
+      // is defined as a rule `dottedName . value` in the model
+      // and migrate the rule if it's possible
+      if (
+        typeof situation[dottedName] === 'string' &&
+        situation[dottedName] !== 'oui' &&
+        situation[dottedName] !== 'non' &&
+        !everyRules.includes(`${dottedName} . ${situation[dottedName]}`)
+      ) {
+        return true
+      }
+    }
+  )
+
+  const filteredSituation = situation
+
+  unsupportedDottedNamesFromSituation.map((dottedName: string) => {
+    const situationValue = situation[dottedName] as string
+    if (Object.keys(dottedNamesMigration.key).includes(dottedName)) {
+      delete Object.assign(filteredSituation, {
+        [dottedNamesMigration.key[dottedName]]: situationValue,
+      })[dottedName]
+    } else if (
+      Object.keys(dottedNamesMigration.value).includes(dottedName) &&
+      Object.keys(dottedNamesMigration.value[dottedName]).includes(
+        situationValue
+      )
+    ) {
+      filteredSituation[dottedName] =
+        dottedNamesMigration.value[dottedName][situationValue]
+    } else {
+      delete filteredSituation[dottedName]
+    }
+  })
+
+  return filteredSituation
+}
