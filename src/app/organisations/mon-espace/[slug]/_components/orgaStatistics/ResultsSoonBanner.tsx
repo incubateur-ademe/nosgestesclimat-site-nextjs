3943aa39f935a251c0939f445624b3d165bfafbd
Benjamin Arias <bjlaa@proton.me>
2024-01-10 12:32:42 +0100

    feat: Completion du style de la bannière

--- src/app/organisations/mon-espace/[slug]/_components/orgaStatistics/ResultsSoonBanner.tsx
+++ src/app/organisations/mon-espace/[slug]/_components/orgaStatistics/ResultsSoonBanner.tsx
@@ -1,11 +1,10 @@
 import HourglassIcon from '@/components/icons/HourglassIcon'
 import Trans from '@/components/translation/Trans'
-import Button from '@/design-system/inputs/Button'
 import Card from '@/design-system/layout/Card'
 
 export default function ResultsSoonBanner() {
-  function handleClick() {
-    const faqElement = document.getElementById('orga-faq')
+  function handleScrollIntoView(id: string) {
+    const faqElement = document.getElementById(id)
 
     faqElement?.scrollIntoView({
       behavior: 'smooth',
@@ -14,22 +13,30 @@ export default function ResultsSoonBanner() {
   }
 
   return (
-    <Card className="w-full flex-row gap-4">
-      <div className="flex gap-4">
+    <Card className="w-full flex-row flex-wrap items-center justify-between gap-4 p-8 md:flex-nowrap">
+      <div className="flex max-w-2xl gap-4">
         <HourglassIcon />
-        <p>
-          <Trans>
-            Bientôt, vous verrez apparaître ici vos statistiques. Partagez le
-            test pour obtenir vos premiers résultats
-          </Trans>{' '}
-          -{' '}
-          <Button color="text" className="underline" onClick={handleClick}>
-            <Trans>En savoir plus</Trans>
-          </Button>
-        </p>
+        <div>
+          <p className="mb-0">
+            <Trans>
+              Bientôt, vous verrez apparaître ici vos statistiques. Partagez le
+              test pour obtenir vos premiers résultats
+            </Trans>{' '}
+            -{' '}
+            <button
+              className="text-primary-500 underline"
+              onClick={() => handleScrollIntoView('orga-faq')}>
+              <Trans>En savoir plus</Trans>
+            </button>
+          </p>
+        </div>
       </div>
 
-      <Button color="text">Partagez le test</Button>
+      <button
+        className="whitespace-nowrap font-bold text-primary-500 underline"
+        onClick={() => handleScrollIntoView('orga-partage')}>
+        Partagez le test
+      </button>
     </Card>
   )
 }
