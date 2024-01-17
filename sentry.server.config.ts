f5a2979bcb25194ac850d47c51cc5983aa3f42a0
Florian <hi@florianpanchout.com>
2024-01-17 18:06:48 +0100

    fix: disable Sentry performance monitoring

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -7,9 +7,6 @@ import * as Sentry from '@sentry/nextjs'
 Sentry.init({
   dsn: 'https://75dcf9dfe74c4439977a517be2805122@sentry.incubateur.net/118',
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 0.05,
-
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
