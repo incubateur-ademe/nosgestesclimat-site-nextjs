504be32fb3893db559146573efa925030d88f032
Florian <hi@florianpanchout.com>
2024-01-17 10:18:05 +0100

    feat: use ip instead of cookie

--- src/middlewares/splitTestingMiddleware.ts
+++ src/middlewares/splitTestingMiddleware.ts
@@ -1,4 +1,3 @@
-import { splitTestingCookieName } from '@/constants/split-testing'
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
@@ -9,46 +8,20 @@ export default function splitTestingMiddleware(request: NextRequest) {
     return NextResponse.next()
   }
 
-  const host = request.headers.get('host')
-  const referrer = request.headers.get('referer')
+  const ip = request.ip
 
-  let splitNumber = getSplitCookieFromRequest(request)
-
-  console.log('splitTestingMiddleware: splitNumber', splitNumber)
-
-  // If the host and referrer are differents, we may be inside an iframe
-  if ((!host || !referrer || !referrer.includes(host)) && !splitNumber) {
-    console.log(
-      'splitTestingMiddleware: inside an iframe ------------------------------------------------'
-    )
+  if (!ip || !isIPv4(ip)) {
     return NextResponse.next()
   }
 
-  // If no cookie is set and we are allready fetching files, we may be inside an iframe
-  const regex =
-    /\.(?:png|jpg|jpeg|gif|bmp|svg|webp|woff|woff2|js|css|webmanifest|well-known)/i
-  if (!splitNumber && regex.test(request.nextUrl.href)) {
-    console.log(
-      'splitTestingMiddleware: inside an iframe (regex) ------------------------------ '
-    )
-    return NextResponse.next()
-  }
-
-  // If no split cookie is set, we generate a random number
-  if (!splitNumber) {
-    console.log('generate random number')
-    const randomNumber = Math.random()
-    splitNumber = String(randomNumber)
-  }
+  const lastNumber = Number(ip.split('.').pop())
 
   const shouldRedirectToChallenger =
-    Number(splitNumber) <
+    Number(lastNumber / 255) <
     Number(process.env.NEXT_PUBLIC_SPLIT_TESTING_PERCENTAGE ?? 0.5)
 
   if (!shouldRedirectToChallenger || redirectUrl === request.nextUrl.origin) {
-    console.log('splitTestingMiddleware: redirect to challenger')
     const response = NextResponse.next()
-    response.cookies.set(splitTestingCookieName, splitNumber)
     return response
   } else {
     console.log('splitTestingMiddleware: redirect to control')
@@ -57,18 +30,11 @@ export default function splitTestingMiddleware(request: NextRequest) {
       ''
     )}`
     const response = NextResponse.rewrite(rewriteTo)
-    response.cookies.set(splitTestingCookieName, splitNumber)
     return response
   }
 }
 
-function getSplitCookieFromRequest(request: NextRequest) {
-  const cookieString = request.headers
-    .get('cookie')
-    ?.split('; ')
-    .find((cookieString) => cookieString.includes(splitTestingCookieName))
-
-  if (!cookieString) return null
-
-  return cookieString.replace(splitTestingCookieName, '').replace('=', '')
+function isIPv4(ip: string) {
+  const ipv4Pattern = /^(\d{1,3}\.){3}\d{1,3}$/
+  return ipv4Pattern.test(ip)
 }
