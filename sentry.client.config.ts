d8246d8fb85489627b0814116a1d246fd7a12ff7
Florian <hi@florianpanchout.com>
2024-01-18 08:05:28 +0100

    feat: explicitly disable tracing

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -7,6 +7,8 @@ import * as Sentry from '@sentry/nextjs'
 Sentry.init({
   dsn: 'https://75dcf9dfe74c4439977a517be2805122@sentry.incubateur.net/118',
 
+  enableTracing: false,
+
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
