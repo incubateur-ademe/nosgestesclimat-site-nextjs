8047a83911e1c2bea4d79cab37984d13b5c03acf
Florian <hi@florianpanchout.com>
2024-03-07 14:15:23 +0100

    chore: remove log

--- src/publicodes-state/helpers/migration/convertSimulation.ts
+++ src/publicodes-state/helpers/migration/convertSimulation.ts
@@ -8,7 +8,6 @@ type Return = Simulation
 export function convertSimulation({ simulation }: Props): Return {
   // If the value inside the situation is an object {valeur: value}, we want to convert it to value
   Object.keys(simulation.situation).map((key) => {
-    console.log(key)
     if (simulation.situation[key]?.valeur !== undefined) {
       simulation.situation[key] = simulation.situation[key].valeur
     }
