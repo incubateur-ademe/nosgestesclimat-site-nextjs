93e62b21176c7e5a5a00ece1169223d6e8277f20
Benjamin Arias <bjlaa@proton.me>
2024-03-12 18:00:13 +0100

    fake commit

--- src/app/(layout-with-navigation)/(simulation)/infos/codepostal/page.tsx
+++ src/app/(layout-with-navigation)/(simulation)/infos/codepostal/page.tsx
@@ -35,12 +35,14 @@ export default function PostalCode() {
         className="text-lg md:text-2xl"
         title={<Trans>Votre code postal</Trans>}
         subtitle={
-          <span className="text-secondary-500 font-bold italic">
+          <span className="font-bold italic text-secondary-500">
             <Trans>Facultatif</Trans>
           </span>
         }
       />
+
       <PostalCodeInput postalCode={postalCode} setPostalCode={setPostalCode} />
+
       <Navigation
         linkToPrev={getLinkToPrevInfosPage({ curPage: POSTAL_CODE_PAGE })}
         submitDisabled={!getLinkToNextInfosPage({ curPage: POSTAL_CODE_PAGE })}
