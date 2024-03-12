8f53665ed21a97eb8e1e3a36897122618e118250
Benjamin Arias <bjlaa@proton.me>
2024-03-12 08:48:54 +0100

    fix: Utilise mieux formatCarbonFootprint

--- src/app/(layout-with-navigation)/(simulation)/organisations/[slug]/resultats-detailles/_components/orgaStatisticsCharts/repartitionChart/BarItem.tsx
+++ src/app/(layout-with-navigation)/(simulation)/organisations/[slug]/resultats-detailles/_components/orgaStatisticsCharts/repartitionChart/BarItem.tsx
@@ -18,19 +18,21 @@ export default function BarItem({
 }: Props) {
   const { t } = useClientTranslation()
 
+  const { formattedValue } = formatCarbonFootprint(value)
+
   return (
     <div
       data-tooltip-id={id}
       data-tooltip-content={t('{{value}} tonnes', {
-        value: formatCarbonFootprint(value)?.formattedValue,
+        value: formattedValue,
       })}
       className={`absolute h-8 w-2 bg-primary-500 opacity-20 hover:scale-[10%] ${
         shouldBeHighlighted
-          ? '!bg-secondary-500 !z-10 border-x border-white !opacity-100'
+          ? '!z-10 border-x border-white !bg-secondary-500 !opacity-100'
           : ''
       }`}
       style={{
-        left: `${(value / maxValue) * 100}%`,
+        left: `${(parseFloat(formattedValue as string) / maxValue) * 100}%`,
         top: '50%',
         transform: 'translate(-50%, -50%)',
       }}
