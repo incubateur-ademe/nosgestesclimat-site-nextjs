dfa58af0b4f26065ee8cef146ebac7c7a3dbc66f
Florian <hi@florianpanchout.com>
2024-03-14 15:49:38 +0100

    feat: stop sending groupIds to backend

--- src/hooks/groups/useFetchGroupsOfUser.ts
+++ src/hooks/groups/useFetchGroupsOfUser.ts
@@ -2,31 +2,20 @@ import { GROUP_URL } from '@/constants/urls'
 import { useUser } from '@/publicodes-state'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
-import { useMemo } from 'react'
 
 export function useFetchGroupsOfUser() {
-  const { user, simulations } = useUser()
-
-  const groupIds = useMemo(
-    () =>
-      simulations
-        .filter((simulation) => simulation.group)
-        .map((simulation) => simulation.group),
-    [simulations]
-  )
+  const { user } = useUser()
 
   return useQuery({
-    queryKey: ['groups', user.userId, groupIds],
+    queryKey: ['groups', user.userId],
     queryFn: () =>
       axios
         .post(`${GROUP_URL}/fetch-groups`, {
           userId: user.userId,
-          groupIds,
         })
         .then((response) => {
           return response.data
         }),
-    enabled: groupIds.length > 0,
     initialData: [],
   })
 }
