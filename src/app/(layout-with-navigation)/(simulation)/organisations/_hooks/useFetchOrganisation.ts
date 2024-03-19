7af8544fd8ae73d184986c3fc66ee705f1e98c01
Benjamin Arias <bjlaa@proton.me>
2024-03-19 11:06:58 +0100

    fix: Modifie la condition d'affichage élément fil d'ariane

--- src/app/(layout-with-navigation)/(simulation)/organisations/_hooks/useFetchOrganisation.ts
+++ src/app/(layout-with-navigation)/(simulation)/organisations/_hooks/useFetchOrganisation.ts
@@ -1,10 +1,18 @@
 import { SERVER_URL } from '@/constants/urls'
-import { useQuery } from '@tanstack/react-query'
+import { Organisation } from '@/types/organisations'
+import { UseQueryResult, useQuery } from '@tanstack/react-query'
 import axios from 'axios'
+import { useParams } from 'next/navigation'
 
-export default function useFetchOrganisation({ email }: { email: string }) {
+export default function useFetchOrganisation({
+  email,
+}: {
+  email: string
+}): UseQueryResult<Organisation> {
+  const { slug } = useParams()
+
   return useQuery({
-    queryKey: ['organisation-validate-jwt', email],
+    queryKey: ['organisation-validate-jwt', email, slug],
     queryFn: () =>
       axios
         .post(
