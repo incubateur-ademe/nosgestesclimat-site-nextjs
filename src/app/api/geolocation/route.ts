8c6435cd17ee50a239e4b8bc41b575aaa7835614
Clément <clem.auger@hotmail.fr>
2024-04-03 09:55:20 +0200

    feat: add ue filter in route directly

--- src/app/api/geolocation/route.ts
+++ src/app/api/geolocation/route.ts
@@ -1,12 +1,33 @@
+import { getSupportedRegions } from '@/helpers/modelFetching/getSupportedRegions'
 import { NextRequest, NextResponse } from 'next/server'
 import countries from './countries.json'
+import ue_country_codes from './ue_country_codes.json'
 
 export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
+  const detectedCountryCode = request.geo?.country
+
+  if (!detectedCountryCode) {
+    return undefined
+  }
+
+  if (await isNotSupportedUECountry(detectedCountryCode)) {
+    return NextResponse.json({ name: 'Europe', code: 'EU' })
+  }
+
   const country = countries.find(
-    (country) => country.code === request.geo?.country
+    (country) => country.code === detectedCountryCode
   )
 
   return NextResponse.json({ country })
+}
+
+async function isNotSupportedUECountry(countryCode: string): Promise<boolean> {
+  const supportedRegions = await getSupportedRegions()
+
+  const nonSupportedUECountryCodes = ue_country_codes.filter(
+    (code) => !supportedRegions[code]
+  )
+  return nonSupportedUECountryCodes.includes(countryCode)
 }
