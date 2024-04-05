709c3ee99616db901b72371f3d57553bb81ece44
Benjamin Arias <bjlaa@proton.me>
2024-04-05 12:32:27 +0200

    feat: manquants

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,18 @@
 import Route404 from '@/components/layout/404'
-import { t } from '@/helpers/metadata/fakeMetadataT'
-import { Metadata } from 'next'
+import { getServerTranslation } from '@/helpers/getServerTranslation'
 
-export const metadata: Metadata = {
-  title: t('404 - Nos Gestes Climat'),
-  description: t(
-    "Oups, vous êtes bien sur Nos Gestes Climat, mais cette page n'existe pas."
-  ),
-  alternates: {
-    canonical: '/404',
-  },
+export async function generateMetadata() {
+  const { t } = await getServerTranslation()
+
+  return {
+    title: t('404 - Nos Gestes Climat'),
+    description: t(
+      "Oups, vous êtes bien sur Nos Gestes Climat, mais cette page n'existe pas."
+    ),
+    alternates: {
+      canonical: '/404',
+    },
+  }
 }
 
 export default function NotFound() {
