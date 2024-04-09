a1cd4633214e9de96447fda2ee26c490811d4131
Benjamin Arias <bjlaa@proton.me>
2024-04-09 14:57:30 +0200

    Update ActionsIcon.tsx

--- src/components/icons/ActionsIcon.tsx
+++ src/components/icons/ActionsIcon.tsx
@@ -4,9 +4,9 @@ export default function ActionsIcon({ className }: { className?: string }) {
   return (
     <svg
       className={twMerge('inline-block fill-default', className)}
-      width="22"
-      height="22"
-      viewBox="0 0 22 22"
+      width="18"
+      height="18"
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg">
       <path
