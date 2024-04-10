8713ee82d3bffeb6d9846fd815e8d647e0af6ee0
Clément <55186402+Clemog@users.noreply.github.com>
2024-04-10 14:19:27 +0200

    Release 1.4.4
    Changelog: 
    - Fix datashare referrer #488
    - [NEW] Support UE region (NGC-755, NGC-709) #487
    - Parcours Orga / Paramètres d’URL : pré remplissage email + réinit simu [NGC-708] #486

--- src/app/(layout-with-navigation)/(simulation)/infos/email/page.tsx
+++ src/app/(layout-with-navigation)/(simulation)/infos/email/page.tsx
@@ -10,11 +10,14 @@ import { useOrganisationQueryParams } from '@/hooks/organisations/useOrganisatio
 import { useClientTranslation } from '@/hooks/useClientTranslation'
 import { useUser } from '@/publicodes-state'
 import { isEmailValid } from '@/utils/isEmailValid'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { FormEvent, useCallback, useState } from 'react'
 import Navigation from '../_components/Navigation'
 
 export default function Email() {
+  const searchParams = useSearchParams()
+  const fixedEmail = searchParams.get('fixedemail') ? true : false
+
   const { user, updateEmail } = useUser()
 
   const [email, setEmail] = useState(
@@ -80,9 +83,11 @@ export default function Email() {
             <Trans>
               Pour conserver vos résultats et les retrouver à l’avenir
             </Trans>
-            <span className="ml-2 inline-block font-bold italic text-secondary-500">
-              <Trans>facultatif</Trans>
-            </span>
+            {!fixedEmail ? (
+              <span className="ml-2 inline-block font-bold italic text-secondary-500">
+                <Trans>facultatif</Trans>
+              </span>
+            ) : null}
           </>
         }
       />
@@ -91,6 +96,7 @@ export default function Email() {
         setEmail={setEmail}
         error={error}
         setError={setError}
+        readOnly={fixedEmail}
       />
       <Navigation
         linkToPrev={getLinkToPrevInfosPage({ curPage: EMAIL_PAGE })}
--- src/publicodes-state/hooks/useUser/useUserDetails.ts
+++ src/publicodes-state/hooks/useUser/useUserDetails.ts
@@ -1,43 +1,59 @@
-import { Dispatch, SetStateAction } from 'react'
-import { User, UserOrganisationInfo } from '../../types'
+import { Dispatch, SetStateAction, useCallback } from 'react'
+import { RegionFromGeolocation, User, UserOrganisationInfo } from '../../types'
 
 type Props = {
   setUser: Dispatch<SetStateAction<User>>
 }
 export default function useUserDetails({ setUser }: Props) {
-  const updateName = (name: string) =>
-    setUser((prevUser: User) => ({ ...prevUser, name }))
+  const updateName = useCallback(
+    (name: string) => setUser((prevUser: User) => ({ ...prevUser, name })),
+    [setUser]
+  )
 
-  const updateEmail = (email: string) =>
-    setUser((prevUser: User) => ({ ...prevUser, email }))
+  const updateEmail = useCallback(
+    (email: string) => setUser((prevUser: User) => ({ ...prevUser, email })),
+    [setUser]
+  )
 
-  const updateRegion = (region: { code: string; name: string }) =>
-    setUser((prevUser: User) => ({ ...prevUser, region }))
+  const updateRegion = useCallback(
+    (region: RegionFromGeolocation) =>
+      setUser((prevUser: User) => ({ ...prevUser, region })),
+    [setUser]
+  )
 
-  const updateLoginExpirationDate = (loginExpirationDate: Date | undefined) =>
-    setUser((prevUser: User) => ({ ...prevUser, loginExpirationDate }))
+  const updateLoginExpirationDate = useCallback(
+    (loginExpirationDate: Date | undefined) =>
+      setUser((prevUser: User) => ({ ...prevUser, loginExpirationDate })),
+    [setUser]
+  )
 
-  const updateUserOrganisation = (organisation: UserOrganisationInfo) => {
-    const organisationModifications: UserOrganisationInfo = {}
+  const updateUserOrganisation = useCallback(
+    (organisation: UserOrganisationInfo) => {
+      const organisationModifications: UserOrganisationInfo = {}
 
-    if (organisation.administratorEmail) {
-      organisationModifications.administratorEmail =
-        organisation.administratorEmail
-    }
+      if (organisation.administratorEmail) {
+        organisationModifications.administratorEmail =
+          organisation.administratorEmail
+      }
 
-    if (organisation.slug) {
-      organisationModifications.slug = organisation.slug
-    }
+      if (organisation.slug) {
+        organisationModifications.slug = organisation.slug
+      }
 
-    if (organisation.name) {
-      organisationModifications.name = organisation.name
-    }
+      if (organisation.name) {
+        organisationModifications.name = organisation.name
+      }
 
-    setUser((prevUser: User) => ({
-      ...prevUser,
-      organisation: { ...prevUser.organisation, ...organisationModifications },
-    }))
-  }
+      setUser((prevUser: User) => ({
+        ...prevUser,
+        organisation: {
+          ...prevUser.organisation,
+          ...organisationModifications,
+        },
+      }))
+    },
+    [setUser]
+  )
 
   return {
     updateName,
