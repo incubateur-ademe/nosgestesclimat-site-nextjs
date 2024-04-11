090344064c9a5a216997cac27082e4cc496e65f1
Benjamin Arias <bjlaa@proton.me>
2024-04-11 16:13:50 +0200

    Update DishesNumberInfo.tsx

--- src/components/questions/Plats.tsx
+++ src/components/questions/Plats.tsx
@@ -1,46 +1,11 @@
 import Question from '@/components/form/Question'
-import Trans from '@/components/translation/Trans'
-import Emoji from '@/design-system/utils/Emoji'
-import { useEngine } from '@/publicodes-state'
+import DishesNumberInfo from './plats/DishesNumberInfo'
 
 export default function Plats() {
-  const { getNumericValue } = useEngine()
-
-  const totalNumberOfPlats = getNumericValue('ui . nombre de repas par semaine')
-
   return (
     <>
       <Question question={'alimentation . plats'} />
-      <div aria-live="polite" className="mb-2 text-center text-sm">
-        {totalNumberOfPlats < 12 ? (
-          <span className="text-red-700">
-            <strong>{totalNumberOfPlats}</strong>{' '}
-            <strong>
-              <Trans>repas</Trans>
-            </strong>{' '}
-            <Trans>par semaine, quel appétit de moineau</Trans>
-            <Emoji>🐦</Emoji>
-          </span>
-        ) : null}
-        {totalNumberOfPlats > 16 ? (
-          <span className="text-red-700">
-            <strong>{totalNumberOfPlats}</strong>{' '}
-            <strong>
-              <Trans>repas</Trans>
-            </strong>{' '}
-            <Trans>par semaine, quel appétit !</Trans> <Emoji>💪</Emoji>
-          </span>
-        ) : null}
-        {totalNumberOfPlats >= 12 && totalNumberOfPlats <= 16 ? (
-          <span>
-            <strong>{totalNumberOfPlats}</strong>{' '}
-            <strong>
-              <Trans>repas</Trans>
-            </strong>{' '}
-            <Trans>par semaine, miam</Trans> <Emoji>😋</Emoji>
-          </span>
-        ) : null}
-      </div>
+      <DishesNumberInfo />
     </>
   )
 }
