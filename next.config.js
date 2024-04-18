3ac207dcec22abb5335d489a3db8f13114b5ae20
Benjamin Arias <bjlaa@proton.me>
2024-04-18 10:34:15 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -65,7 +65,7 @@ const sentryConfig = [
   },
 ]
 
-module.exports = //withSentryConfig(
-  withBundleAnalyzer(withMDX(withYaml(nextConfig))) //,
-//...sentryConfig
-//)
+module.exports = withSentryConfig(
+  withBundleAnalyzer(withMDX(withYaml(nextConfig))),
+  ...sentryConfig
+)
