f4aff334f56a38e3f98d1ae8b8294e16349df89b
Benjamin Arias <bjlaa@proton.me>
2024-04-24 13:55:26 +0200

    feat: Ajoute bouton

--- src/app/(layout-with-navigation)/(simulation)/organisations/[slug]/parametres/_components/QuestionsComplementaires.tsx
+++ src/app/(layout-with-navigation)/(simulation)/organisations/[slug]/parametres/_components/QuestionsComplementaires.tsx
@@ -4,6 +4,7 @@ import { useUpdateOrganisation } from '@/app/(layout-with-navigation)/(simulatio
 import ModificationSaved from '@/components/messages/ModificationSaved'
 import Trans from '@/components/translation/Trans'
 import { organisationsParametersToggleAdditionnalQuestionsPostCode } from '@/constants/tracking/pages/organisationsParameters'
+import Button from '@/design-system/inputs/Button'
 import { useUser } from '@/publicodes-state'
 import { Organisation } from '@/types/organisations'
 import { trackEvent } from '@/utils/matomo/trackEvent'
@@ -135,10 +136,15 @@ export default function QuestionsComplementaires({
         />
       </div>
 
-      <ModificationSaved
-        shouldShowMessage={isConfirmingUpdate}
-        label={<Trans>Modification sauvegardée</Trans>}
-      />
+      <div className="mt-6 flex gap-2">
+        <Button color="link" size="sm">
+          + <Trans>Ajouter une question personnalisée</Trans>
+        </Button>
+        <ModificationSaved
+          shouldShowMessage={isConfirmingUpdate}
+          label={<Trans>Modification sauvegardée</Trans>}
+        />
+      </div>
     </section>
   )
 }
