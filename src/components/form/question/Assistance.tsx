0caebdf6e00965a43ed71b3735e936a664aa068d
Florian <hi@florianpanchout.com>
2024-04-24 17:41:41 +0200

    feat: Lubies Clemoviennes

--- src/components/form/question/Assistance.tsx
+++ src/components/form/question/Assistance.tsx
@@ -1,29 +1,56 @@
 import { useRule } from '@/publicodes-state'
-import { useState } from 'react'
 
 import Label from '@/components/form/question/Label'
 import NumberInput from '@/components/form/question/NumberInput'
+import { useEffect, useRef } from 'react'
 
 type Props = {
   question: string
   assistance: string
+  setTempValue?: (value: number | undefined) => void
 }
 
-//TODO: Model shenanigans
-export default function Assistance({ question, assistance }: Props) {
-  const { setValue: setValueOfParent, parent } = useRule(question)
-
-  const assistanceFullDottedName = parent + ' . ' + assistance
+export default function Assistance({
+  question,
+  assistance,
+  setTempValue,
+}: Props) {
+  const { setValue: setValueOfQuestion, value: valueOfQuestion } =
+    useRule(question)
 
   const {
     type,
     label,
     description,
     unit,
-    value: constantMultiplier,
-  } = useRule(assistanceFullDottedName)
+    numericValue: numericValueOfAssistance,
+    setValue: setValueOfAssistance,
+    parent,
+  } = useRule(assistance)
 
-  const [inputValue, setInputValue] = useState(0)
+  const { numericValue: numericValueOfParent } = useRule(parent)
+
+  // If the assistance value changed and it is not synced with the question value
+  // we update the question value (and the tempValue of the input)
+  const prevNumericValueOfParent = useRef(numericValueOfParent)
+  useEffect(() => {
+    if (
+      numericValueOfParent !== valueOfQuestion &&
+      prevNumericValueOfParent.current !== numericValueOfParent
+    ) {
+      setValueOfQuestion(numericValueOfParent, { foldedStep: question })
+      if (setTempValue) {
+        setTempValue(numericValueOfParent)
+      }
+    }
+    prevNumericValueOfParent.current = numericValueOfParent
+  }, [
+    numericValueOfParent,
+    valueOfQuestion,
+    setValueOfQuestion,
+    setTempValue,
+    question,
+  ])
 
   return (
     <div className="mb-4 w-[20rem] max-w-full rounded-xl bg-white p-4 sm:w-2/3">
@@ -38,15 +65,9 @@ export default function Assistance({ question, assistance }: Props) {
       {type === 'number' && (
         <NumberInput
           unit={unit ? unit.split('/')[0] : ''}
-          value={inputValue}
-          setValue={(value: number = 0) => {
-            setValueOfParent(
-              Math.round(12 * (value / Number(constantMultiplier)) * 10) / 10,
-              { foldedStep: question }
-            )
-            setInputValue(value)
-          }}
-          isMissing={inputValue ? false : true}
+          value={numericValueOfAssistance}
+          setValue={setValueOfAssistance}
+          isMissing={numericValueOfAssistance ? false : true}
         />
       )}
     </div>
