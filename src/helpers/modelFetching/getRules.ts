672c88aa3ccbbd8d9a15bccec883236db4faa2d1
Florian <hi@florianpanchout.com>
2024-04-25 18:21:37 +0200

    feat: same model for everyone

--- src/helpers/modelFetching/getRules.ts
+++ src/helpers/modelFetching/getRules.ts
@@ -1,36 +1,30 @@
 import { NGCRules } from '@/publicodes-state/types'
-import { getFileFromModel } from './getFileFromModel'
-import { getSupportedRegions } from './getSupportedRegions'
+import rules from '@incubateur-ademe/nosgestesclimat'
 
-type Props = {
-  isOptim?: boolean
-  regionCode?: string
-  locale?: string
-  PRNumber?: string
-}
+// type Props = {
+//   isOptim?: boolean
+//   regionCode?: string
+//   locale?: string
+//   PRNumber?: string
+// }
 /*
  * This function is used to get the rules. It is used in the useRules hook and can also be called directly from a server component.
  */
-export async function getRules(
-  { locale = 'fr', regionCode = 'FR', isOptim = true, PRNumber }: Props = {
-    locale: 'fr',
-    regionCode: 'FR',
-    isOptim: true,
-  }
-): Promise<NGCRules> {
-  const supportedRegions = await getSupportedRegions()
-
+export async function getRules(test: any): Promise<NGCRules> {
+  // const supportedRegions = await getSupportedRegions()
+  console.log(test)
   // We provide the FR version of the model if the region is not supported
-  const regionCodeToProvide = supportedRegions[regionCode] ? regionCode : 'FR'
+  // const regionCodeToProvide = supportedRegions[regionCode] ? regionCode : 'FR'
 
-  let fileName = ''
+  // let fileName = ''
 
+  return Promise.resolve(rules)
   // We provide optimized version of the model only for the FR region
-  if (regionCodeToProvide === 'FR') {
-    fileName = `co2-model.FR-lang.${locale}${isOptim ? '-opti' : ''}.json`
-  } else {
-    fileName = `co2-model.${regionCodeToProvide}-lang.${locale}.json`
-  }
+  // if (regionCodeToProvide === 'FR') {
+  //   fileName = `co2-model.FR-lang.${locale}${isOptim ? '-opti' : ''}.json`
+  // } else {
+  //   fileName = `co2-model.${regionCodeToProvide}-lang.${locale}.json`
+  // }
 
-  return getFileFromModel({ fileName, PRNumber })
+  // return getFileFromModel({ fileName, PRNumber })
 }
