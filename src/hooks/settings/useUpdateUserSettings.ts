26b0ccf0d680d26f7150342966aea4420dbc0740
Benjamin Arias <bjlaa@proton.me>
2024-04-29 11:30:07 +0200

    fix: Permet la sauvegarde d'un nouvel email via les paramètres

--- src/hooks/settings/useUpdateUserSettings.ts
+++ src/hooks/settings/useUpdateUserSettings.ts
@@ -10,14 +10,15 @@ type Props = {
 type FuncProps = {
   name: string
   newsletterIds: Record<number, boolean>
+  email?: string
 }
 
 export function useUpdateUserSettings({ email, userId }: Props) {
   return useMutation({
     mutationKey: ['updateUserSettings', email, userId],
-    mutationFn: async ({ name, newsletterIds }: FuncProps) =>
+    mutationFn: async ({ name, newsletterIds, email: emailParam }: FuncProps) =>
       axios.post(SERVER_URL + '/update-settings', {
-        email,
+        email: email || emailParam,
         userId,
         name,
         newsletterIds,
