18dcc607f71bcdbfe15de86a6bb68b2d6e452d05
Benjamin Arias <bjlaa@proton.me>
2024-04-30 16:23:36 +0200

    feat: Passe ref

--- src/app/(layout-with-navigation)/(simulation)/organisations/[slug]/parametres/_components/questionsComplementaires/EditableToggleField.tsx
+++ src/app/(layout-with-navigation)/(simulation)/organisations/[slug]/parametres/_components/questionsComplementaires/EditableToggleField.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler } from 'react'
+import { ChangeEventHandler, ForwardedRef, forwardRef } from 'react'
 import { DebounceInput } from 'react-debounce-input'
 import { twMerge } from 'tailwind-merge'
 
@@ -8,11 +8,10 @@ type Props = {
   className?: string
 }
 
-export default function EditableToggleField({
-  name = 'newQuestion',
-  onChange,
-  className,
-}: Props) {
+export default forwardRef(function EditableToggleField(
+  { name = 'newQuestion', onChange, className }: Props,
+  ref: ForwardedRef<HTMLInputElement>
+) {
   return (
     <div
       className={twMerge(
@@ -21,19 +20,20 @@ export default function EditableToggleField({
       )}>
       <div className="flex w-full justify-between">
         <DebounceInput
+          inputRef={ref}
           onChange={onChange}
           debounceTimeout={500}
           name={name}
           type="text"
           // eslint-disable-next-line jsx-a11y/no-autofocus
           autoFocus
-          className="mr-2 w-full border-none bg-transparent px-1 text-base"
+          className="mr-2 w-full border-primary-700 bg-transparent px-1 pb-1 text-base outline-none focus:!border-b-2"
         />
 
         <div className="relative inline-flex items-center justify-between">
-          <div className="peer h-6 w-11 rounded-full bg-primary-200  after:absolute  after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-primary-700 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-primary-300" />
+          <div className="peer h-6 w-11 rounded-full bg-primary-200  after:absolute  after:left-[2px] after:top-[5px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-primary-700 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-primary-300" />
         </div>
       </div>
     </div>
   )
-}
+})
