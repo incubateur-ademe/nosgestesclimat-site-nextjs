1240be6bd2915d89fd5cdca6cdf7ac9c8071305e
Florian <hi@florianpanchout.com>
2024-05-09 14:27:15 +0200

    feat: add share directly on button

--- src/app/(layout-with-navigation)/(simulation)/fin/_components/heading/HeadingButtons.tsx
+++ src/app/(layout-with-navigation)/(simulation)/fin/_components/heading/HeadingButtons.tsx
@@ -2,6 +2,7 @@ import SaveIcon from '@/components/icons/SaveIcon'
 import ShareIcon from '@/components/icons/ShareIcon'
 import { endClickSaveShortcut } from '@/constants/tracking/pages/end'
 import Button from '@/design-system/inputs/Button'
+import { useEndPageSharedUrl } from '@/hooks/useEndPageSharedUrl'
 import { trackEvent } from '@/utils/matomo/trackEvent'
 import { twMerge } from 'tailwind-merge'
 
@@ -22,6 +23,27 @@ type Props = {
   size?: 'sm' | 'md'
 }
 export default function HeadingButtons({ size = 'md' }: Props) {
+  const { sharedUrl } = useEndPageSharedUrl()
+
+  const handleShare = async () => {
+    if (navigator?.share) {
+      await navigator
+        .share({
+          text: sharedUrl,
+          url: sharedUrl,
+          title: 'Découvre mon empreinte carbone !',
+        })
+        .catch(handleScroll)
+    } else {
+      handleScroll('share-block')
+    }
+  }
+
+  const handleScroll = (id: string) => {
+    const emailBlock = document.getElementById(id)
+    emailBlock?.scrollIntoView({ behavior: 'smooth', block: 'center' })
+  }
+
   return (
     <div className="flex gap-4">
       <Button
@@ -29,9 +51,7 @@ export default function HeadingButtons({ size = 'md' }: Props) {
         className={twMerge('p-0', sizeClassNames[size])}
         onClick={() => {
           trackEvent(endClickSaveShortcut)
-
-          const emailBlock = document.getElementById('email-block')
-          emailBlock?.scrollIntoView({ behavior: 'smooth', block: 'center' })
+          handleScroll('email-block')
         }}>
         <SaveIcon
           className={twMerge('fill-primary-700', saveClassNames[size])}
@@ -39,10 +59,7 @@ export default function HeadingButtons({ size = 'md' }: Props) {
       </Button>
       <Button
         className={twMerge('p-0', sizeClassNames[size])}
-        onClick={() => {
-          const emailBlock = document.getElementById('share-block')
-          emailBlock?.scrollIntoView({ behavior: 'smooth', block: 'center' })
-        }}>
+        onClick={handleShare}>
         <ShareIcon
           className={twMerge('mr-[1px] fill-white', shareClassNames[size])}
         />
