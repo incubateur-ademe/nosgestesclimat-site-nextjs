954e9f58a39e8a46719ebdc20ef6c1358c3cbc47
Benjamin Arias <bjlaa@proton.me>
2024-05-09 11:09:33 +0200

    feat: Crée composant

--- src/app/(layout-with-navigation)/(simulation)/organisations/connexion/_components/emailSection/verificationForm/NotReceived.tsx
+++ src/app/(layout-with-navigation)/(simulation)/organisations/connexion/_components/emailSection/verificationForm/NotReceived.tsx
@@ -1,18 +1,13 @@
 'use client'
 
-import CheckCircleIcon from '@/components/icons/CheckCircleIcon'
 import EyeIcon from '@/components/icons/EyeIcon'
-import LockIcon from '@/components/icons/LockIcon'
 import ReturnIcon from '@/components/icons/ReturnIcon'
-import SendIcon from '@/components/icons/SendIcon'
 import Trans from '@/components/translation/Trans'
-import { organisationsConnexionClickCode } from '@/constants/tracking/pages/organisationsConnexion'
 import Button from '@/design-system/inputs/Button'
-import { useClientTranslation } from '@/hooks/useClientTranslation'
 import { useUser } from '@/publicodes-state'
-import { trackEvent } from '@/utils/matomo/trackEvent'
 import { useRouter } from 'next/navigation'
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
+import ResendButton from './notReceived/ResendButton'
 
 type Props = {
   isRetryButtonDisabled: boolean
@@ -29,15 +24,9 @@ export default function NotReceived({
   setTimeLeft,
   timeLeft,
 }: Props) {
-  const [shouldDisplayConfirmation, setShouldDisplayConfirmation] =
-    useState(false)
-  const { t } = useClientTranslation()
-
   const router = useRouter()
 
   const { updateLoginExpirationDate } = useUser()
-
-  const timeoutRef = useRef<NodeJS.Timeout>()
 
   function handleGoBackToForm() {
     // Reset the login expiration date
@@ -45,30 +34,6 @@ export default function NotReceived({
 
     router.push('/organisations/connexion')
   }
-
-  async function handleResendVerificationCode() {
-    if (isRetryButtonDisabled) {
-      return
-    }
-
-    trackEvent(organisationsConnexionClickCode)
-
-    await sendVerificationCode()
-    setShouldDisplayConfirmation(true)
-
-    timeoutRef.current = setTimeout(() => {
-      setTimeLeft(30)
-      setShouldDisplayConfirmation(false)
-    }, 1500)
-  }
-
-  useEffect(() => {
-    return () => {
-      if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current)
-      }
-    }
-  }, [])
 
   return (
     <>
@@ -89,45 +54,12 @@ export default function NotReceived({
       </p>
 
       {!isErrorResend && (
-        <div className="flex items-center">
-          <Button
-            color="link"
-            size="sm"
-            aria-disabled={isRetryButtonDisabled}
-            aria-label={
-              isRetryButtonDisabled
-                ? t('Renvoyer le code, désactivé pendant 30 secondes')
-                : ''
-            }
-            className="-ml-2 font-normal"
-            onClick={handleResendVerificationCode}>
-            {isRetryButtonDisabled && timeLeft > 0 && (
-              <span className="mr-2 flex items-center">
-                <LockIcon className="mr-2 h-4 w-4 fill-primary-700" />
-                <Trans>Renvoyer le code</Trans>
-              </span>
-            )}
-
-            {shouldDisplayConfirmation && (
-              <span className="flex items-center text-green-500 no-underline">
-                <CheckCircleIcon className="mr-2 h-4 w-4 fill-green-500" />
-                <Trans>Code renvoyé</Trans>
-              </span>
-            )}
-
-            {!shouldDisplayConfirmation && !isRetryButtonDisabled && (
-              <span className="flex items-center underline">
-                <SendIcon className="mr-2 h-4 w-4 fill-primary-700" />
-                <Trans>Renvoyer le code</Trans>
-              </span>
-            )}
-          </Button>
-          {isRetryButtonDisabled && timeLeft > 0 && (
-            <span className="text-xs font-normal text-gray-500 !no-underline">
-              <Trans>(Attendre</Trans> {timeLeft} <Trans>secondes)</Trans>
-            </span>
-          )}
-        </div>
+        <ResendButton
+          isRetryButtonDisabled={isRetryButtonDisabled}
+          sendVerificationCode={sendVerificationCode}
+          setTimeLeft={setTimeLeft}
+          timeLeft={timeLeft}
+        />
       )}
 
       {isErrorResend && (
