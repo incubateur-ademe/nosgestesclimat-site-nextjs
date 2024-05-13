691418330339029b4129b5cc59cfeaee82853ae8
Florian <hi@florianpanchout.com>
2024-05-13 15:23:01 +0200

    feat: colors status for categories

--- src/components/total/totalSticky/categoriesSummary/Category.tsx
+++ src/components/total/totalSticky/categoriesSummary/Category.tsx
@@ -35,6 +35,7 @@ export default function Category({ category }: Props) {
 
   const isCompleted = completion === 1
   const isStarted = completion > 0
+  const isCurrent = currentCategory === category
 
   const { formattedValue, unit } = formatCarbonFootprint(numericValue, {
     t,
@@ -45,23 +46,31 @@ export default function Category({ category }: Props) {
     <div
       className={twMerge(
         'relative mb-0 flex w-full items-center justify-between gap-4 overflow-hidden rounded-xl border-2 bg-transparent px-2 py-2 transition-colors',
-        isStarted ? getTextDarkColor(category) : 'text-slate-600',
+        isStarted || isCurrent ? getTextDarkColor(category) : 'text-slate-600',
 
-        currentCategory === category
+        isCurrent
           ? getBorderColor(category)
-          : getBorderLightColor(category)
+          : isStarted
+            ? getBorderLightColor(category)
+            : 'border-slate-200'
       )}>
       <div
         className={twMerge(
           'absolute bottom-0 left-0 right-0 top-0 origin-left transition-transform duration-200',
-          currentCategory === category
+          isCurrent
             ? getBackgroundLightColor(category)
             : getBackgroundLightColor(category)
         )}
         style={{ transform: `scaleX(${completion})` }}
       />
       <div className="relative flex items-center font-medium">
-        <Emoji className="mr-2">{icons}</Emoji>
+        <Emoji
+          className={twMerge(
+            isStarted || isCurrent ? 'opacity-100' : 'opacity-50',
+            'mr-2 transition-opacity'
+          )}>
+          {icons}
+        </Emoji>
         <span>{title}</span>{' '}
       </div>
       <span
