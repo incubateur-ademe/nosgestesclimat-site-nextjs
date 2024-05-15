9b6fc6e5426cb77dead2f32c2a72f3f4592fc798
Benjamin Arias <bjlaa@proton.me>
2024-05-15 11:47:32 +0200

    feat: Retire le formatage des situations + unformat uniquement les clés formatées

--- src/utils/formatDataForDB.ts
+++ src/utils/formatDataForDB.ts
@@ -1,28 +1,40 @@
 import { Situation } from '@/publicodes-state/types'
 
-type SituationFormated = {
-  [key: string]: any
-}
+export function unformatSituation(situation?: Situation) {
+  return Object.entries({ ...situation }).reduce(
+    (acc: Situation, [key, value]: [string, any]) => {
+      // Key is not formatted
+      if (!key.includes('_')) {
+        acc[key] = value
+        return acc
+      }
 
-/**
- * Formats the simulation data, removing the ' . ' from the keys
- * @param situation
- * @returns SituationFormated
- */
-export const formatSituation = (situation: Situation): SituationFormated => {
-  return Object.entries({ ...situation } as { [key: string]: any }).reduce(
-    (acc: { [key: string]: any }, [key, value]: [string, any]) => {
-      acc[key.replaceAll(' . ', '_').replaceAll(' ', '-')] = value
-      return acc
-    },
-    {}
-  )
-}
+      const keyUnformatted = key.replaceAll('_', ' . ').replaceAll('-', ' ')
 
-export const unformatSituation = (situation: SituationFormated): Situation => {
-  return Object.entries({ ...situation } as { [key: string]: any }).reduce(
-    (acc: { [key: string]: any }, [key, value]: [string, any]) => {
-      acc[key.replaceAll('_', ' . ').replaceAll('-', ' ')] = value
+      const wordsToHardcode = {
+        't shirt': 't-shirt',
+        'sèche linge': 'sèche-linge',
+        'lave linge': 'lave-linge',
+        'lave vaisselle': 'lave-vaisselle',
+        'micro onde': 'micro-onde',
+        'éco construit': 'éco-construit',
+      }
+
+      let keyUnformattedHandlingHardcodedWords = keyUnformatted
+
+      for (const [keyToHardcode, valueToHardcode] of Object.entries(
+        wordsToHardcode
+      )) {
+        if (keyUnformatted.includes(keyToHardcode)) {
+          keyUnformattedHandlingHardcodedWords = keyUnformatted.replace(
+            keyToHardcode,
+            valueToHardcode
+          )
+        }
+      }
+
+      acc[keyUnformattedHandlingHardcodedWords] = value
+
       return acc
     },
     {}
