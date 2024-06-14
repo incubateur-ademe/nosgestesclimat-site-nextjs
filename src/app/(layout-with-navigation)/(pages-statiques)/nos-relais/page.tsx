5443b8ee3bdd269c547b1ef2eeb430f116fab610
Benjamin Arias <bjlaa@proton.me>
2024-06-14 10:16:24 +0200

    feat: Ajoute et modifie

--- src/app/(layout-with-navigation)/(pages-statiques)/nos-relais/page.tsx
+++ src/app/(layout-with-navigation)/(pages-statiques)/nos-relais/page.tsx
@@ -1,4 +1,5 @@
 import Trans from '@/components/translation/Trans'
+import InlineLink from '@/design-system/inputs/InlineLink'
 import Card from '@/design-system/layout/Card'
 import Title from '@/design-system/layout/Title'
 import { getServerTranslation } from '@/helpers/getServerTranslation'
@@ -33,17 +34,28 @@ export default function page() {
       </Title>
 
       <p>
-        Plus de 40 acteurs relaient ou ont relayé Nos Gestes Climat à travers
-        l’intégration du calculateur sur leur site internet et/ou sa diffusion
-        via des campagnes (mail, réseaux sociaux et/ou affichage). C’est
-        majoritairement grâce à eux que nous sensibilisons près de 2 000
-        nouvelles personnes en moyenne chaque jour et nous les en remercions.
+        Plus de 40 acteurs relaient{' '}
+        <span className="text-secondary-700">ou</span> ont relayé Nos Gestes
+        Climat à travers 
+        <a
+          href="https://accelerateur-transition-ecologique-ademe.notion.site/Int-grer-Nos-Gestes-Climat-en-iframe-abdeb175baf84143922006964d80348c"
+          target="_blank"
+          rel="noopener noreferrer">
+          l’intégration du calculateur
+        </a>{' '}
+        sur leur site internet <span className="text-secondary-700">ou</span> sa
+        diffusion via{' '}
+        <InlineLink href="/organisations">des campagnes</InlineLink> (mail,
+        réseaux sociaux et/ou affichage). C’est majoritairement grâce à eux que
+        nous sensibilisons près de 2 000 nouvelles personnes en moyenne chaque
+        jour et nous les en remercions.
       </p>
 
       <p>
         Vous avez relayé Nos Gestes Climat et souhaitez apparaître dans notre
-        galerie de relais ? Merci de nous envoyer un message avec notre logo à 
-        <span className="underline">contact@nosgestesclimat.fr</span>.
+        galerie de relais ? Merci de nous envoyer un message avec{' '}
+        <span className="text-secondary-700">votre logo</span> via{' '}
+        <InlineLink href="/contact">notre page de contact</InlineLink>.
       </p>
 
       <p className="mb-8 italic">
