7b5e5aef0d4404dd63305a19477f84e438607553
Benjamin Arias <bjlaa@proton.me>
2024-06-20 11:48:31 +0200

    fix: Améliore la sélection d'actions

--- src/components/actions/howToAct/RecommendedActions.tsx
+++ src/components/actions/howToAct/RecommendedActions.tsx
@@ -1,21 +1,49 @@
 'use client'
 
+import { useActions, useEngine } from '@/publicodes-state'
+import { DottedName } from '@/publicodes-state/types'
+import { useMemo } from 'react'
 import RecommendedAction from './recommendedActions/RecommendedAction'
 
 export default function RecommendedActions() {
+  const { orderedActions: orderedActionDottedNames } = useActions()
+  const { getCategory } = useEngine()
+
+  const filteredSameCategoriesActionDottedNames = useMemo(
+    () =>
+      orderedActionDottedNames
+        .reduce(
+          (accumulator: string[], currentActionDottedName: DottedName) => {
+            if (currentActionDottedName.includes('services sociétaux')) {
+              return accumulator
+            }
+
+            return accumulator.find((actionDottedName) => {
+              return (
+                getCategory(actionDottedName) ===
+                getCategory(currentActionDottedName)
+              )
+            })
+              ? accumulator
+              : [...accumulator, currentActionDottedName]
+          },
+          []
+        )
+        .slice(0, 3),
+    [orderedActionDottedNames, getCategory]
+  )
+
   return (
     <div className="max-w-screen overflow-auto">
       <ul className="flex flex-row gap-4">
-        {[
-          'alimentation . viande faible empreinte',
-          'transport . covoiturage',
-          'logement . rénovation énergétique',
-        ].map((actionDottedName, index) => (
-          <RecommendedAction
-            actionDottedName={actionDottedName}
-            key={`recommended-actionDottedName-${index}`}
-          />
-        ))}
+        {filteredSameCategoriesActionDottedNames.map(
+          (actionDottedName, index) => (
+            <RecommendedAction
+              actionDottedName={actionDottedName}
+              key={`recommended-actionDottedName-${index}`}
+            />
+          )
+        )}
       </ul>
     </div>
   )
