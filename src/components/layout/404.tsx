f4d23bf34a92ac174a5a1941163f6543c109c6b7
Florian <hi@florianpanchout.com>
2024-06-24 09:06:53 +0200

    feat: new wave animation

--- src/components/layout/404.tsx
+++ src/components/layout/404.tsx
@@ -1,79 +1,51 @@
 import ButtonLink from '@/design-system/inputs/ButtonLink'
 import { getServerTranslation } from '@/helpers/getServerTranslation'
 import Image from 'next/image'
+import Wave from 'react-wavify'
 import Trans from '../translation/Trans'
 
 export default async function Route404() {
   const { t } = await getServerTranslation()
   return (
-    <div className="mx-auto text-center text-primary-700">
-      <div className="fixed left-0 right-0 top-[16%]">
-        <svg
-          className="absolute left-0 right-0 top-0 h-screen w-[3000px] fill-primary-700"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 24 150 28">
-          <path
-            className="wave-1"
-            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+    <div className="relative h-svh">
+      <div className="absolute bottom-0 left-0 right-0 top-0 bg-[#1617C5] opacity-75" />
+      <div className="relative mt-20 flex flex-col items-center lg:mt-36">
+        <div className="relative w-full bg-white">
+          <h1 className="mb-2 flex items-center justify-center text-[10rem] font-black text-amber-400  md:text-[20rem]">
+            <span className="island leading-none">4</span>
+            <span className="island relative leading-none">
+              <Image
+                className="absolute -top-16 left-0 right-0 m-auto w-10 md:w-12"
+                src="/images/misc/404_bonhomme.svg"
+                width="60"
+                height="60"
+                alt={t('Un bonhomme se demandant où il est')}
+              />
+              0
+            </span>
+            <span className="island leading-none">4</span>
+          </h1>
+          <Wave
+            fill="#1617C5"
+            className="absolute bottom-0 left-0 right-0 h-full w-full opacity-75"
+            options={{
+              height: 70,
+              amplitude: 70,
+              speed: 0.11,
+              points: 3,
+            }}
           />
-          <path
-            className="wave-2 w-1/2 opacity-95"
-            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
-          />
-          <path
-            className="wave-3 opacity-90"
-            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
-          />
-          <path
-            className="wave-4 opacity-85"
-            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
-          />
-          <path
-            className="wave-5 opacity-90"
-            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
-          />
-          <path
-            className="wave-6 opacity-95"
-            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
-          />
-          <path
-            className="wave-7 opacity-80"
-            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
-          />
-          <path
-            className="wave-8 opacity-95"
-            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
-          />
-          <path
-            className="wave-9 opacity-85"
-            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
-          />
-        </svg>
-      </div>
-
-      <div className="absolute left-0 right-0 top-[12rem] flex flex-col items-center justify-end">
-        <h1 className="flex h-[15rem] items-center justify-center text-[10rem] text-primary-950 md:h-[20rem] md:text-[20rem]">
-          <span className="island mt-24 animate-bounce">4</span>
-          <span className="island relative">
-            <Image
-              className="absolute -top-[32px] left-0 right-0 m-auto w-10 md:top-[10px] md:w-12"
-              src="/images/misc/404_bonhomme.svg"
-              width="60"
-              height="60"
-              alt={t('Un bonhomme se demandant où il est')}
-            />
-            0
-          </span>
-          <span className="island mt-24 animate-bounce">4</span>
-        </h1>
-
-        <p className="text-base font-bold text-white sm:text-lg md:mt-8 md:text-2xl">
-          <Trans>Rien à l'horizon ! La page recherchée n'existe pas.</Trans>
+        </div>
+        <p className="relative text-center font-bold text-white sm:text-lg md:text-2xl">
+          <Trans>
+            Rien à l'horizon ! <br className="md:hidden" />
+            La page recherchée n'existe pas.
+          </Trans>
         </p>
         <ButtonLink
-          color="secondary"
+          color="primary"
           href="/"
-          className="mt-8 justify-self-center bg-white">
+          className="relative mt-8 justify-self-center border-2 border-white bg-white !text-primary-700 shadow-sm hover:bg-white hover:text-primary-700">
           <Trans>Revenir à l'accueil</Trans>
         </ButtonLink>
       </div>
