137b0e151a2c475d520af53fb7b6cba1c6fb0b66
Florian <hi@florianpanchout.com>
2024-07-18 08:48:28 +0200

    feat: slimmer dots

--- src/components/fin/CarboneTotalChart.tsx
+++ src/components/fin/CarboneTotalChart.tsx
@@ -9,7 +9,7 @@ type Props = {
 }
 export default function CarboneTotalChart({ total }: Props) {
   return (
-    <div className="lg:py-8">
+    <div className="pb-4 lg:py-8">
       <div className="relative mx-auto mb-16 mt-32 w-full md:w-[640px] lg:mt-36">
         <TotalNumber total={total} />
         <Gauge />
