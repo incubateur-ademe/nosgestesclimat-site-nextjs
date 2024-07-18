3da0438457341a2e98ae76b395a9f587a56d84e8
Florian <hi@florianpanchout.com>
2024-07-18 08:26:06 +0200

    feat: change text for domestic water

--- src/app/(layout-with-navigation)/(simulation)/fin/_components/eau/DomesticWater.tsx
+++ src/app/(layout-with-navigation)/(simulation)/fin/_components/eau/DomesticWater.tsx
@@ -16,7 +16,7 @@ export default function DomesticWater() {
       <Title tag="h2">
         <Trans>
           Qu’est-ce que{' '}
-          <strong className="text-secondary-700">l’eau domestique ?</strong>
+          <strong className="text-secondary-700">l’eau domestique</strong> ?
         </Trans>
       </Title>
       <p>
@@ -25,7 +25,7 @@ export default function DomesticWater() {
           travers de votre facture d’eau.
         </Trans>
       </p>
-      <div className="mb-4 flex gap-4 rounded-xl border-2 border-primary-50 bg-gray-100 p-4 lg:pr-8">
+      <div className="mb-4 flex gap-4 self-center rounded-xl border-2 border-primary-50 bg-gray-100 p-4 lg:pr-8">
         <svg
           className="h-auto fill-water lg:w-14"
           width="48"
@@ -36,7 +36,7 @@ export default function DomesticWater() {
           <path d="M37.7916 23.6907L24.197 1.45337L11.5996 23.5387C8.66632 28.2187 8.41032 34.304 11.4916 39.344C15.8716 46.508 25.309 48.808 32.573 44.4867C39.8343 40.1667 42.1716 30.86 37.7916 23.6907Z" />
         </svg>
         <p className="mb-0 lg:text-xl">
-          <Trans>Vous consommez</Trans> <br className="hidden lg:inline" />
+          <Trans>Vous utilisez</Trans> <br className="hidden lg:inline" />
           <strong className="font-black text-water">
             {formattedValue} <Trans>{unit}</Trans>
           </strong>{' '}
@@ -46,8 +46,10 @@ export default function DomesticWater() {
 
       <p>
         <Trans>
-          Elle représente une petite partie de votre empreinte eau mais elle
-          peut avoir un{' '}
+          Contrairement à l'empreinte eau, l'eau domestique n'est pas consommée,
+          mais seulement prélevée. Cela signifie qu'elle est dépolluée puis
+          restituée au cycle de l'eau après utilisation. Néanmoins elle peut
+          avoir un{' '}
           <strong className="font-black text-secondary-700">
             très fort impact localisé
           </strong>{' '}
@@ -61,7 +63,7 @@ export default function DomesticWater() {
           subir) des restrictions d'eau aujourd'hui :
         </Trans>
       </p>
-      <div className="mx-auto mb-8 max-w-96">
+      <div className="mb-8 max-w-96 self-center">
         <VigieEau />
       </div>
       <p>
