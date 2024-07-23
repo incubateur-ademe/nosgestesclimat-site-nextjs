a33c8dc9752c4283fcb425b8fb48206c3aa83579
Clément <clem.auger@hotmail.fr>
2024-07-23 09:07:06 +0200

    fix: never import supportedRegions from PR

--- src/helpers/modelFetching/getSupportedRegions.ts
+++ src/helpers/modelFetching/getSupportedRegions.ts
@@ -1,19 +1,9 @@
 import { SupportedRegions } from '@incubateur-ademe/nosgestesclimat'
 import supportedRegions from '@incubateur-ademe/nosgestesclimat/public/supportedRegions.json'
-import { importPreviewFile } from './importPreviewFile'
-type Props = {
-  PRNumber?: string
-}
+
 /**
  * This function is used to get the supported regions. It can be called directly from a server component.
  */
-export async function getSupportedRegions({
-  PRNumber,
-}: Props = {}): Promise<SupportedRegions> {
-  if (PRNumber) {
-    const fileName = `supportedRegions.json`
-    return importPreviewFile({ fileName, PRNumber })
-  }
-
+export async function getSupportedRegions(): Promise<SupportedRegions> {
   return Promise.resolve(supportedRegions)
 }
