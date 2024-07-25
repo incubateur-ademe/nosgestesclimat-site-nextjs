33c95529ce28906c39a694e143016946f8e1f1a0
Florian <hi@florianpanchout.com>
2024-07-25 09:50:49 +0200

    fix: water now takes all width

--- src/components/fin/waterTotalChart/WaveContent.tsx
+++ src/components/fin/waterTotalChart/WaveContent.tsx
@@ -3,7 +3,7 @@ import Octopus from './waveContent/Octopus'
 
 export default function WaveContent() {
   return (
-    <div className="relative overflow-hidden rounded-b-xl px-4 pb-4 pt-12 lg:px-24 lg:pb-8 lg:pt-14">
+    <div className="relative overflow-hidden rounded-b-xl px-4 pb-4 pt-12 lg:px-36 lg:pb-8 lg:pt-14">
       <Wave
         fill="#5152D0"
         className="pointer-events-none absolute bottom-0 left-0 right-0 h-full w-full"
