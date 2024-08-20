822d1be4cedf19c56e3e32166597552dd57175c7
Benjamin Arias <bjlaa@proton.me>
2024-08-20 15:02:15 +0200

    :recycle: Improvements after pair review

--- src/hooks/groups/__tests__/fixtures/createGroup.ts
+++ src/hooks/groups/__tests__/fixtures/createGroup.ts
@@ -2,24 +2,33 @@ import { Participant } from '@/types/groups'
 import { faker } from '@faker-js/faker'
 import personas from '@incubateur-ademe/nosgestesclimat/public/personas-fr.json'
 
-export function createGroup(
-  participants: Partial<Participant>[],
+export function createGroup({
+  participants,
+  currentUserId,
+}: {
+  participants: Partial<Participant>[]
   currentUserId: string
-) {
+}) {
   return {
-    participants: participants.map((p, index) => {
-      console.log(
-        'ICI',
-        personas[`personas . ${p.name}` as keyof typeof personas]
-      )
-      return {
-        // We set the first participant as the current user
-        userId: !index ? currentUserId : faker.string.uuid(),
-        simulation: {
-          situation:
-            personas[`personas . ${p.name}` as keyof typeof personas].situation,
+    participants: participants.map((p, index) => ({
+      // We set the first participant as the current user
+      userId: !index ? currentUserId : faker.string.uuid(),
+      simulation: {
+        id: faker.string.uuid(),
+        date: faker.date.recent().toISOString(),
+        foldedSteps: [],
+        actionChoices: {},
+        computedResults: {
+          bilan: faker.number.float(),
+          categories: {},
         },
-      }
-    }),
+
+        progression: 1,
+        situation:
+          personas[`personas . ${p.name}` as keyof typeof personas].situation,
+      },
+      name: p.name || faker.name.firstName(),
+      _id: faker.string.uuid(),
+    })),
   }
 }
--- src/hooks/groups/__tests__/useGetGroupStats.spec.ts
+++ src/hooks/groups/__tests__/useGetGroupStats.spec.ts
@@ -1,18 +1,17 @@
 jest.mock('@/publicodes-state')
 
 import { useGetGroupStats } from '@/hooks/groups/useGetGroupStats'
-import type { PointsFortsFaiblesType } from '@/types/groups'
 import { faker } from '@faker-js/faker'
 import { createGroup } from './fixtures/createGroup'
 
 describe('useGetGroupStats', () => {
   describe('given a group with 2 participants that have different carbon footprint', () => {
-    let group: any
+    let group: ReturnType<typeof createGroup>
     const currentUserId = faker.string.uuid()
 
     beforeEach(() => {
-      group = createGroup(
-        [
+      group = createGroup({
+        participants: [
           {
             name: 'nolan',
           },
@@ -20,15 +19,14 @@ describe('useGetGroupStats', () => {
             name: 'corentin',
           },
         ],
-        currentUserId
-      )
+        currentUserId,
+      })
     })
 
     describe('when the points forts and points faibles are compared', () => {
-      let result: any
+      let result: ReturnType<typeof useGetGroupStats>
 
       beforeEach(() => {
-        console.log('GROUP', group)
         result = useGetGroupStats({
           groupMembers: group.participants,
           userId: currentUserId,
@@ -38,39 +36,17 @@ describe('useGetGroupStats', () => {
       it('then it should return 2 points fort and 3 points faibles', () => {
         expect(result.pointsForts).toHaveLength(2)
         expect(result.pointsFaibles).toHaveLength(3)
-        //   expect(result).toEqual(
-        //     expect.objectContaining({
-        //       pointsForts: [
-        //         {
-        //           key: 'transport . deux roues',
-        //           resultObject: {
-        //             value: 0,
-        //             difference: -116.86560493827166,
-        //             name: 'transport . deux roues',
-        //           },
-        //         },
-        //         {
-        //           key: 'alimentation . boisson',
-        //           resultObject: {
-        //             difference: -46.626666666666694,
-        //             name: 'alimentation . boisson',
-        //             value: 180.16976666666665,
-        //           },
-        //         },
-        //       ],
-        //     })
-        //   )
       })
     })
   })
 
   describe('given a group with 3 participants that have different carbon footprints', () => {
-    let group: any
+    let group: ReturnType<typeof createGroup>
     const currentUserId = faker.string.uuid()
 
     beforeEach(() => {
-      group = createGroup(
-        [
+      group = createGroup({
+        participants: [
           {
             name: 'nolan',
           },
@@ -81,12 +57,12 @@ describe('useGetGroupStats', () => {
             name: 'sandy',
           },
         ],
-        currentUserId
-      )
+        currentUserId,
+      })
     })
 
     describe('when the points forts and points faibles are compared', () => {
-      let result: any
+      let result: ReturnType<typeof useGetGroupStats>
 
       beforeEach(() => {
         result = useGetGroupStats({
@@ -96,16 +72,11 @@ describe('useGetGroupStats', () => {
       })
 
       it('then it should return pointsForts and pointFaibles that have a difference value !== 0', () => {
-        console.log(result)
         expect(
-          result.pointsForts.every(
-            (p: PointsFortsFaiblesType) => p.resultObject.difference !== 0
-          )
+          result.pointsForts.every((p) => p.resultObject.difference !== 0)
         ).toBeTruthy()
         expect(
-          result.pointsFaibles.every(
-            (p: PointsFortsFaiblesType) => p.resultObject.difference !== 0
-          )
+          result.pointsFaibles.every((p) => p.resultObject.difference !== 0)
         ).toBeTruthy()
       })
     })
