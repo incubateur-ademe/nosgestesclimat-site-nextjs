3e71636482ea99f36cf7fd9eeab32d6aecfff9b6
Florian <hi@florianpanchout.com>
2024-08-22 16:24:30 +0200

    feat: small fixes

--- src/app/(simulation)/simulateur/[root]/_components/simulateur/summary/Question.tsx
+++ src/app/(simulation)/simulateur/[root]/_components/simulateur/summary/Question.tsx
@@ -51,7 +51,6 @@ export default function Question({ question, toggleQuestionList }: Props) {
           ? getBackgroundColor(category)
           : getBackgroundLightColor(category),
         getTextDarkColor(category)
-        // statusClassNames[status]
       )}
       onClick={() => {
         if (isDebug) {
