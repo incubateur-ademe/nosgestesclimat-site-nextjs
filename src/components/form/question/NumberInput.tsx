e853c21b82a51911ed7df16ba2a6eb77276a9501
Benjamin Arias <bjlaa@proton.me>
2024-08-29 12:28:37 +0200

    :sparkles: Use react-number-format

--- src/components/form/question/NumberInput.tsx
+++ src/components/form/question/NumberInput.tsx
@@ -1,9 +1,7 @@
-'use client'
-
 import Trans from '@/components/translation/Trans'
-import { useLocale } from '@/hooks/useLocale'
-import { HTMLAttributes, useCallback, useEffect, useState } from 'react'
+import { forwardRef, HTMLAttributes } from 'react'
 import { DebounceInput } from 'react-debounce-input'
+import { NumericFormat } from 'react-number-format'
 import { twMerge } from 'tailwind-merge'
 
 type Props = {
@@ -14,54 +12,27 @@ type Props = {
   min?: number
   id?: string
   className?: string
+  defaultValue?: string | number | null | undefined
 }
+
+forwardRef(function DebouncedInputWithForwardRef(
+  props: React.ComponentProps<typeof DebounceInput>,
+  ref: React.ForwardedRef<HTMLInputElement>
+) {
+  return <DebounceInput type="number" {...props} inputRef={ref} />
+})
 
 export default function NumberInput({
   unit,
   value = '',
   isMissing,
   setValue,
-  min = 0,
   className,
   id,
   ...props
 }: HTMLAttributes<HTMLInputElement> & Props) {
-  const [formattedValue, setFormattedValue] = useState('')
-
-  const locale = useLocale()
-
-  const formatNumberWithSpaces = useCallback(
-    (number: number) => {
-      return new Intl.NumberFormat(locale, {
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 2,
-      }).format(number)
-    },
-    [locale]
-  )
-
-  const unformatNumber = (formattedNumber: string) => {
-    return Number(formattedNumber.replace(/\s+/g, '').replace(/,/g, '.'))
-  }
-
   const handleValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = event.target.value
-
-    if (inputValue === '') {
-      setValue(undefined)
-    } else {
-      setValue(unformatNumber(inputValue.replace(/[^0-9.-]+/g, '')))
-    }
-  }
-
-  useEffect(() => {
-    setFormattedValue(
-      formatNumberWithSpaces(Number(`${value}`.replace(/\s+/g, '')))
-    )
-  }, [value, formatNumberWithSpaces])
-
-  const handleInput = (event: React.FormEvent<HTMLInputElement>) => {
-    const inputValue = (event.target as HTMLInputElement).value
 
     if (inputValue === '') {
       setValue(undefined)
@@ -70,21 +41,26 @@ export default function NumberInput({
     }
   }
 
+  function unformatNumber(number: string) {
+    // Supprimer les séparateurs de milliers
+    return Number(number.replace(/[^0-9.-]+/g, ''))
+  }
+
   return (
     <div
       className={twMerge(`flex items-center justify-start gap-1`, className)}>
-      <DebounceInput
-        debounceTimeout={300}
+      <NumericFormat
+        value={isMissing ? '' : value}
+        placeholder={isMissing ? '0' : ''}
         className={`focus:ring-primary max-w-[8rem] rounded-xl border-2 border-primary-200 bg-white p-2 text-right transition-colors focus:border-primary-700 focus:ring-2 md:max-w-full`}
-        inputMode="numeric"
-        min={min}
-        value={isMissing ? '' : formattedValue}
-        placeholder={isMissing ? formattedValue ?? '0' : '0'}
+        customInput={DebounceInput}
+        thousandSeparator={' '}
+        allowNegative={false}
         onChange={handleValueChange}
-        onInput={handleInput}
         id={id}
         {...props}
       />
+
       {unit ? (
         <span className="whitespace-nowrap">
           &nbsp;
