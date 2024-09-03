06ddc685cc3151a0806b68da2b8a620370b778fa
Benjamin Arias <bjlaa@proton.me>
2024-09-03 16:42:15 +0200

    Update src/utils/debounce.ts
    Co-authored-by: Paul Souche <paul@bettercallpaul.fr>

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,4 +1,4 @@
-export function debounce<T extends (...args: any[]) => void>(
+export function debounce<T extends (...args: unknown[]) => void>(
   func: T,
   delay: number
 ): (...args: Parameters<T>) => void {
