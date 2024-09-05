30ab7d9af2af7e37cc317922ff5bb3143fcd43b8
Benjamin Arias <bjlaa@proton.me>
2024-09-05 15:29:19 +0200

    :recycle: Undo

--- src/components/fin/metricSlider/heading/HeadingButtons.tsx
+++ src/components/fin/metricSlider/heading/HeadingButtons.tsx
@@ -43,19 +43,20 @@ export default function HeadingButtons({ size = 'md', endPage }: Props) {
   const timeoutRef = useRef<NodeJS.Timeout | null>(null)
 
   const handleShare = async () => {
-    const shareText = t(
-      'Nos Gestes Climat : une estimation de votre empreinte carbone de consommation en 10 min\n{{sharedUrl}}',
-      { sharedUrl }
-    )
-
     if (navigator?.share && window.innerWidth < 768) {
       await navigator
         .share({
-          url: shareText,
+          url: sharedUrl,
+          title: 'Nos Gestes Climat : estimation empreinte carbone',
+          text: 'Nos Gestes Climat : une estimation de votre empreinte carbone de consommation en 10 min',
         })
         .catch((e) => console.log(e))
     } else {
       try {
+        const shareText = t(
+          'Nos Gestes Climat : une estimation de votre empreinte carbone de consommation en 10 min\n{{sharedUrl}}',
+          { sharedUrl }
+        )
         await navigator.clipboard.writeText(shareText)
 
         displaySuccessToast('Lien de partage copié dans le presse-papier !')
