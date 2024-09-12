dc1207c92ec329cab1d1d01bc3b1d1f82c8c425f
Benjamin Arias <bjlaa@proton.me>
2024-09-12 12:01:03 +0200

    :sparkles: Add water footprint block

--- src/components/organisations/orgaStatistics/StatisticsBlocks.tsx
+++ src/components/organisations/orgaStatistics/StatisticsBlocks.tsx
@@ -1,58 +1,107 @@
+'use client'
+
 import VerticalBarChart from '@/components/charts/VerticalBarChart'
 import Trans from '@/components/translation/Trans'
-import { carboneMetric } from '@/constants/metric'
+import { carboneMetric, eauMetric } from '@/constants/metric'
 import { formatCarbonFootprint } from '@/helpers/formatters/formatCarbonFootprint'
+import { formatFootprint } from '@/helpers/formatters/formatFootprint'
+import { useLocale } from '@/hooks/useLocale'
 import { Entries } from '@/publicodes-state/types'
 import { SimulationRecap } from '@/types/organisations'
+import Wave from 'react-wavify'
 import CategoryChartItem from './statisticsBlocks/CategoryChartItem'
 import ResultsSoonBanner from './statisticsBlocks/ResultsSoonBanner'
 
 // Create a mock results object with the default carbon footprints values for each category
 const mockResults = {
-  bilan: 8000,
-  transport: 3000,
-  logement: 1000,
-  alimentation: 1000,
-  divers: 500,
-  'services sociétaux': 2000,
+  carbone: {
+    bilan: 8000,
+    transport: 3000,
+    logement: 1000,
+    alimentation: 1000,
+    divers: 500,
+    'services sociétaux': 2000,
+  },
+  eau: {
+    bilan: 1000,
+    transport: 1000,
+    logement: 1000,
+    alimentation: 1000,
+    divers: 1000,
+  },
 }
 
 function formatSimulationRecaps(simulationRecaps: SimulationRecap[]) {
   const result = simulationRecaps.reduce(
     (acc, simulation) => {
       return {
-        bilan: acc.bilan + simulation.computedResults[carboneMetric].bilan,
-        transport:
-          acc.transport +
-          simulation.computedResults[carboneMetric].categories?.transport,
-        logement:
-          acc.logement +
-          simulation.computedResults[carboneMetric].categories?.logement,
-        alimentation:
-          acc.alimentation +
-          simulation.computedResults[carboneMetric].categories?.alimentation,
-        divers:
-          acc.divers +
-          simulation.computedResults[carboneMetric].categories?.divers,
-        'services sociétaux':
-          acc['services sociétaux'] +
-          simulation.computedResults[carboneMetric].categories?.[
-            'services sociétaux'
-          ],
+        carbone: {
+          bilan:
+            acc.carbone.bilan + simulation.computedResults[carboneMetric].bilan,
+          transport:
+            acc.carbone.transport +
+            simulation.computedResults[carboneMetric].categories?.transport,
+          logement:
+            acc.carbone.logement +
+            simulation.computedResults[carboneMetric].categories?.logement,
+          alimentation:
+            acc.carbone.alimentation +
+            simulation.computedResults[carboneMetric].categories?.alimentation,
+          divers:
+            acc.carbone.divers +
+            simulation.computedResults[carboneMetric].categories?.divers,
+          'services sociétaux':
+            acc.carbone['services sociétaux'] +
+            simulation.computedResults[carboneMetric].categories?.[
+              'services sociétaux'
+            ],
+        },
+        eau: {
+          bilan:
+            acc.eau.bilan + (simulation.computedResults[eauMetric]?.bilan ?? 0),
+          transport:
+            acc.eau.transport +
+            (simulation.computedResults[eauMetric]?.categories?.transport ?? 0),
+          logement:
+            acc.eau.logement +
+            (simulation.computedResults[eauMetric]?.categories?.logement ?? 0),
+          alimentation:
+            acc.eau.alimentation +
+            (simulation.computedResults[eauMetric]?.categories?.alimentation ??
+              0),
+          divers:
+            acc.eau.divers +
+            (simulation.computedResults[eauMetric]?.categories?.divers ?? 0),
+        },
       }
     },
     {
-      bilan: 0,
-      transport: 0,
-      logement: 0,
-      alimentation: 0,
-      divers: 0,
-      'services sociétaux': 0,
+      carbone: {
+        bilan: 0,
+        transport: 0,
+        logement: 0,
+        alimentation: 0,
+        divers: 0,
+        'services sociétaux': 0,
+      },
+      eau: {
+        bilan: 0,
+        transport: 0,
+        logement: 0,
+        alimentation: 0,
+        divers: 0,
+      },
     }
   )
-  Object.keys(result).forEach((key) => {
-    result[key as keyof typeof result] =
-      result[key as keyof typeof result] / simulationRecaps.length
+
+  Object.keys(result.carbone).forEach((key) => {
+    result.carbone[key as keyof typeof result.carbone] =
+      result.carbone[key as keyof typeof result.carbone] /
+      simulationRecaps.length
+  })
+  Object.keys(result.eau).forEach((key) => {
+    result.eau[key as keyof typeof result.eau] =
+      result.eau[key as keyof typeof result.eau] / simulationRecaps.length
   })
 
   return result
@@ -65,6 +114,8 @@ export default function StatisticsBlocks({
   simulationRecaps: SimulationRecap[]
   simulationRecapsWithoutExtremes: SimulationRecap[]
 }) {
+  const locale = useLocale()
+
   if (!simulationRecaps) {
     return null
   }
@@ -75,15 +126,24 @@ export default function StatisticsBlocks({
     ? mockResults
     : formatSimulationRecaps(simulationRecapsWithoutExtremes)
 
-  const { formattedValue, unit } = formatCarbonFootprint(result?.bilan, {
-    maximumFractionDigits: 1,
-  })
+  const { formattedValue, unit } = formatCarbonFootprint(
+    result?.carbone?.bilan,
+    {
+      maximumFractionDigits: 1,
+    }
+  )
+
+  const { formattedValue: formattedWaterValue, unit: waterUnit } =
+    formatFootprint(result.eau.bilan, {
+      metric: eauMetric,
+      localize: false,
+    })
 
   return (
-    <div className="items grid w-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-      <div className="rounded-xl bg-gray-100 p-8 sm:col-span-2 md:col-span-1">
+    <div className="grid w-full auto-rows-fr grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+      <div className="rounded-xl bg-gray-100 p-8">
         <p className="text-4xl font-bold text-primary-700">
-          {simulationRecaps.length}
+          {simulationRecaps?.length?.toLocaleString(locale) ?? 0}
         </p>
 
         <p className="text-xl">
@@ -95,44 +155,73 @@ export default function StatisticsBlocks({
         </p>
       </div>
 
-      <div className="relative col-span-1 grid grid-cols-1 gap-4 sm:col-span-2 sm:grid-cols-2">
-        {hasLessThan3Participants && (
-          <ResultsSoonBanner
-            hasLessThan3Participants={hasLessThan3Participants}
-          />
-        )}
+      {hasLessThan3Participants ? (
+        <ResultsSoonBanner
+          hasLessThan3Participants={hasLessThan3Participants}
+        />
+      ) : (
+        <>
+          <div className="bg-rainbow-rotation overflow-hidden rounded-xl bg-gray-100 p-8">
+            <p className="text-4xl font-bold text-primary-700">
+              {formattedValue}{' '}
+              <span className="text-base font-normal">{unit} CO2 eq</span>
+            </p>
 
-        <div className="bg-rainbow-rotation col-span-1 rounded-xl bg-gray-100 p-8">
-          <p className="text-4xl font-bold text-primary-700">
-            {formattedValue}{' '}
-            <span className="text-base font-normal">{unit} CO2 eq</span>
-          </p>
+            <p className="text-xl">
+              <Trans>Empreinte moyenne</Trans>
+            </p>
+          </div>
 
-          <p className="text-xl">
-            <Trans>Empreinte moyenne</Trans>
-          </p>
-        </div>
+          {result.eau.bilan > 0 && (
+            <div className="relative overflow-hidden rounded-xl bg-gray-100 p-8">
+              <Wave
+                fill="#5152D0"
+                className="pointer-events-none absolute bottom-0 left-0 right-0 h-full w-full rounded-b-xl"
+                options={{
+                  speed: 0.11,
+                  points: 3,
+                }}
+              />
+              <div className="relative z-10">
+                <p className="text-3xl font-bold text-white">
+                  {parseInt(formattedWaterValue ?? 0).toLocaleString(locale, {
+                    maximumFractionDigits: 1,
+                  })}{' '}
+                  <span className="text-base font-normal">{waterUnit}</span>
+                </p>
 
-        <div className="col-span-1 min-h-[212px] rounded-xl bg-gray-100 py-4">
-          <VerticalBarChart className={`mt-0 h-[calc(100%-48px)]`}>
-            {(Object.entries(result) as Entries<typeof result>)
-              .filter(([key]) => key !== 'bilan')
-              .map(([key, value], index) => (
-                <CategoryChartItem
-                  index={index}
-                  key={key}
-                  category={key}
-                  maxValue={result.bilan / 1000}
-                  value={value / 1000}
-                />
-              ))}
-          </VerticalBarChart>
+                <p className="text-xl text-white">
+                  <Trans>
+                    <strong>Empreinte eau</strong> moyenne
+                  </Trans>
+                </p>
+              </div>
+            </div>
+          )}
 
-          <h3 className="mb-4 ml-6 mt-4 text-sm">
-            <Trans>Moyenne du groupe par catégorie</Trans>
-          </h3>
-        </div>
-      </div>
+          <div className="rounded-xl bg-gray-100 py-4">
+            <VerticalBarChart className="mt-0 h-[calc(100%-48px)]">
+              {(
+                Object.entries(result.carbone) as Entries<typeof result.carbone>
+              )
+                .filter(([key]) => key !== 'bilan')
+                .map(([key, value], index) => (
+                  <CategoryChartItem
+                    index={index}
+                    key={key}
+                    category={key}
+                    maxValue={result.carbone.bilan / 1000}
+                    value={value / 1000}
+                  />
+                ))}
+            </VerticalBarChart>
+
+            <h3 className="mb-4 ml-6 mt-4 text-sm">
+              <Trans>Moyenne du groupe par catégorie</Trans>
+            </h3>
+          </div>
+        </>
+      )}
     </div>
   )
 }
