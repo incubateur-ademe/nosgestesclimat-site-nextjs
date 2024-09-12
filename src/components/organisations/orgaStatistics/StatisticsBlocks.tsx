80af1165768b459fb37f0ee49d79d7c6120f5c97
Benjamin Arias <bjlaa@proton.me>
2024-09-12 12:04:45 +0200

    :recycle: Move function into a helper file

--- src/components/organisations/orgaStatistics/StatisticsBlocks.tsx
+++ src/components/organisations/orgaStatistics/StatisticsBlocks.tsx
@@ -2,9 +2,10 @@
 
 import VerticalBarChart from '@/components/charts/VerticalBarChart'
 import Trans from '@/components/translation/Trans'
-import { carboneMetric, eauMetric } from '@/constants/metric'
+import { eauMetric } from '@/constants/metric'
 import { formatCarbonFootprint } from '@/helpers/formatters/formatCarbonFootprint'
 import { formatFootprint } from '@/helpers/formatters/formatFootprint'
+import { getSimulationRecapAggregatedResult } from '@/helpers/organisations/getSimulationRecapAggregatedResult'
 import { useLocale } from '@/hooks/useLocale'
 import { Entries } from '@/publicodes-state/types'
 import { SimulationRecap } from '@/types/organisations'
@@ -31,82 +32,6 @@ const mockResults = {
   },
 }
 
-function formatSimulationRecaps(simulationRecaps: SimulationRecap[]) {
-  const result = simulationRecaps.reduce(
-    (acc, simulation) => {
-      return {
-        carbone: {
-          bilan:
-            acc.carbone.bilan + simulation.computedResults[carboneMetric].bilan,
-          transport:
-            acc.carbone.transport +
-            simulation.computedResults[carboneMetric].categories?.transport,
-          logement:
-            acc.carbone.logement +
-            simulation.computedResults[carboneMetric].categories?.logement,
-          alimentation:
-            acc.carbone.alimentation +
-            simulation.computedResults[carboneMetric].categories?.alimentation,
-          divers:
-            acc.carbone.divers +
-            simulation.computedResults[carboneMetric].categories?.divers,
-          'services sociétaux':
-            acc.carbone['services sociétaux'] +
-            simulation.computedResults[carboneMetric].categories?.[
-              'services sociétaux'
-            ],
-        },
-        eau: {
-          bilan:
-            acc.eau.bilan + (simulation.computedResults[eauMetric]?.bilan ?? 0),
-          transport:
-            acc.eau.transport +
-            (simulation.computedResults[eauMetric]?.categories?.transport ?? 0),
-          logement:
-            acc.eau.logement +
-            (simulation.computedResults[eauMetric]?.categories?.logement ?? 0),
-          alimentation:
-            acc.eau.alimentation +
-            (simulation.computedResults[eauMetric]?.categories?.alimentation ??
-              0),
-          divers:
-            acc.eau.divers +
-            (simulation.computedResults[eauMetric]?.categories?.divers ?? 0),
-        },
-      }
-    },
-    {
-      carbone: {
-        bilan: 0,
-        transport: 0,
-        logement: 0,
-        alimentation: 0,
-        divers: 0,
-        'services sociétaux': 0,
-      },
-      eau: {
-        bilan: 0,
-        transport: 0,
-        logement: 0,
-        alimentation: 0,
-        divers: 0,
-      },
-    }
-  )
-
-  Object.keys(result.carbone).forEach((key) => {
-    result.carbone[key as keyof typeof result.carbone] =
-      result.carbone[key as keyof typeof result.carbone] /
-      simulationRecaps.length
-  })
-  Object.keys(result.eau).forEach((key) => {
-    result.eau[key as keyof typeof result.eau] =
-      result.eau[key as keyof typeof result.eau] / simulationRecaps.length
-  })
-
-  return result
-}
-
 export default function StatisticsBlocks({
   simulationRecaps,
   simulationRecapsWithoutExtremes,
@@ -124,7 +49,7 @@ export default function StatisticsBlocks({
 
   const result = hasLessThan3Participants
     ? mockResults
-    : formatSimulationRecaps(simulationRecapsWithoutExtremes)
+    : getSimulationRecapAggregatedResult(simulationRecapsWithoutExtremes)
 
   const { formattedValue, unit } = formatCarbonFootprint(
     result?.carbone?.bilan,
