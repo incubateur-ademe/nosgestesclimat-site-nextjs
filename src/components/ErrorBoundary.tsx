adefb27cbc2de694aa2713b4f080fe03e6c3cfa4
Benjamin Arias <bjlaa@proton.me>
2024-09-18 11:45:54 +0200

    :sparkles: Re-add an error boundary (#702)

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,42 @@
+import { captureException } from '@sentry/nextjs'
+import React, { ErrorInfo, ReactNode } from 'react'
+import ErrorModal from './ErrorModal'
+
+interface ErrorBoundaryProps {
+  children: ReactNode
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean
+  error: Error | null
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props)
+    this.state = { hasError: false, error: null }
+  }
+
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    return { hasError: true, error }
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('Uncaught error:', error, errorInfo)
+    // Send error to Sentry
+    captureException(error)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <ErrorModal error={this.state.error} />
+    }
+
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
