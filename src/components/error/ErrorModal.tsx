8242834c9198024fc67cdb10d2b2be8a35437a8b
Benjamin Arias <bjlaa@proton.me>
2024-09-18 16:34:35 +0200

    :recycle: Remove dead code :skull:

--- src/components/error/ErrorModal.tsx
+++ src/components/error/ErrorModal.tsx
@@ -1,18 +1,12 @@
 'use client'
 
 import Modal from '@/design-system/modals/Modal'
-import { ReactNode } from 'react'
 import ErrorContent from './ErrorContent'
 
-export default function ErrorModal({
-  children,
-}: {
-  children?: ReactNode
-  error: Error | null
-}) {
+export default function ErrorModal() {
   return (
     <Modal isOpen={true} closeModal={() => {}} hasAbortButton={false}>
-      {children ?? <ErrorContent />}
+      {<ErrorContent />}
     </Modal>
   )
 }
