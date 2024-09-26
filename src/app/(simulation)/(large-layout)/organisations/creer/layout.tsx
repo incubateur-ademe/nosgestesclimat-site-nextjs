6e8a79535728f0124c1aa4509e907e2b630920f7
Benjamin Arias <bjlaa@proton.me>
2024-09-26 12:00:56 +0200

    :bug: Fix canonical URLs + add missing language alternate

--- src/app/(simulation)/(large-layout)/organisations/creer/layout.tsx
+++ src/app/(simulation)/(large-layout)/organisations/creer/layout.tsx
@@ -11,7 +11,7 @@ export async function generateMetadata() {
       'Accédez à des services sur mesure pour sensibiliser vos partenaires au sein de votre organisation.'
     ),
     alternates: {
-      canonical: '/',
+      canonical: '/organisations/creer',
     },
   })
 }
