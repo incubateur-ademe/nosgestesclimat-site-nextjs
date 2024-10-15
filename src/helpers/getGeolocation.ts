5bfb97179467c3825955c4bbc3e5cfa67330b5bc
Florian <hi@florianpanchout.com>
2024-10-15 15:11:57 +0200

    feat: send france if no region

--- src/helpers/getGeolocation.ts
+++ src/helpers/getGeolocation.ts
@@ -1,5 +1,5 @@
 export async function getGeolocation(): Promise<string> {
-  const region = await fetch(
+  const regionCode = await fetch(
     `${
       process.env.VERCEL_ENV === 'preview' ||
       process.env.VERCEL_ENV === 'production'
@@ -8,7 +8,7 @@ export async function getGeolocation(): Promise<string> {
     }://${process.env.VERCEL_URL || 'localhost:3000'}/api/geolocation`
   )
     .then((res) => res.json())
-    .then((res) => res.country.code)
+    .then((res) => res.country?.code ?? 'FR')
 
-  return region
+  return regionCode
 }
