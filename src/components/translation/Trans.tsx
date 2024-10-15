082f055351ba53d407852853c83102660d589d3e
Florian <hi@florianpanchout.com>
2024-10-15 14:14:08 +0200

    feat: add back trans

--- src/components/translation/Trans.tsx
+++ src/components/translation/Trans.tsx
@@ -1,7 +1,15 @@
 import { TransPropsWithInterpolation } from '@/types/translation'
+import TransClient from './trans/TransClient'
+import TransServer from './trans/TransServer'
 
 export default function Trans({
   children,
+  i18nKey,
 }: TransPropsWithInterpolation): JSX.Element {
-  return <>{children}</>
+  const componentType = typeof window === 'undefined' ? 'server' : 'client'
+
+  if (componentType === 'server') {
+    return <TransServer i18nKey={i18nKey}>{children}</TransServer>
+  }
+  return <TransClient i18nKey={i18nKey}>{children}</TransClient>
 }
