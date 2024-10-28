49a771405d679dcedbb9271313c2d86106522833
Benjamin Arias <benjamin.j.arias@gmail.com>
2024-10-28 10:18:10 +0100

    :recycle: Switch back to previous code implementation

--- src/helpers/getGeolocation.ts
+++ src/helpers/getGeolocation.ts
@@ -2,21 +2,16 @@
 
 import { RegionFromGeolocation } from '@/publicodes-state/types'
 import axios from 'axios'
-import { headers } from 'next/headers'
 
 export async function getGeolocation(): Promise<RegionFromGeolocation> {
-  const headersList = headers()
-
-  // This function is called by the server, so we need a specific way to get the current URL
-  let currentUrl = headersList.get('x-url') || ''
-
-  if (!currentUrl) {
-    const host = headersList.get('host') || 'localhost:3000'
-    const proto = headersList.get('x-forwarded-proto') || 'http'
-    currentUrl = `${proto}://${host}`
-  }
-
   return await axios
-    .get(`${currentUrl}/api/geolocation`)
+    .get(
+      `${
+        process.env.VERCEL_ENV === 'preview' ||
+        process.env.VERCEL_ENV === 'production'
+          ? 'https'
+          : 'http'
+      }://${process.env.VERCEL_URL || 'localhost:3000'}/api/geolocation`
+    )
     .then((res) => res.data)
 }
