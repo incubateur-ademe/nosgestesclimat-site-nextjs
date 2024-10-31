90200425ef31878b9904540171d13c61a5291750
Benjamin Arias <benjamin.j.arias@gmail.com>
2024-10-31 16:34:16 +0100

    :sparkles: Add floating elements

--- src/app/_components/InteractiveIllustration.tsx
+++ src/app/_components/InteractiveIllustration.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Trans from '@/components/translation/Trans'
 import Image from 'next/image'
 import PulsatingDot from './interactiveIllustration/PulsatingDot'
 
@@ -14,16 +15,36 @@ export default function InteractiveIllustration() {
       />
 
       {/* Computer */}
-      <PulsatingDot className="bottom-[15%] left-[50%]" />
+      <PulsatingDot
+        className="bottom-[15%] left-[50%]"
+        itemTitle={<Trans>Ordinateur</Trans>}
+        carbonScore={4}
+        waterScore={4}
+      />
 
       {/* Shirt */}
-      <PulsatingDot className="right-[30%] top-[40%]" />
+      <PulsatingDot
+        className="right-[30%] top-[40%]"
+        itemTitle={<Trans>Chemisier</Trans>}
+        carbonScore={3}
+        waterScore={5}
+        floatingInfoOrientation="left"
+      />
 
       {/* Coffee */}
-      <PulsatingDot className="bottom-[35%] left-[12%]" />
+      <PulsatingDot
+        className="bottom-[35%] left-[12%]"
+        itemTitle={<Trans>Café</Trans>}
+        carbonScore={4}
+        waterScore={3}
+      />
 
       {/* Car */}
-      <PulsatingDot className="bottom-[10%] left-[10%]" />
+      <PulsatingDot
+        className="bottom-[10%] left-[10%]"
+        itemTitle={<Trans>Voiture</Trans>}
+        carbonScore={5}
+      />
     </div>
   )
 }
--- src/app/_components/interactiveIllustration/PulsatingDot.tsx
+++ src/app/_components/interactiveIllustration/PulsatingDot.tsx
@@ -1,16 +1,57 @@
 'use client'
-
+import type { ReactNode } from 'react'
+import { useState } from 'react'
 import { twMerge } from 'tailwind-merge'
+import FloatingInfo from './pulsatingDot/FloatingInfo'
 
-export default function PulsatingDot({ className }: { className?: string }) {
+export default function PulsatingDot({
+  className,
+  carbonScore,
+  waterScore,
+  itemTitle,
+  floatingInfoOrientation,
+}: {
+  className?: string
+  carbonScore?: number
+  waterScore?: number
+  itemTitle: ReactNode
+  floatingInfoOrientation?: 'left' | 'right'
+}) {
+  const [isHovered, setIsHovered] = useState(false)
   return (
-    <div
-      className={twMerge(
-        'group absolute h-6 w-6 cursor-pointer rounded-full shadow-lg',
-        'animate-pulse-bg',
-        className
-      )}>
-      <div className="absolute inset-0 scale-[0.15] rounded-full bg-secondary-600 transition-all duration-300 group-hover:scale-[0.7]" />
+    <div className={twMerge('absolute', className)}>
+      {isHovered && (
+        <FloatingInfo
+          title={itemTitle}
+          carbonScore={carbonScore}
+          waterScore={waterScore}
+          orientation={floatingInfoOrientation}
+          className="left-0 top-0"
+        />
+      )}
+
+      <div
+        className={twMerge(
+          'group absolute h-6 w-6 cursor-pointer rounded-full shadow-lg',
+          'animate-pulse-bg'
+        )}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        tabIndex={0}
+        role="button"
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' || e.key === ' ') {
+            setIsHovered(true)
+          }
+        }}
+        onKeyUp={(e) => {
+          if (e.key === 'Enter' || e.key === ' ') {
+            setIsHovered(false)
+          }
+        }}
+        aria-label="Toggle information">
+        <div className="absolute inset-0 scale-[0.15] rounded-full bg-secondary-600 transition-all duration-300 group-hover:scale-[0.7]" />
+      </div>
     </div>
   )
 }
