517e75fb50ab2f8e95dad5add09147dc9518ed4e
Benjamin Arias <bjlaa@proton.me>
2024-12-10 18:23:51 +0100

    🐛 Fix Iframe locale reloading bug (#784)

--- src/helpers/navigation/simulateurPages.ts
+++ src/helpers/navigation/simulateurPages.ts
@@ -2,14 +2,16 @@ import type { DottedName } from '@incubateur-ademe/nosgestesclimat'
 
 type Props = {
   question?: DottedName
+  locale?: string
 }
-export const getLinkToSimulateur = ({ question }: Props = {}) => {
+export const getLinkToSimulateur = ({ question, locale }: Props = {}) => {
+  const basePath = locale ? `/${locale}` : ''
   // If no question is provided, we return
   if (!question) {
-    return '/simulateur/bilan'
+    return `${basePath}/simulateur/bilan`
   }
   //
-  return `/simulateur/bilan?question=${question
+  return `${basePath}/simulateur/bilan?question=${question
     .replaceAll(' . ', '.')
     .replaceAll(' ', '_')}`
 }
