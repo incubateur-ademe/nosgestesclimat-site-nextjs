e2e9a0ecd62f2473883b1113825a6be392ce9893
Benjamin Arias <benjamin.j.arias@gmail.com>
2024-12-18 14:48:54 +0100

    :bug: Debugging

--- src/helpers/blog/fetchArticlePageContent.ts
+++ src/helpers/blog/fetchArticlePageContent.ts
@@ -2,7 +2,8 @@ import { cmsClient } from '@/adapters/cms'
 import type { ArticleType } from '@/types/blog'
 import axios from 'axios'
 
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction =
+  process.env.NODE_ENV === 'production' && process.env.VERCEL_ENV !== 'preview'
 
 export async function fetchArticlePageContent({
   articleSlug,
