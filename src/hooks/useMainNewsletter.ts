b6b825e6318908933b25bb5586a4697e2443f3b4
Paul Souche <paul@bettercallpaul.fr>
2025-01-13 09:29:08 +0100

    :recycle: get main newsletters subscribers

---
+++ src/hooks/useMainNewsletter.ts
@@ -0,0 +1,21 @@
+import { keepPreviousData, useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { LIST_MAIN_NEWSLETTER } from '../constants/brevo'
+import { NEWSLETTER_URL } from '../constants/urls'
+
+export type Newsletter = {
+  id: number
+  name: string
+  totalSubscribers: number
+}
+
+export function useMainNewsletter() {
+  return useQuery({
+    queryKey: ['mainNewsletter'],
+    queryFn: () =>
+      axios
+        .get<Newsletter>(`${NEWSLETTER_URL}/${LIST_MAIN_NEWSLETTER}`)
+        .then((res) => res.data),
+    placeholderData: keepPreviousData,
+  })
+}
