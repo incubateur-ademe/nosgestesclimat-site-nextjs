8aed47aa068a023dfde5c31f17b6c2a9a5451d3e
Benjamin Arias <bjlaa@proton.me>
2025-01-13 15:21:15 +0100

    🔧 Add redirections from old blog to new one (#799)

--- config/redirects.js
+++ config/redirects.js
@@ -107,6 +107,112 @@ const redirects = [
     destination: '/nos-relais',
     permanent: true,
   },
+  // Blog v1 > Blog v2
+  {
+    source: '/blog/empreinte-eau-pourquoi-comment',
+    destination:
+      '/blog/actualites-et-fonctionnalites/empreinte-eau-pourquoi-comment',
+    permanent: true,
+  },
+  {
+    source: 'blog/fonctionnalite-animateurs-ateliers-sensibilisation',
+    destination:
+      'blog/actualites-et-fonctionnalites/fonctionnalite-animateurs-ateliers-sensibilisation',
+    permanent: true,
+  },
+  {
+    source: '/blog/nouveau-parcours-organisations',
+    destination:
+      '/blog/actualites-et-fonctionnalites/nouveau-parcours-organisations',
+    permanent: true,
+  },
+  {
+    source: '/blog/challenge-tes-amis',
+    destination: '/blog/actualites-et-fonctionnalites/challenge-tes-amis',
+    permanent: true,
+  },
+  {
+    source: '/blog/journee-mondial-environnement',
+    destination:
+      '/blog/actualites-et-fonctionnalites/journee-mondial-environnement',
+    permanent: true,
+  },
+  {
+    source: '/blog/campus',
+    destination: '/blog/actualites-et-fonctionnalites/campus',
+    permanent: true,
+  },
+  {
+    source: '/blog/historique',
+    destination: '/blog/actualites-et-fonctionnalites/historique',
+    permanent: true,
+  },
+  {
+    source: '/blog/transports-fuir-transports-cherir',
+    destination: 'blog/mobilites/transports-fuir-transports-cherir',
+    permanent: true,
+  },
+  {
+    source: '/blog/alternatives-voiture-individuelle',
+    destination: '/blog/mobilites/alternatives-voiture-individuelle',
+    permanent: true,
+  },
+  {
+    source: '/blog/affichage-ges-transport',
+    destination: '/blog/mobilites/affichage-ges-transport',
+    permanent: true,
+  },
+  {
+    source: '/blog/impact-mobilite',
+    destination: '/blog/mobilites/impact-mobilite',
+    permanent: true,
+  },
+  {
+    source: '/blog/lexique-eau-tout-comprendre',
+    destination: '/blog/environnement/lexique-eau-tout-comprendre',
+    permanent: true,
+  },
+  {
+    source: '/blog/effet-rebond',
+    destination: '/blog/environnement/effet-rebond',
+    permanent: true,
+  },
+  {
+    source: '/blog/carbone-empreinte-parmi-autres',
+    destination: '/blog/environnement/carbone-empreinte-parmi-autres',
+    permanent: true,
+  },
+  {
+    source: '/blog/maladaptation',
+    destination: '/blog/environnement/maladaptation',
+    permanent: true,
+  },
+  {
+    source: '/blog/budget',
+    destination: '/blog/environnement/budget',
+    permanent: true,
+  },
+  {
+    source: '/empreinte-climat',
+    destination: '/blog/environnement/definition-empreinte-carbone',
+    permanent: true,
+  },
+  {
+    source: '/blog/reflexes-textile-econome-empreinte-eau',
+    destination: '/blog/consommation/reflexes-textile-econome-empreinte-eau',
+    permanent: true,
+  },
+  {
+    source: '/blog/demenagement-decarbonation',
+    destination: '/blog/logement/demenagement-decarbonation',
+    permanent: true,
+  },
+  {
+    source: '/blog/8-facons-ameliorer-empreinte-de-mon-assiette',
+    destination:
+      '/blog/alimentation/8-facons-ameliorer-empreinte-de-mon-assiette',
+    permanent: true,
+  },
 ]
 
 module.exports = redirects
