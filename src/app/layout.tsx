20c22c07111e815cf3c999a29de8d52f9298d6dc
Benjamin Arias <benjamin.j.arias@gmail.com>
2025-01-13 13:33:21 +0100

    🔧 Add config for pre-prod Matomo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,25 +73,50 @@ export default async function RootLayout({ children }: PropsWithChildren) {
 
           <meta name="theme-color" content="#4949ba" />
 
-          {process.env.NEXT_PUBLIC_MATOMO_ID === '1' && (
-            <Script id="matomo">
-              {`
-            var _paq = window._paq = window._paq || [];
-            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-            _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-            _paq.push(["setCookieDomain", "*.nosgestesclimat.fr"]);
-            _paq.push(['enableLinkTracking']);
-            (function() {
-              var u="https://stats.beta.gouv.fr/";
-              _paq.push(['setTrackerUrl', u+'matomo.php']);
-              _paq.push(['setSiteId', '20']);
-              _paq.push(['addTracker', 'https://stats.data.gouv.fr/matomo.php', '153'])
-              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-            })();
-          `}
-            </Script>
-          )}
+          {
+            // Matomo Prod
+            process.env.NEXT_PUBLIC_MATOMO_ID === '1' && (
+              <Script id="matomo">
+                {`
+                  var _paq = window._paq = window._paq || [];
+                  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+                  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+                  _paq.push(["setCookieDomain", "*.nosgestesclimat.fr"]);
+                  _paq.push(['enableLinkTracking']);
+                  (function() {
+                    var u="https://stats.beta.gouv.fr/";
+                    _paq.push(['setTrackerUrl', u+'matomo.php']);
+                    _paq.push(['setSiteId', '20']);
+                    _paq.push(['addTracker', 'https://stats.data.gouv.fr/matomo.php', '153'])
+                    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+                  })();
+                `}
+              </Script>
+            )
+          }
+
+          {
+            // Matomo Pre-prod
+            process.env.NEXT_PUBLIC_MATOMO_ID === '2' && (
+              <Script id="matomo">
+                {`
+                var _paq = window._paq = window._paq || [];
+                /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+                _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+                _paq.push(["setCookieDomain", "*.nosgestesclimat.fr"]);
+                _paq.push(['enableLinkTracking']);
+                (function() {
+                  var u="https://stats.beta.gouv.fr/";
+                  _paq.push(['setTrackerUrl', u+'matomo.php']);
+                  _paq.push(['setSiteId', '79']);
+                  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+                })();
+              `}
+              </Script>
+            )
+          }
         </head>
 
         <body
