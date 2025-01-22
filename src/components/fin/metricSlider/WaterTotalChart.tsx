1c58a307f07b908e1b6e7edae8b961c1dbd98161
Benjamin Arias <benjamin.j.arias@gmail.com>
2025-01-22 15:04:27 +0100

    ♻️ Re-add text + translate

--- src/components/fin/metricSlider/WaterTotalChart.tsx
+++ src/components/fin/metricSlider/WaterTotalChart.tsx
@@ -1,3 +1,4 @@
+import Trans from '@/components/translation/Trans'
 import { motion } from 'framer-motion'
 import { twMerge } from 'tailwind-merge'
 import WaterTotalNumber from './waterTotalChart/WaterTotalNumber'
@@ -18,6 +19,16 @@ export default function WaterTotalChart({ total, isSmall }: Props) {
         isSmall && 'mt-0.5 lg:mt-0.5'
       )}>
       <WaterTotalNumber total={total} isSmall={isSmall} />
+      <p className="relative mx-2 mb-0 mt-2 hidden text-sm md:block">
+        <Trans>Ce chiffre vous semble impressionnant ?</Trans>
+      </p>
+      <p className="relative mx-2 mb-0 mt-2 hidden text-sm md:block">
+        <Trans>
+          C'est pourtant bien l'eau qui sert à produire ce que vous consommez :
+          votre empreinte eau, c'est l'impact de votre mode de vie sur les
+          cycles naturels de l'eau.
+        </Trans>
+      </p>
       <WaveContent />
     </motion.div>
   )
