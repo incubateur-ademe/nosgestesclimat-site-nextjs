13d83fe0e0ef904329c5c3607944b90170ac68f0
Benjamin Arias <benjamin.j.arias@gmail.com>
2025-01-29 22:46:11 +0100

    ♻️ Test something else

--- next.config.js
+++ next.config.js
@@ -109,11 +109,11 @@ const sentryConfig = {
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
 
-  // // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // // See the following for more information:
-  // // https://docs.sentry.io/product/crons/
-  // // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  // automaticVercelMonitors: true,
 }
 
 module.exports =
