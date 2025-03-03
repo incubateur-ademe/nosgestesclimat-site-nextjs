eb3fa39dce1daad98bf545b8f54ee80e78d196af
Benjamin Arias <benjamin.j.arias@gmail.com>
2025-03-03 14:17:56 +0100

    ♻️ Fix typing issue

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -3,27 +3,27 @@
 import type { MDXComponents } from 'mdx/types'
 
 import ButtonLink from '@/design-system/inputs/ButtonLink'
-import Title from '@/design-system/layout/Title'
+import Title, { type TitleProps } from '@/design-system/layout/Title'
 import Image from 'next/image'
 
 // This file is required to use MDX in `app` directory.
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     // Allows customizing built-in components, e.g. to add styling.
-    h1: (props: any) => <Title {...props} />,
+    h1: (props: TitleProps) => <Title {...props} />,
     ButtonLink: (props: any) => (
       <div className="text-center">
         <ButtonLink href={props.href}>{props.children}</ButtonLink>
       </div>
     ),
-    img: (props: any) => (
+    img: (props) => (
       <Image
         sizes="100vw"
         width={props.width ?? 900}
         height={props.height ?? 500}
         style={{ width: '100%', height: 'auto' }}
         alt={(props.alt as string) ?? ''}
-        {...(props as any)}
+        {...props}
       />
     ),
     ...components,
