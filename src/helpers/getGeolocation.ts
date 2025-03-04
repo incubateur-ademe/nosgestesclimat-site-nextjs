684257aa2df86c85b95f4cbd3575341fac12f20d
Paul Souche <paul@bettercallpaul.fr>
2025-03-04 10:31:22 +0100

    :adhesive_bandage: geolocation call

--- src/helpers/getGeolocation.ts
+++ src/helpers/getGeolocation.ts
@@ -7,11 +7,8 @@ export async function getGeolocation(): Promise<RegionFromGeolocation> {
   return await axios
     .get(
       `${
-        process.env.VERCEL_ENV === 'preview' ||
-        process.env.VERCEL_ENV === 'production'
-          ? 'https'
-          : 'http'
-      }://${process.env.VERCEL_URL || 'localhost:3000'}/api/geolocation`
+        process.env.NEXT_URL ? process.env.NEXT_URL : 'http://localhost:3000'
+      }/api/geolocation`
     )
     .then((res) => res.data)
 }
