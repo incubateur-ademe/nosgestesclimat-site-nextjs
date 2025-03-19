7fe52ece3de66f79e51239147028b00c8c82e50b
Benjamin Arias <benjamin.j.arias@gmail.com>
2025-03-19 16:07:04 +0100

    Update Banner.tsx

--- src/design-system/cms/Banner.tsx
+++ src/design-system/cms/Banner.tsx
@@ -15,12 +15,12 @@ export default function Banner() {
   if (!banner) return null
 
   return (
-    <div className="inline-flex w-full items-center justify-center bg-primary-600 px-4 py-2 text-sm text-white md:h-12">
+    <div className="inline-flex w-full items-center justify-center bg-primary-700 px-4 py-2 text-sm text-white md:h-12">
       <p className="mb-0 !inline">
         {banner.text}{' '}
         <ButtonLink
           size="sm"
-          className="!inline-flex bg-white px-2 py-1 text-primary-800 transition-colors duration-300 hover:bg-primary-100 hover:text-primary-800"
+          className="!inline-flex border-primary-100 bg-white px-2 py-1 text-primary-800 transition-colors duration-300 hover:border-primary-200 hover:bg-primary-100 hover:text-primary-800"
           href={banner.link.url}>
           {banner.link.label}
         </ButtonLink>
