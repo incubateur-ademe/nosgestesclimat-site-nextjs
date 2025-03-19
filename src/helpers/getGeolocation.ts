36d1ec1531216eacebce8d0c6b16c600393f6a37
Julien Parmentier <julien.parmentier@pathtech.fr>
2025-03-19 11:14:03 +0100

    fix: add missing full geo URL

--- src/helpers/getGeolocation.ts
+++ src/helpers/getGeolocation.ts
@@ -8,7 +8,7 @@ export async function getGeolocation(): Promise<RegionFromGeolocation> {
     .get(
       (process.env.NODE_ENV === 'production') ?
       `https://${process.env.NEXT_PUBLIC_SERVER_URL}/api/geolocation` :
-      'http://localhost:3000'
+      'http://localhost:3000/api/geolocation'
     )
     .then((res) => res.data)
 }
