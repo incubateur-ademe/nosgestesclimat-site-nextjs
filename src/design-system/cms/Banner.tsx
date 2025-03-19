a0ace6c29782c08065a1df1ba520720eeed01a36
Benjamin Arias <benjamin.j.arias@gmail.com>
2025-03-19 17:34:17 +0100

    ✨ Track link click

--- src/design-system/cms/Banner.tsx
+++ src/design-system/cms/Banner.tsx
@@ -1,5 +1,5 @@
 import { getBanner } from '@/helpers/api/getBanner'
-import ButtonLink from '../inputs/ButtonLink'
+import BannerLink from './banner/BannerLink'
 
 export default async function Banner() {
   const banner = await getBanner()
@@ -9,12 +9,7 @@ export default async function Banner() {
     <div className="inline-flex w-full items-center justify-center bg-primary-700 px-4 py-2 text-sm text-white md:h-12">
       <p className="mb-0 !inline">
         {banner.text}{' '}
-        <ButtonLink
-          size="sm"
-          className="!inline-flex border-primary-100 bg-white px-2 py-1 text-primary-800 transition-colors duration-300 hover:border-primary-200 hover:bg-primary-100 hover:text-primary-800"
-          href={banner.link.URL}>
-          {banner.link.label}
-        </ButtonLink>
+        <BannerLink href={banner.link.URL} label={banner.link.label} />
       </p>
     </div>
   )
