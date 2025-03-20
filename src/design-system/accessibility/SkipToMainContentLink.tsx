a120ab363923f984ad68763332308fee7037e747
Benjamin Arias <benjamin.j.arias@gmail.com>
2025-03-20 11:02:29 +0100

    Update SkipToMainContentLink.tsx

--- src/design-system/accessibility/SkipToMainContentLink.tsx
+++ src/design-system/accessibility/SkipToMainContentLink.tsx
@@ -5,7 +5,7 @@ export default function SkipToMainContentLink({ locale }: { locale: string }) {
   return (
     <Link
       href="#main-content"
-      className="sr-only focus:static focus:h-auto focus:w-auto focus:border-2 focus:border-primary-700">
+      className="sr-only bg-white focus:relative focus:left-0 focus:top-0 focus:z-[100000] focus:h-auto focus:w-auto focus:border-2 focus:border-primary-700">
       <TransServer locale={locale}>Passer au contenu principal</TransServer>
     </Link>
   )
