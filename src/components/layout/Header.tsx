e87db0bdd9ce4e0a81fe2efb98b1d25a33013b2c
Benjamin Arias <benjamin.j.arias@gmail.com>
2025-03-25 16:18:28 +0100

    ♻️ Fix menu focus

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import checkIfIsMobile from 'is-mobile'
 import HeaderDesktop from './header/HeaderDesktop'
 import HeaderMobile from './header/HeaderMobile'
 
@@ -7,6 +8,7 @@ type Props = {
   isSticky?: boolean
 }
 export default function Header({ isSticky = true }: Props) {
+  const isMobile = checkIfIsMobile()
   return (
     <>
       <HeaderMobile isSticky={isSticky} />
