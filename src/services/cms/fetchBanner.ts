fb056d2909264b41c1e9fbdc02416d4840b8e16f
Benjamin Arias <benjamin.j.arias@gmail.com>
2025-03-27 10:16:05 +0100

    ♻️ Format file

--- src/services/cms/fetchBanner.ts
+++ src/services/cms/fetchBanner.ts
@@ -18,8 +18,12 @@ export async function fetchBanner(locale: Locale): Promise<BannerType | null> {
           i18nConfig.locales[1],
       sort: 'startDate:desc',
       // Get the banner for the current date ; the date needs to be between the start and end date
-      'filters[$and][0][startDate][$lte]': dayjs(new Date()).endOf('day').toISOString(),
-      'filters[$and][1][endDate][$gte]': dayjs(new Date()).startOf('day').toISOString(),
+      'filters[$and][0][startDate][$lte]': dayjs(new Date())
+        .endOf('day')
+        .toISOString(),
+      'filters[$and][1][endDate][$gte]': dayjs(new Date())
+        .startOf('day')
+        .toISOString(),
       ...(isProduction ? { status: 'published' } : { status: 'draft' }),
       populate: '*',
     })
