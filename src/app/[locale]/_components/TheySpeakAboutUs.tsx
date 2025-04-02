224444bdb098fbcd5bed1cb744a2f164fe42f362
Benjamin Arias <benjamin.j.arias@gmail.com>
2025-04-02 10:22:54 +0200

    ✨ Update partners component

--- src/app/[locale]/_components/TheySpeakAboutUs.tsx
+++ src/app/[locale]/_components/TheySpeakAboutUs.tsx
@@ -1,5 +1,6 @@
 import Background from '@/components/landing-pages/Background'
-import Trans from '@/components/translation/trans/TransServer'
+import TransServer from '@/components/translation/trans/TransServer'
+import ButtonLink from '@/design-system/inputs/ButtonLink'
 import Image from 'next/image'
 
 export default function TheySpeakAboutUs({ locale }: { locale: string }) {
@@ -16,11 +17,34 @@ export default function TheySpeakAboutUs({ locale }: { locale: string }) {
       />
 
       <div className="relative flex flex-col items-center gap-10 md:mx-auto md:max-w-5xl">
-        <h2 className="text-center text-2xl md:text-3xl">
-          <Trans locale={locale}>Ils parlent de nous</Trans>
+        <h2 className="text-center text-xl md:text-2xl">
+          <TransServer locale={locale}>
+            Plusieurs milliers d’organisations nous font confiance pour
+            sensibiliser efficacement
+          </TransServer>
         </h2>
 
         <ul className="flex w-full flex-row flex-wrap items-center justify-center gap-7 md:justify-between">
+          <li>
+            <Image
+              src="/images/ambassadeurs/sncf-voyageurs.png"
+              alt="SNCF Voyageurs"
+              className="h-auto w-24 md:w-36"
+              width={150}
+              height={150}
+            />
+          </li>
+
+          <li>
+            <Image
+              src="/images/ambassadeurs/leboncoin.png"
+              alt="Leboncoin"
+              className="h-auto w-24 md:w-36"
+              width={150}
+              height={150}
+            />
+          </li>
+
           <li>
             <Image
               src="/images/ambassadeurs/france_info.png"
@@ -43,44 +67,30 @@ export default function TheySpeakAboutUs({ locale }: { locale: string }) {
 
           <li>
             <Image
-              src="/images/ambassadeurs/bon-pote-no-background.svg"
-              alt="Bon Pote"
-              className="h-auto w-24 md:w-36"
-              width={140}
+              src="/images/ambassadeurs/strasbourg.jpg"
+              alt="Strasbourg Métropole"
+              className="h-auto w-24 md:w-40"
+              width={100}
               height={100}
             />
           </li>
 
           <li>
             <Image
-              src="/images/ambassadeurs/bfntv.png"
-              alt="BFM TV"
-              className="h-auto w-12 md:w-20"
-              width={80}
-              height={100}
-            />
-          </li>
-
-          <li>
-            <Image
-              src="/images/ambassadeurs/vert_rectangulaire.png"
-              alt="Vert Media"
-              className="h-auto w-20 md:w-32"
-              width={120}
-              height={100}
-            />
-          </li>
-
-          <li>
-            <Image
-              src="/images/ambassadeurs/challenges.png"
-              alt="Challenges"
-              className="h-auto w-28 md:w-40"
-              width={160}
+              src="/images/ambassadeurs/nantes metropole.jpg"
+              alt="Nantes Métropole"
+              className="h-auto w-16 md:w-24"
+              width={100}
               height={100}
             />
           </li>
         </ul>
+
+        <div className="flex justify-center">
+          <ButtonLink color="secondary" size="xl" href="/nos-relais">
+            <TransServer locale={locale}>Rejoignez-les</TransServer>
+          </ButtonLink>
+        </div>
       </div>
     </div>
   )
