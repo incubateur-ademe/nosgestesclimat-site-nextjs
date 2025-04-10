4aaefd13608fa347cb38986572d436e42879f831
Benjamin Arias <bjlaa@proton.me>
2025-04-10 10:48:26 +0200

    ♻️ Fix displaying header + fix end page style (#927)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import checkIfIsMobile from 'is-mobile'
 import HeaderDesktop from './header/HeaderDesktop'
 import HeaderMobile from './header/HeaderMobile'
 
@@ -8,7 +7,6 @@ type Props = {
   isSticky?: boolean
 }
 export default function Header({ isSticky = true }: Props) {
-  const isMobile = checkIfIsMobile()
   return (
     <>
       <HeaderMobile isSticky={isSticky} />
