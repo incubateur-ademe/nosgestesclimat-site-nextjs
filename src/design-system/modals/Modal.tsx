eec51a599b4ac45d460b07bbd047d04a5090c752
Benjamin Arias <benjamin.j.arias@gmail.com>
2025-04-15 17:56:26 +0200

    ♻️ Improve enter/out modal animation

--- src/design-system/modals/Modal.tsx
+++ src/design-system/modals/Modal.tsx
@@ -3,7 +3,7 @@
 import Trans from '@/components/translation/trans/TransClient'
 import { useClientTranslation } from '@/hooks/useClientTranslation'
 import type { ReactNode } from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import ReactModal from 'react-modal'
 import { twMerge } from 'tailwind-merge'
 import Button from '../buttons/Button'
@@ -32,33 +32,51 @@ export default function Modal({
   className,
   ...props
 }: Props) {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const { t } = useClientTranslation()
+
   useEffect(() => {
     document.body.style.overflow = 'hidden'
     ReactModal.setAppElement('#modal')
 
+    if (isOpen) {
+      requestAnimationFrame(() => setIsVisible(true))
+    }
+
     return () => {
       document.body.style.overflow = 'auto'
+      setIsVisible(false)
     }
-  }, [])
+  }, [isOpen])
 
-  const { t } = useClientTranslation()
+  const closeDelayed = () => {
+    setIsVisible(false)
+    setTimeout(() => closeModal(), 300)
+  }
 
   return (
     <ReactModal
       isOpen={isOpen}
-      onRequestClose={!isLoading ? closeModal : undefined}
+      onRequestClose={!isLoading ? closeDelayed : undefined}
       className={twMerge(
-        'fixed top-1/2 left-1/2 w-[40rem] max-w-[90vw] -translate-x-1/2 -translate-y-1/2 rounded-xl bg-white p-8 pt-4',
+        'fixed bottom-0 left-1/2 w-[40rem] max-w-[90vw] -translate-x-1/2 rounded-t-xl bg-white p-8 pt-4 transition-all duration-300 ease-out md:top-1/2 md:bottom-auto',
+        isVisible
+          ? 'translate-y-0 opacity-100 md:-translate-y-1/2 md:rounded-xl'
+          : 'translate-y-12 opacity-0 md:-translate-y-[calc(50%-3rem)]',
         className
       )}
-      overlayClassName="fixed top-0 left-0 right-0 bottom-0 bg-black/50 z-10000 overflow-hidden"
+      overlayClassName={twMerge(
+        'fixed top-0 left-0 right-0 bottom-0 bg-black/50 z-10000 overflow-hidden transition-opacity',
+        isVisible ? 'opacity-100' : 'opacity-20'
+      )}
       ariaHideApp={ariaHideApp}
       {...props}>
       {hasAbortCross && (
         <div className="flex justify-end">
           <button
             disabled={isLoading}
-            onClick={!isLoading ? closeModal : () => {}}
+            onClick={!isLoading ? closeDelayed : () => {}}
             className="text-xl"
             title={t('Fermer')}>
             ×
@@ -74,7 +92,7 @@ export default function Modal({
             <Button
               color="secondary"
               disabled={isLoading}
-              onClick={!isLoading ? closeModal : () => {}}>
+              onClick={!isLoading ? closeDelayed : () => {}}>
               <Trans>Annuler</Trans>
             </Button>
           )}
