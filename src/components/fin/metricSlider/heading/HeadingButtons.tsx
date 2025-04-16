336da2a53ea300d5ca1f62a3b332da2cd1d58843
Benjamin Arias <benjamin.j.arias@gmail.com>
2025-04-16 10:17:20 +0200

    ♻️ Move logic to separate component

--- src/components/fin/metricSlider/heading/HeadingButtons.tsx
+++ src/components/fin/metricSlider/heading/HeadingButtons.tsx
@@ -1,23 +1,11 @@
 'use client'
 
 import SaveIcon from '@/components/icons/SaveIcon'
-import FacebookIcon from '@/components/icons/share/FacebookIcon'
-import LinkedinIcon from '@/components/icons/share/LinkedinIcon'
-import MailIcon from '@/components/icons/share/MailIcon'
-import MessengerIcon from '@/components/icons/share/MessengerIcon'
-import WhatsappIcon from '@/components/icons/share/WhatsappIcon'
+import ShareSimulationButton from '@/components/sharing/ShareSimulationButton'
 import Trans from '@/components/translation/trans/TransClient'
 import { endClickSaveShortcut } from '@/constants/tracking/pages/end'
 import { simulationClickSaveShortcut } from '@/constants/tracking/pages/simulateur'
-import {
-  FACEBOOK_SHARE_URL,
-  LINKEDIN_SHARE_URL,
-  MESSENGER_SHARE_MOBILE_URL,
-  WHATSAPP_SHARE_URL,
-} from '@/constants/urls/share'
 import Button from '@/design-system/buttons/Button'
-import Share from '@/design-system/sharing/Share'
-import { getShareTrackEvent } from '@/helpers/tracking/share'
 import { useClientTranslation } from '@/hooks/useClientTranslation'
 import { useEndPageSharedUrl } from '@/hooks/useEndPageSharedUrl'
 import { trackEvent } from '@/utils/analytics/trackEvent'
@@ -68,62 +56,7 @@ export default function HeadingButtons({ size = 'md', endPage }: Props) {
         </span>
       </Button>
 
-      <Share
-        buttonLabel={t('Partager')}
-        modalTitle={t('Partager le simulateur')}
-        modalDescription={t(
-          'Envoyez le simulateur à vos proches et faites votre 1ère bonne action !'
-        )}
-        shareItems={[
-          {
-            icon: <WhatsappIcon />,
-            label: t('Whatsapp'),
-            link: `${WHATSAPP_SHARE_URL}${sharedUrl}`,
-            eventTracked: getShareTrackEvent({
-              page: 'Fin',
-              target: 'Whatsapp',
-            }),
-          },
-          {
-            icon: <FacebookIcon />,
-            label: t('Facebook'),
-            link: `${FACEBOOK_SHARE_URL}${sharedUrl}`,
-            eventTracked: getShareTrackEvent({
-              page: 'Fin',
-              target: 'Facebook',
-            }),
-          },
-          {
-            icon: <MessengerIcon />,
-            label: t('Messenger'),
-            link: `${MESSENGER_SHARE_MOBILE_URL}${sharedUrl}`,
-            mobileOnly: true,
-            eventTracked: getShareTrackEvent({
-              page: 'Fin',
-              target: 'Messenger',
-            }),
-          },
-          {
-            icon: <LinkedinIcon />,
-            label: t('Linkedin'),
-            link: `${LINKEDIN_SHARE_URL}${sharedUrl}`,
-            eventTracked: getShareTrackEvent({
-              page: 'Fin',
-              target: 'Linkedin',
-            }),
-          },
-          {
-            icon: <MailIcon className="fill-primary-700 w-4" />,
-            label: t('Envoyer par e-mail'),
-            link: `mailto:?subject=${t('Voici mes empreintes carbone et eau ; tu connais les tiennes ?')}&body=${sharedUrl}`,
-            eventTracked: getShareTrackEvent({
-              page: 'Fin',
-              target: 'E-mail',
-            }),
-          },
-        ]}
-        link={sharedUrl}
-      />
+      <ShareSimulationButton url={sharedUrl} />
     </div>
   )
 }
---
+++ src/components/sharing/ShareSimulationButton.tsx
@@ -0,0 +1,79 @@
+'use client'
+
+import {
+  FACEBOOK_SHARE_URL,
+  LINKEDIN_SHARE_URL,
+  MESSENGER_SHARE_MOBILE_URL,
+  WHATSAPP_SHARE_URL,
+} from '@/constants/urls/share'
+import Share from '@/design-system/sharing/Share'
+import { getShareTrackEvent } from '@/helpers/tracking/share'
+import { useClientTranslation } from '@/hooks/useClientTranslation'
+import FacebookIcon from '../icons/share/FacebookIcon'
+import LinkedinIcon from '../icons/share/LinkedinIcon'
+import MailIcon from '../icons/share/MailIcon'
+import MessengerIcon from '../icons/share/MessengerIcon'
+import WhatsappIcon from '../icons/share/WhatsappIcon'
+
+export default function ShareSimulationButton({ url }: { url: string }) {
+  const { t } = useClientTranslation()
+
+  return (
+    <Share
+      buttonLabel={t('Partager')}
+      modalTitle={t('Partager le simulateur')}
+      modalDescription={t(
+        'Envoyez le simulateur à vos proches et faites votre 1ère bonne action !'
+      )}
+      shareItems={[
+        {
+          icon: <WhatsappIcon />,
+          label: t('Whatsapp'),
+          link: `${WHATSAPP_SHARE_URL}${url}`,
+          eventTracked: getShareTrackEvent({
+            page: 'Fin',
+            target: 'Whatsapp',
+          }),
+        },
+        {
+          icon: <FacebookIcon />,
+          label: t('Facebook'),
+          link: `${FACEBOOK_SHARE_URL}${url}`,
+          eventTracked: getShareTrackEvent({
+            page: 'Fin',
+            target: 'Facebook',
+          }),
+        },
+        {
+          icon: <MessengerIcon />,
+          label: t('Messenger'),
+          link: `${MESSENGER_SHARE_MOBILE_URL}${url}`,
+          mobileOnly: true,
+          eventTracked: getShareTrackEvent({
+            page: 'Fin',
+            target: 'Messenger',
+          }),
+        },
+        {
+          icon: <LinkedinIcon />,
+          label: t('Linkedin'),
+          link: `${LINKEDIN_SHARE_URL}${url}`,
+          eventTracked: getShareTrackEvent({
+            page: 'Fin',
+            target: 'Linkedin',
+          }),
+        },
+        {
+          icon: <MailIcon className="fill-primary-700 w-4" />,
+          label: t('Envoyer par e-mail'),
+          link: `mailto:?subject=${t('Voici mes empreintes carbone et eau ; tu connais les tiennes ?')}&body=${url}`,
+          eventTracked: getShareTrackEvent({
+            page: 'Fin',
+            target: 'E-mail',
+          }),
+        },
+      ]}
+      link={url}
+    />
+  )
+}
