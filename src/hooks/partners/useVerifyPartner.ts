99a903d86ec68480212e1ee4f8818adb22e884c4
Benjamin Arias <benjamin.j.arias@gmail.com>
2025-04-16 13:58:21 +0200

    ✨ Verify partner

---
+++ src/hooks/partners/useVerifyPartner.ts
@@ -0,0 +1,20 @@
+import { PARTNER_URL } from '@/constants/urls'
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
+
+export function useVerifyPartner(partner?: string) {
+  const { mutate } = useMutation({
+    mutationFn: async () => {
+      if (!partner) return false
+
+      try {
+        await axios.get(`${PARTNER_URL}/${partner}`)
+
+        return true
+      } catch (err) {
+        return false
+      }
+    },
+  })
+  return mutate
+}
