1cdaafa393b0a194ccdd9ed26c68c24c42173787
Benjamin Arias <bjlaa@proton.me>
2025-04-17 17:33:37 +0200

    ✨ Add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,25 @@
+import type { StorybookConfig } from '@storybook/nextjs'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/experimental-addon-test',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  staticDirs: ['../public'],
+  webpackFinal: async (config) => {
+    if (config.module?.rules) {
+      config.module.rules.push({
+        test: /\.ya?ml$/,
+        use: 'yaml-loader',
+      })
+    }
+    return config
+  },
+}
+export default config
