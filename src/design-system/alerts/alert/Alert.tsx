3d3f1c049c51836abd9c7262404b38c11f376a31
Benjamin Arias <bjlaa@proton.me>
2025-04-17 17:32:26 +0200

    ✨ Add type prop and classes

---
+++ src/design-system/alerts/alert/Alert.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import CloseIcon from '@/components/icons/Close'
+import Trans from '@/components/translation/trans/TransClient'
+import { type ReactNode, createElement } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+type AlertType = 'default' | 'success' | 'warning' | 'error'
+
+const getTypeClassNames = (type: AlertType) => {
+  switch (type) {
+    case 'default':
+      return 'border-blue-200 bg-blue-100'
+    case 'success':
+      return 'border-green-200 bg-green-100'
+    case 'warning':
+      return 'border-orange-200 bg-orange-100'
+    case 'error':
+      return 'border-red-200 bg-red-100'
+  }
+}
+
+export default function Alert({
+  title,
+  titleTag,
+  description,
+  onClose,
+  type = 'default',
+}: {
+  title: ReactNode | string
+  titleTag?: string
+  description: ReactNode | string
+  onClose?: () => void
+  type?: AlertType
+}) {
+  console.log(onClose)
+  return (
+    <section
+      className={twMerge(
+        'relative rounded-xl border-2 p-8',
+        getTypeClassNames(type)
+      )}>
+      {onClose && (
+        <button className="absolute top-3 right-4" onClick={onClose}>
+          <span className="not-sr-only">
+            <CloseIcon className="w-5" />
+          </span>
+          <span className="sr-only">
+            <Trans>Fermer</Trans>
+          </span>
+        </button>
+      )}
+
+      {createElement(
+        titleTag ?? 'h2',
+        { className: 'mb-3 text-sm font-bold' },
+        title
+      )}
+      <p className="mb-0 text-sm">{description}</p>
+    </section>
+  )
+}
