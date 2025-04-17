da53a5c96616c6d9930abc5d4c3004882eb38ad3
Benjamin Arias <bjlaa@proton.me>
2025-04-17 17:34:13 +0200

    :bug: Fix config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@chromatic-com/storybook',
