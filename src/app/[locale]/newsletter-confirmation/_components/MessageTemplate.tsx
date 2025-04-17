2c933a370cd671213538b09fc6fcf61facca3e13
Benjamin Arias <bjlaa@proton.me>
2025-04-17 17:47:49 +0200

    💄 Adjust spacings

--- src/app/[locale]/newsletter-confirmation/_components/MessageTemplate.tsx
+++ src/app/[locale]/newsletter-confirmation/_components/MessageTemplate.tsx
@@ -13,9 +13,9 @@ export default function MessageTemplate({
     <>
       <h1 className="mb-4 px-3 text-lg md:px-0 md:text-xl">{title}</h1>
 
-      <p className="mb-10 px-3 md:px-0">{body}</p>
+      <p className="mb-11 px-3 md:px-0">{body}</p>
 
-      <div className="flex flex-col items-center justify-center gap-4 md:flex-row">
+      <div className="flex flex-col items-center justify-center gap-6 md:flex-row">
         {buttonElement}
       </div>
     </>
