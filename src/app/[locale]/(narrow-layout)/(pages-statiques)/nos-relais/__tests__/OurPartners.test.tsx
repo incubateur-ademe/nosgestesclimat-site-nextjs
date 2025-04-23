98b06fbe220e39845cf4c599ba2d76c002918305
Benjamin Arias <bjlaa@proton.me>
2025-04-23 17:05:13 +0200

    :sparkles: Use Strapi data to display partners across website (#923)

---
+++ src/app/[locale]/(narrow-layout)/(pages-statiques)/nos-relais/__tests__/OurPartners.test.tsx
@@ -0,0 +1,55 @@
+import { fetchPartners } from '@/services/cms/fetchPartners'
+import type { DefaultPageProps } from '@/types'
+import { beforeAll } from '@jest/globals'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import OurPartners from '../page'
+
+// Mock de la fonction fetchPartners
+jest.mock('@/services/cms/fetchPartners')
+
+const mockPartners = [
+  {
+    name: 'Partner 1',
+    imageSrc: '/partner1.png',
+    link: 'https://partner1.com',
+    category: { category: 'Category 1' },
+    displayOrder: 1,
+  },
+  {
+    name: 'Partner 2',
+    imageSrc: '/partner2.png',
+    link: 'https://partner2.com',
+    category: { category: 'Category 2' },
+    displayOrder: 2,
+  },
+]
+
+describe('OurPartners page', () => {
+  beforeAll(() => {
+    ;(fetchPartners as jest.Mock).mockResolvedValue(mockPartners)
+  })
+
+  it('should display partners correctly', async () => {
+    // Given
+    const props = {
+      params: Promise.resolve({ locale: 'fr' }),
+      searchParams: Promise.resolve({}),
+    } satisfies DefaultPageProps
+
+    // When
+    render(await OurPartners(props))
+
+    // Then
+    // The categories are displayed
+    expect(
+      screen.queryAllByText(mockPartners[0].category.category)[0]
+    ).toBeInTheDocument()
+    expect(
+      screen.queryAllByText(mockPartners[1].category.category)[0]
+    ).toBeInTheDocument()
+    // The partners are displayed
+    expect(screen.queryByText(mockPartners[0].name)).toBeInTheDocument()
+    expect(screen.queryByText(mockPartners[1].name)).toBeInTheDocument()
+  })
+})
--- src/app/[locale]/_components/TheySpeakAboutUs.tsx
+++ src/app/[locale]/_components/TheySpeakAboutUs.tsx
@@ -1,10 +1,27 @@
 import Background from '@/components/landing-pages/Background'
 import Trans from '@/components/translation/trans/TransServer'
+import ButtonLink from '@/design-system/buttons/ButtonLink'
+import { fetchPartners } from '@/services/cms/fetchPartners'
 import Image from 'next/image'
+import { twMerge } from 'tailwind-merge'
 
-export default function TheySpeakAboutUs({ locale }: { locale: string }) {
+export default async function TheySpeakAboutUs({
+  locale,
+  className,
+  ctaHref = '/nos-relais',
+}: {
+  locale: string
+  className?: string
+  ctaHref?: string
+}) {
+  const partners = await fetchPartners({ displayOnLandingPage: true })
+
   return (
-    <div className="relative mb-10 px-4 py-16 md:mb-20 md:py-28 xl:mb-32">
+    <div
+      className={twMerge(
+        'relative mb-10 px-4 py-16 md:mb-20 md:py-28 xl:mb-32',
+        className
+      )}>
       {/* Helps cover the triangles of white shown because of the perspective change in Background */}
       <div className="bg-heroLightBackground absolute top-0 left-0 h-1/2 w-[200%]" />
 
@@ -16,71 +33,32 @@ export default function TheySpeakAboutUs({ locale }: { locale: string }) {
       />
 
       <div className="relative flex flex-col items-center gap-10 md:mx-auto md:max-w-5xl">
-        <h2 className="text-center text-2xl md:text-3xl">
-          <Trans locale={locale}>Ils parlent de nous</Trans>
+        <h2 className="text-center text-xl md:text-2xl">
+          <Trans locale={locale}>
+            Plusieurs milliers d’organisations nous font confiance pour
+            sensibiliser efficacement
+          </Trans>
         </h2>
 
         <ul className="flex w-full flex-row flex-wrap items-center justify-center gap-7 md:justify-between">
-          <li>
-            <Image
-              src="/images/ambassadeurs/france_info.png"
-              alt="France Info"
-              className="h-auto w-24 md:w-36"
-              width={150}
-              height={150}
-            />
-          </li>
-
-          <li>
-            <Image
-              src="/images/ambassadeurs/france_inter.png"
-              alt="France Inter"
-              className="h-auto w-16 md:w-24"
-              width={100}
-              height={100}
-            />
-          </li>
-
-          <li>
-            <Image
-              src="/images/ambassadeurs/bon-pote-no-background.svg"
-              alt="Bon Pote"
-              className="h-auto w-24 md:w-36"
-              width={140}
-              height={100}
-            />
-          </li>
-
-          <li>
-            <Image
-              src="/images/ambassadeurs/bfntv.png"
-              alt="BFM TV"
-              className="h-auto w-12 md:w-20"
-              width={80}
-              height={100}
-            />
-          </li>
-
-          <li>
-            <Image
-              src="/images/ambassadeurs/vert_rectangulaire.png"
-              alt="Vert Media"
-              className="h-auto w-20 md:w-32"
-              width={120}
-              height={100}
-            />
-          </li>
-
-          <li>
-            <Image
-              src="/images/ambassadeurs/challenges.png"
-              alt="Challenges"
-              className="h-auto w-28 md:w-40"
-              width={160}
-              height={100}
-            />
-          </li>
+          {partners.map((partner) => (
+            <li key={partner.name} data-testid="partner-theyspeakaboutus">
+              <Image
+                src={partner.imageSrc}
+                alt={partner.name}
+                className="h-auto w-20 md:w-24"
+                width={150}
+                height={150}
+              />
+            </li>
+          ))}
         </ul>
+
+        <div className="flex justify-center">
+          <ButtonLink color="secondary" size="xl" href={ctaHref}>
+            <Trans locale={locale}>Rejoignez-les</Trans>
+          </ButtonLink>
+        </div>
       </div>
     </div>
   )
