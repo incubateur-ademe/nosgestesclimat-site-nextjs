63e08a9a835c877c0f0f9e1c4c98cf25a6501d46
Benjamin Arias <bjlaa@proton.me>
2025-04-24 13:15:54 +0200

    ✅ Improve tests

--- src/app/[locale]/newsletter-confirmation/__tests__/NewsletterConfirmationPage.test.tsx
+++ src/app/[locale]/newsletter-confirmation/__tests__/NewsletterConfirmationPage.test.tsx
@@ -1,9 +1,10 @@
-import { render } from '@testing-library/react'
+import { render, act } from '@testing-library/react'
 import { redirect } from 'next/navigation'
 import NewsletterErrorMessage from '../_components/NewsletterErrorMessage'
 import NewsletterInvalidMessage from '../_components/NewsletterInvalidMessage'
 import NewsletterSuccessMessage from '../_components/NewsletterSuccessMessage'
 import NewsletterConfirmationPage from '../page'
+import QueryClientProviderWrapper from '../../_components/mainLayoutProviders/QueryClientProviderWrapper'
 
 jest.mock('../_components/NewsletterSuccessMessage', () => ({
   __esModule: true,
@@ -20,35 +21,19 @@ jest.mock('../_components/NewsletterInvalidMessage', () => ({
   default: jest.fn(() => null),
 }))
 
-jest.mock('@/components/layout/Header', () => ({
-  __esModule: true,
-  default: () => <div>Header</div>,
-}))
-
-jest.mock('@/design-system/layout/Main', () => ({
-  __esModule: true,
-  default: ({ children }: { children: React.ReactNode }) => (
-    <div>{children}</div>
-  ),
-}))
-
-jest.mock('next/navigation', () => ({
-  redirect: jest.fn(),
-}))
-
 describe('NewsletterConfirmationPage', () => {
-  beforeEach(() => {
-    jest.clearAllMocks()
-  })
-
   it('should render success message when success=true', async () => {
-    // When
-    const jsx = await NewsletterConfirmationPage({
+    const props = {
       params: Promise.resolve({ locale: 'fr' }),
-      searchParams: Promise.resolve({ success: 'true' }),
-    })
+      searchParams: Promise.resolve({ success: 'true' as const }),
+    }
 
-    render(jsx)
+    // When
+    await act(async () => {
+      render(await NewsletterConfirmationPage(props), {
+        wrapper: QueryClientProviderWrapper,
+      })
+    })
 
     // Then
     expect(NewsletterSuccessMessage).toHaveBeenCalledWith(
@@ -60,13 +45,21 @@ describe('NewsletterConfirmationPage', () => {
   })
 
   it('should render the invalid message when success=false and status=404', async () => {
-    // When
-    const jsx = await NewsletterConfirmationPage({
+    // Given
+    const props = {
       params: Promise.resolve({ locale: 'fr' }),
-      searchParams: Promise.resolve({ success: 'false', status: '404' }),
-    })
+      searchParams: Promise.resolve({
+        success: 'false' as const,
+        status: '404' as const,
+      }),
+    }
 
-    render(jsx)
+    // When
+    await act(async () => {
+      render(await NewsletterConfirmationPage(props), {
+        wrapper: QueryClientProviderWrapper,
+      })
+    })
 
     // Then
     expect(NewsletterSuccessMessage).not.toHaveBeenCalled()
@@ -78,13 +71,21 @@ describe('NewsletterConfirmationPage', () => {
   })
 
   it('should render the error message when success=false and status=500', async () => {
-    // When
-    const jsx = await NewsletterConfirmationPage({
+    // Given
+    const props = {
       params: Promise.resolve({ locale: 'fr' }),
-      searchParams: Promise.resolve({ success: 'false', status: '500' }),
-    })
+      searchParams: Promise.resolve({
+        success: 'false' as const,
+        status: '500' as const,
+      }),
+    }
 
-    render(jsx)
+    // When
+    await act(async () => {
+      render(await NewsletterConfirmationPage(props), {
+        wrapper: QueryClientProviderWrapper,
+      })
+    })
 
     // Then
     expect(NewsletterSuccessMessage).not.toHaveBeenCalled()
@@ -97,13 +98,17 @@ describe('NewsletterConfirmationPage', () => {
 
   it('should redirect to the 404 page if wrong success param is passed', async () => {
     // Given
-    const jsx = await NewsletterConfirmationPage({
+    const props = {
       params: Promise.resolve({ locale: 'fr' }),
       searchParams: Promise.resolve({ success: 'toto' as 'true' }),
-    })
+    }
 
     // When
-    render(jsx)
+    await act(async () => {
+      render(await NewsletterConfirmationPage(props), {
+        wrapper: QueryClientProviderWrapper,
+      })
+    })
 
     // Then
     expect(redirect).toHaveBeenCalledWith('/404')
@@ -111,16 +116,20 @@ describe('NewsletterConfirmationPage', () => {
 
   it('should redirect to the 404 page if wrong status param is passed', async () => {
     // Given
-    const jsx = await NewsletterConfirmationPage({
+    const props = {
       params: Promise.resolve({ locale: 'fr' }),
       searchParams: Promise.resolve({
-        success: 'false',
+        success: 'false' as const,
         status: '505' as '500',
       }),
-    })
+    }
 
     // When
-    render(jsx)
+    await act(async () => {
+      render(await NewsletterConfirmationPage(props), {
+        wrapper: QueryClientProviderWrapper,
+      })
+    })
 
     // Then
     expect(redirect).toHaveBeenCalledWith('/404')
