41871d08c0d3fb35f060fc0ee49bf655c0025d6b
Benjamin Arias <bjlaa@proton.me>
2025-04-24 13:18:58 +0200

    ✨ Create newsletter confirmation page (#935)

---
+++ src/app/[locale]/newsletter-confirmation/_components/NewsletterInvalidMessage.tsx
@@ -0,0 +1,31 @@
+import Trans from '@/components/translation/trans/TransServer'
+import ButtonLink from '@/design-system/buttons/ButtonLink'
+import { getServerTranslation } from '@/helpers/getServerTranslation'
+import MessageTemplate from './MessageTemplate'
+
+export default async function NewsletterInvalidMessage({
+  locale,
+}: {
+  locale: string
+}) {
+  const { t } = await getServerTranslation({ locale })
+  return (
+    <MessageTemplate
+      title={t("L'invitation a expiré")}
+      body={t(
+        'Vous devez recommencer votre inscription et valider votre e-mail.'
+      )}
+      buttonElement={
+        <>
+          <ButtonLink color="secondary" href="/">
+            <Trans locale={locale}>Revenir à la page d'accueil</Trans>
+          </ButtonLink>
+
+          <ButtonLink href="/profil#infolettre">
+            <Trans locale={locale}>Recommencer mon inscription</Trans>
+          </ButtonLink>
+        </>
+      }
+    />
+  )
+}
