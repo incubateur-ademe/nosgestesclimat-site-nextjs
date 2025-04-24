473b268805b6bbf4b6ff4ae9cdc3f28a7203483e
Clément <clem.auger@hotmail.fr>
2025-04-24 17:12:41 +0200

    🏷️ fix typing to avoid unknown

--- src/hooks/groups/__tests__/fixtures/createGroup.ts
+++ src/hooks/groups/__tests__/fixtures/createGroup.ts
@@ -8,7 +8,7 @@ import rules from '@incubateur-ademe/nosgestesclimat/public/co2-model.FR-lang.fr
 import personas from '@incubateur-ademe/nosgestesclimat/public/personas-fr.json'
 import Engine from 'publicodes'
 
-const engine = new Engine<DottedName>(rules as unknown as NGCRules, {
+const engine = new Engine<DottedName>(rules as Partial<NGCRules>, {
   logger: { warn: () => {}, error: () => {}, log: () => {} },
   strict: {
     situation: false,
