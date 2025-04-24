90dc453e73e208233031ef53deeb3d5b0b3b6830
Benjamin Arias <bjlaa@proton.me>
2025-04-24 17:45:03 +0200

    ♻️ Fix type error

--- src/hooks/partners/useVerifyPartner.ts
+++ src/hooks/partners/useVerifyPartner.ts
@@ -1,20 +1,26 @@
 import { PARTNER_URL } from '@/constants/urls/main'
 import { useMutation } from '@tanstack/react-query'
 import axios from 'axios'
+import { useEffect } from 'react'
 
-export function useVerifyPartner(partner?: string) {
-  const { mutate } = useMutation({
+export function useVerifyPartner(partner?: string): boolean {
+  const { data, mutate } = useMutation({
     mutationFn: async () => {
       if (!partner) return false
 
       try {
+        // If request is successful, the partner is verified
         await axios.get(`${PARTNER_URL}/${partner}`)
-
         return true
       } catch (err) {
         return false
       }
     },
   })
-  return mutate()
+
+  useEffect(() => {
+    mutate()
+  }, [])
+
+  return data ?? false
 }
