a240a65517c095336a51d3fbd81bd8a2b4d6675c
Benjamin Arias <bjlaa@proton.me>
2025-04-28 16:36:42 +0200

    Update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,10 +1,10 @@
 module.exports = (on, config) => {
   config.env = process.env
 
-  on('task', {
-    log(message) {
-      console.log(message)
-      return null
-    },
-  })
+  // on('task', {
+  //   log(message) {
+  //     console.log(message)
+  //     return null
+  //   },
+  // })
 }
