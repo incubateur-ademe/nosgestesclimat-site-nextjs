3797d34ce88eb217354623364a20136d5fbb6191
Benjamin Arias <bjlaa@proton.me>
2025-04-29 08:23:41 +0200

    Update checkA11y.js

--- cypress/helpers/accessibility/checkA11y.js
+++ cypress/helpers/accessibility/checkA11y.js
@@ -1,23 +1,9 @@
 import 'cypress-axe'
+import CHECK_A11Y_ELEMENTS_EXCLUDED from '../../constants/accessibility'
 
 export const checkA11y = () => {
   cy.injectAxe()
-  cy.checkA11y(
-    {
-      exclude: CHECK_A11Y_ELEMENTS_EXCLUDED,
-    }
-    // null,
-    // (violations) => {
-    //   if (violations.length) {
-    //     cy.task(
-    //       'log',
-    //       `${violations.length} accessibility violation${violations.length === 1 ? '' : 's'} detected`
-    //     )
-    //     violations.forEach(({ id, impact, description, nodes }) => {
-    //       cy.task('log', `[${impact}] ${id}: ${description}`)
-    //       nodes.forEach(({ target }) => cy.task('log', `  Node: ${target}`))
-    //     })
-    //   }
-    // }
-  )
+  cy.checkA11y({
+    exclude: CHECK_A11Y_ELEMENTS_EXCLUDED,
+  })
 }
