549ba33e6dcc2e23a2ae9502e54bb57272bf9db1
Benjamin Arias <bjlaa@proton.me>
2025-04-29 08:32:27 +0200

    Update checkA11y.js

--- cypress/helpers/accessibility/checkA11y.js
+++ cypress/helpers/accessibility/checkA11y.js
@@ -1,9 +1,11 @@
 import 'cypress-axe'
-import CHECK_A11Y_ELEMENTS_EXCLUDED from '../../constants/accessibility'
 
 export const checkA11y = () => {
   cy.injectAxe()
   cy.checkA11y({
-    exclude: CHECK_A11Y_ELEMENTS_EXCLUDED,
+    exclude: [
+      '.splide', // false positive ; allowed-role
+      '.skip-to-main-content', // false positive ; skip-link : Ensure all skip links have a focusable target
+    ],
   })
 }
