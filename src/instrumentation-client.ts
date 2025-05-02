da126767e1ae38af5d6ff03e03e17e143f5d80bf
Benjamin Arias <bjlaa@proton.me>
2025-05-02 16:29:21 +0200

    ♻️ Add comment

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -11,3 +11,5 @@ Sentry.init({
   // Disable sentry for development based on local data
   enabled: process.env.NODE_ENV !== 'development',
 })
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
