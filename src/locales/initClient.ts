52ca4f1819d413a36fad31550a87a25ee5692868
Benjamin Arias <bjlaa@proton.me>
2025-05-11 16:06:24 +0200

    Update initClient.ts

--- src/locales/initClient.ts
+++ src/locales/initClient.ts
@@ -1,24 +1,28 @@
 'use client'
 
 import i18next from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 import { getOptions } from './settings'
 import { translations } from './translation'
 
-i18next.use(initReactI18next).init({
-  ...getOptions(),
-  lng: undefined, // let detect the language on client side
-  resources: {
-    es: {
-      translation: translations['es'],
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    ...getOptions(),
+    lng: undefined, // let detect the language on client side
+    resources: {
+      es: {
+        translation: translations['es'],
+      },
+      en: {
+        translation: translations['en'],
+      },
+      fr: {
+        translation: translations['fr'],
+      },
     },
-    en: {
-      translation: translations['en'],
-    },
-    fr: {
-      translation: translations['fr'],
-    },
-  },
-})
+  })
 
 export default i18next
