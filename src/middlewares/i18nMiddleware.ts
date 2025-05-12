52c0a8fc306125c3e245a7aeab11feb243d1af20
Benjamin Arias <bjlaa@proton.me>
2025-05-12 10:13:33 +0200

    Update i18nMiddleware.ts

--- src/middlewares/i18nMiddleware.ts
+++ src/middlewares/i18nMiddleware.ts
@@ -1,23 +1,23 @@
-import i18nConfig, { type Locale } from '@/i18nConfig'
+import i18nConfig from '@/i18nConfig'
 import { i18nRouter } from 'next-i18n-router'
-import { type NextRequest, NextResponse } from 'next/server'
+import { type NextRequest } from 'next/server'
 
 function i18nMiddleware(request: NextRequest) {
-  if (request.nextUrl.pathname === '/') {
-    // Get the locale from cookie
-    const cookie = request.cookies.get('NEXT_LOCALE')
-    const locale = cookie?.value
+  // if (request.nextUrl.pathname === '/') {
+  //   // Get the locale from cookie
+  //   const cookie = request.cookies.get('NEXT_LOCALE')
+  //   const locale = cookie?.value
 
-    // If we have a valid locale in cookie and it's not the default locale
-    if (
-      locale &&
-      locale !== i18nConfig.defaultLocale &&
-      i18nConfig.locales.includes(locale as Locale)
-    ) {
-      // Redirect to the localized version of the root path
-      return NextResponse.redirect(new URL(`/${locale}`, request.url))
-    }
-  }
+  //   // If we have a valid locale in cookie and it's not the default locale
+  //   if (
+  //     locale &&
+  //     locale !== i18nConfig.defaultLocale &&
+  //     i18nConfig.locales.includes(locale as Locale)
+  //   ) {
+  //     // Redirect to the localized version of the root path
+  //     return NextResponse.redirect(new URL(`/${locale}`, request.url))
+  //   }
+  // }
 
   return i18nRouter(request, i18nConfig)
 }
