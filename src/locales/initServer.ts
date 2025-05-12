b16fd7aaba5d0d9147e5f9f491a0a38d1c46e4d1
Benjamin Arias <bjlaa@proton.me>
2025-05-12 13:42:40 +0200

    🐛 Remove conflicting code for locale detection [NGC-1887] (#958)

--- src/locales/initServer.ts
+++ src/locales/initServer.ts
@@ -1,5 +1,4 @@
 import { createInstance } from 'i18next'
-import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector'
 import resourcesToBackend from 'i18next-resources-to-backend'
 import { initReactI18next } from 'react-i18next/initReactI18next'
 import { getOptions } from './settings'
@@ -8,7 +7,6 @@ import { translations } from './translation'
 const initI18next = async (language: string) => {
   const i18nInstance = createInstance()
   await i18nInstance
-    .use(I18nextBrowserLanguageDetector)
     .use(resourcesToBackend((language: string) => translations[language]))
     .use(initReactI18next)
     .init(getOptions(language))
