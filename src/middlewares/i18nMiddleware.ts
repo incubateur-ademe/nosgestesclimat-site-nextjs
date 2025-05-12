d2ecd2be9c0b91facb05509f42c4f4b98661acff
Benjamin Arias <bjlaa@proton.me>
2025-05-12 10:30:38 +0200

    Update i18nMiddleware.ts

--- src/middlewares/i18nMiddleware.ts
+++ src/middlewares/i18nMiddleware.ts
@@ -3,22 +3,6 @@ import { i18nRouter } from 'next-i18n-router'
 import { type NextRequest } from 'next/server'
 
 function i18nMiddleware(request: NextRequest) {
-  // if (request.nextUrl.pathname === '/') {
-  //   // Get the locale from cookie
-  //   const cookie = request.cookies.get('NEXT_LOCALE')
-  //   const locale = cookie?.value
-
-  //   // If we have a valid locale in cookie and it's not the default locale
-  //   if (
-  //     locale &&
-  //     locale !== i18nConfig.defaultLocale &&
-  //     i18nConfig.locales.includes(locale as Locale)
-  //   ) {
-  //     // Redirect to the localized version of the root path
-  //     return NextResponse.redirect(new URL(`/${locale}`, request.url))
-  //   }
-  // }
-
   return i18nRouter(request, i18nConfig)
 }
 
