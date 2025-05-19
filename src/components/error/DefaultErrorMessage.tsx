6b2e5221c5f7872d661f5e53289152959137530c
Benjamin Arias <bjlaa@proton.me>
2025-05-19 09:27:26 +0200

    ♻️ Make DefaultErrorMessage content replacable

--- src/components/error/DefaultErrorMessage.tsx
+++ src/components/error/DefaultErrorMessage.tsx
@@ -1,25 +1,28 @@
 'use client'
 
 import Alert from '@/design-system/alerts/alert/Alert'
+import type { PropsWithChildren } from 'react'
 import Link from '../Link'
 import Trans from '../translation/trans/TransClient'
 
-export default function DefaultErrorMessage() {
+export default function DefaultErrorMessage({ children }: PropsWithChildren) {
   return (
     <Alert
       type="error"
       className="mb-6"
       description={
-        <>
-          <Trans>
-            Oups ! Une erreur s'est produite. Veuillez réessayer plus tard. Si
-            le problème persiste, vous pouvez
-          </Trans>{' '}
-          <Link href="/contact">
-            <Trans>nous contacter</Trans>
-          </Link>
-          .
-        </>
+        children ?? (
+          <>
+            <Trans>
+              Oups ! Une erreur s'est produite. Veuillez réessayer plus tard. Si
+              le problème persiste, vous pouvez
+            </Trans>{' '}
+            <Link href="/contact">
+              <Trans>nous contacter</Trans>
+            </Link>
+            .
+          </>
+        )
       }
     />
   )
