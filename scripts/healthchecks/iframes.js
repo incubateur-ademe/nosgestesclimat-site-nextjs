fa65b769ebb1447354db13d4ed2de965db6d88d2
Benjamin Arias <bjlaa@proton.me>
2025-05-21 15:04:22 +0200

    ♻️ Remove useless stuff

--- scripts/healthchecks/iframes.js
+++ scripts/healthchecks/iframes.js
@@ -1,8 +1,5 @@
 const axios = require('axios')
-const puppeteer = require('puppeteer-extra')
-const StealthPlugin = require('puppeteer-extra-plugin-stealth')
-puppeteer.use(StealthPlugin())
-
+const puppeteer = require('puppeteer')
 require('dotenv').config()
 
 const urls = [
@@ -27,32 +24,9 @@ async function healthcheck() {
       console.log(`\nChecking URL: ${url}`)
 
       try {
-        const response = await axios.get(url, { timeout: 10000 })
-        console.log(`Axios GET status for ${url}: ${response.status}`)
-      } catch (err) {
-        console.log(`Axios GET failed for ${url}: ${err.message}`)
-      }
-
-      try {
-        await page.setExtraHTTPHeaders({
-          Accept:
-            'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-          'Accept-Language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
-          'Cache-Control': 'no-cache',
-          Pragma: 'no-cache',
-          'Upgrade-Insecure-Requests': '1',
-          'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-          DNT: '1', // Do Not Track
-          'Sec-CH-UA': '"Chromium";v="120", "Not:A-Brand";v="99"',
-          'Sec-CH-UA-Mobile': '?0',
-          'Sec-CH-UA-Platform': '"Windows"',
-          'Sec-Fetch-Dest': 'document',
-          'Sec-Fetch-Mode': 'navigate',
-          'Sec-Fetch-Site': 'none',
-          'Sec-Fetch-User': '?1',
-          'Accept-Encoding': 'gzip, deflate, br',
-        })
+        await page.setUserAgent(
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        )
         await page.goto(url, { waitUntil: 'networkidle0', timeout: 60000 })
 
         try {
