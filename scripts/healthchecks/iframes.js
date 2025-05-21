80e664fe6ba6d69319232d634eec1618e4a00a03
Benjamin Arias <bjlaa@proton.me>
2025-05-21 14:40:38 +0200

    Update iframes.js

--- scripts/healthchecks/iframes.js
+++ scripts/healthchecks/iframes.js
@@ -27,7 +27,7 @@ async function healthcheck() {
         await page.setUserAgent(
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
         )
-        await page.goto(url, { waitUntil: 'domcontentloaded' })
+        await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 60000 })
 
         try {
           await page.waitForSelector(`#${iframeId}`, { timeout: 5000 })
