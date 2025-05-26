afc50652fa572d872cc3673f090b887107960a8e
Benjamin Arias <bjlaa@proton.me>
2025-05-26 18:13:27 +0200

    🔖 Release v2.20.0 (#1043)
    Co-authored-by: Benjamin Arias <12382534+bjlaa@users.noreply.github.com>

--- src/app/[locale]/(simulation)/(large-layout)/infos/email/page.tsx
+++ src/app/[locale]/(simulation)/(large-layout)/infos/email/page.tsx
@@ -3,6 +3,7 @@
 import Trans from '@/components/translation/trans/TransClient'
 import { EMAIL_PAGE } from '@/constants/organisations/infosPages'
 import EmailInput from '@/design-system/inputs/EmailInput'
+import InlineLink from '@/design-system/inputs/InlineLink'
 import Title from '@/design-system/layout/Title'
 import { useInfosPage } from '@/hooks/navigation/useInfosPage'
 import { useFetchPublicPoll } from '@/hooks/organisations/polls/useFetchPublicPoll'
@@ -24,6 +25,8 @@ export default function Email() {
   const fixedEmail = searchParams.get('fixedemail') ? true : false
 
   const { user, updateEmail } = useUser()
+
+  const pollSlug = useSearchParams().get('poll')
 
   const {
     register,
@@ -82,7 +85,6 @@ export default function Email() {
     },
     [poll, updateEmail, router, getLinkToNextInfosPage, setError, t]
   )
-
   return (
     <form>
       <Title
@@ -91,9 +93,24 @@ export default function Email() {
         title={<Trans>Votre adresse electronique</Trans>}
         subtitle={
           <>
-            <Trans>
-              Pour conserver vos résultats et les retrouver à l’avenir
-            </Trans>
+            {pollSlug &&
+            process.env.NEXT_PUBLIC_POLL_CONTEST_SLUGS &&
+            process.env.NEXT_PUBLIC_POLL_CONTEST_SLUGS.split(',') &&
+            process.env.NEXT_PUBLIC_POLL_CONTEST_SLUGS.includes(pollSlug) ? (
+              <span>
+                <Trans>Votre e-mail sera utilisé pour le tirage au sort.</Trans>{' '}
+                <InlineLink
+                  target="_blank"
+                  href="/politique-de-confidentialite">
+                  <Trans>En savoir plus</Trans>
+                </InlineLink>
+              </span>
+            ) : (
+              <Trans>
+                Pour conserver vos résultats et les retrouver à l’avenir
+              </Trans>
+            )}
+
             {!fixedEmail ? (
               <span className="text-secondary-700 ml-2 inline-block font-bold italic">
                 <Trans>facultatif</Trans>
