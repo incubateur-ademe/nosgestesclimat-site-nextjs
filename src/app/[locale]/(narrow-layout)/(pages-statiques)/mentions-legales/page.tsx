2f39628d9aea6a1a484bd1e956db488720d4bf0b
Benjamin Arias <bjlaa@proton.me>
2025-05-26 12:44:42 +0200

    ♻️ Update legal notice, remove remainings of Vercel use [NGC-1965] (#1038)
    Co-authored-by: Benjamin Arias <12382534+bjlaa@users.noreply.github.com>

--- src/app/[locale]/(narrow-layout)/(pages-statiques)/mentions-legales/page.tsx
+++ src/app/[locale]/(narrow-layout)/(pages-statiques)/mentions-legales/page.tsx
@@ -21,10 +21,10 @@ export default function MentionsLegalesPage() {
         licenceUrl="https://github.com/incubateur-ademe/nosgestesclimat-site-nextjs/blob/main/LICENSE"
         privacyPolicyUrl="/politique-de-confidentialite"
         siteHost={{
-          name: 'Vercel Inc.',
-          address: '440 N Barranca Ave #4133<br/>Covina, CA 91723',
-          country: 'États-Unis',
-          email: 'privacy@vercel.com',
+          name: 'Scalingo',
+          address: '13 RUE JACQUES PEIROTES, 67000 STRASBOURG',
+          country: 'France',
+          email: 'support@scalingo.com',
         }}
       />
     </div>
