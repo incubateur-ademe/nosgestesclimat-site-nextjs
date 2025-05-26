ba955c7ae7cee12ffdfb8f717fe4f8ee4a44ffa1
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-05-26 15:56:51 +0200

    :bug: Use safeLocalStorage and safeSessionStorage everywhere

--- src/components/layout/header/headerDesktop/DebugIndicator.tsx
+++ src/components/layout/header/headerDesktop/DebugIndicator.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useDebug } from '@/hooks/useDebug'
+import { safeSessionStorage } from '@/utils/browser/safeSessionStorage'
 import { useRouter } from 'next/navigation'
 
 export default function DebugIndicator() {
@@ -11,7 +12,7 @@ export default function DebugIndicator() {
     <button
       className="flex items-center gap-2 rounded-xl bg-red-600 p-2 text-center text-sm font-bold text-white uppercase"
       onClick={() => {
-        sessionStorage.removeItem('debug')
+        safeSessionStorage.removeItem('debug')
         router.refresh()
       }}>
       Debug
