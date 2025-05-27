d843e30c5257769ffaeacf6ad2aa1af0cc59fe13
Paul Souche <paul@bettercallpaul.fr>
2025-05-27 13:50:14 +0200

    :sparkles: export excel from server

--- src/components/organisations/ExportDataButton.tsx
+++ src/components/organisations/ExportDataButton.tsx
@@ -1,35 +1,26 @@
 'use client'
 
-import { carboneMetric } from '@/constants/model/metric'
 import type { ButtonProps } from '@/design-system/buttons/Button'
 import Button from '@/design-system/buttons/Button'
-import { createXLSXFileAndDownload } from '@/helpers/export/createXLSXFileAndDownload'
-import { useFetchPublicPollSimulations } from '@/hooks/organisations/polls/useFetchPublicPollSimulations'
+import { fetchPollResults } from '@/services/organisations/fetchPollResults'
 import type { PublicOrganisationPoll } from '@/types/organisations'
-import dayjs from 'dayjs'
+import { captureException } from '@sentry/nextjs'
 import { useState } from 'react'
 import DownloadIcon from '../icons/DownloadIcon'
 import Trans from '../translation/trans/TransClient'
 
 type Props = {
-  poll?: PublicOrganisationPoll | null
+  poll: PublicOrganisationPoll
   color?: 'primary' | 'secondary'
   onClick?: () => void
 }
 
 export default function ExportDataButton({
-  poll,
   color = 'secondary',
   onClick,
   ...props
 }: ButtonProps & Props) {
   const [isLoading, setIsLoading] = useState(false)
-
-  const { data: simulations } = useFetchPublicPollSimulations()
-
-  if (!simulations || simulations.length < 3) return null
-
-  const pollSimulations = simulations
 
   const handleClick = () => {
     if (onClick) {
@@ -38,53 +29,15 @@ export default function ExportDataButton({
 
     setIsLoading(true)
 
-    createXLSXFileAndDownload({
-      data: pollSimulations.map((simulation) => {
-        const simulationToParse = { ...simulation }
-
-        const data: Record<string, unknown> = {
-          date: dayjs(simulationToParse.date).format('DD/MM/YYYY'),
-          total: Math.round(
-            simulationToParse.computedResults[carboneMetric].bilan
-          ),
-          transport: Math.round(
-            simulationToParse.computedResults[carboneMetric].categories
-              .transport
-          ),
-          alimentation: Math.round(
-            simulationToParse.computedResults[carboneMetric].categories
-              .alimentation
-          ),
-          logement: Math.round(
-            simulationToParse.computedResults[carboneMetric].categories.logement
-          ),
-          divers: Math.round(
-            simulationToParse.computedResults[carboneMetric].categories.divers
-          ),
-          'services sociétaux': Math.round(
-            simulationToParse.computedResults[carboneMetric].categories[
-              'services sociétaux'
-            ]
-          ),
-        }
-
-        if (poll?.customAdditionalQuestions) {
-          poll.customAdditionalQuestions.forEach(({ question }) => {
-            data[question] =
-              simulationToParse.additionalQuestionsAnswers.find(
-                ({ key }) => key === question
-              )?.answer ?? ''
-          })
-        }
-
-        return data
-      }),
-      fileName: `export-donnees-nos-gestes-climat-${dayjs().format(
-        'DD-MM-YYYY_HH-MM'
-      )}.xlsx`,
-      callback: () => setIsLoading(false),
-    })
+    fetchPollResults(props)
+      .then((data) => window.open(data.url, '_blank'))
+      .catch((error) => {
+        captureException(error)
+        // TODO display an error toast
+      })
+      .finally(() => setIsLoading(false))
   }
+
   return (
     <Button color={color} disabled={isLoading} onClick={handleClick} {...props}>
       <DownloadIcon className="fill-primary-700 mr-2" />
