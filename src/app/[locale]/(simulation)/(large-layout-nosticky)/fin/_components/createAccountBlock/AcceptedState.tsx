5ffbf3c4a21a76cda24b96626cb13c5f8c48e98b
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-06-04 13:09:57 +0200

    ✨ Add accepted state

---
+++ src/app/[locale]/(simulation)/(large-layout-nosticky)/fin/_components/createAccountBlock/AcceptedState.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import Title from '@/design-system/layout/Title'
+import { useClientTranslation } from '@/hooks/useClientTranslation'
+
+export default function AcceptedState() {
+  const { t } = useClientTranslation()
+  return (
+    <>
+      <Title
+        tag="h2"
+        className="text-lg font-bold"
+        title={t('Merci de votre intérêt !')}
+      />
+
+      <p>
+        La création de compte sera bientôt disponible, vous en serez informé
+        ici.
+      </p>
+    </>
+  )
+}
