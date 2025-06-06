b207c8d6aec84b4f8c408ce6d0a1d0650cee8843
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-06-06 13:41:24 +0200

    wip

--- src/utils/getIsIframe.ts
+++ src/utils/getIsIframe.ts
@@ -2,11 +2,8 @@ export function getIsIframe(): boolean {
   if (typeof window === 'undefined') return false
 
   try {
-    console.log('in getIsIframe, window.self', window.self)
-    console.log('window.top', window.top)
     return window.self !== window.top
   } catch (e) {
-    console.log(e)
     return true
   }
 }
