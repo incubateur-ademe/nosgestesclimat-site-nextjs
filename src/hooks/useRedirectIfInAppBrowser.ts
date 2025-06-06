62362b36a63361e6b04e2b211531f9b3e08b468c
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-06-06 12:32:16 +0200

    Update useRedirectIfInAppBrowser.ts

--- src/hooks/useRedirectIfInAppBrowser.ts
+++ src/hooks/useRedirectIfInAppBrowser.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { useIframe } from './useIframe'
 
 // We only check if the user agent is an In App browser on Android
 const rules = ['WebView', 'Android.*(wv)']
@@ -6,16 +7,17 @@ const regex = new RegExp(`(${rules.join('|')})`, 'ig')
 
 export const useRedirectIfInAppBrowser = () => {
   const [isInApp, setIsInApp] = useState(false)
+  const { isIframe } = useIframe()
 
   useEffect(() => {
     setIsInApp(Boolean(navigator.userAgent.match(regex)))
   }, [])
 
   useEffect(() => {
-    if (isInApp) {
+    if (isInApp && !isIframe) {
       window.location.href = `intent:${window.location.href}/#Intent;end`
     }
-  }, [isInApp])
+  }, [isInApp, isIframe])
 
   return { isInApp }
 }
