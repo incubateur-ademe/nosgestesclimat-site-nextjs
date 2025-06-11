48a53c1ec40f943febf6c349947a40488ef93cd8
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-06-11 19:49:27 +0200

    Update redirects.js

--- config/redirects.js
+++ config/redirects.js
@@ -220,7 +220,7 @@ const redirects = [
   },
   {
     source: '/blog/undefined/neuf-limites-planetaires-empreintes',
-    destination: '/blog',
+    destination: '/blog/environnement/neuf-limites-planetaires-empreintes',
     permanent: true,
   },
   // For now, we redirect actions/plus homepage to blog homepage. We still have each actions/plus page available but ghosted.
