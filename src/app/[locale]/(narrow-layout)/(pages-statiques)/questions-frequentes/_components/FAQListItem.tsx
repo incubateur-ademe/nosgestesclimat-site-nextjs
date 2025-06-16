85fa30a88566d02ab81da8cde51ed7eb2be0347c
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-06-16 15:28:53 +0200

    🔥 Remove useless logic

--- src/app/[locale]/(narrow-layout)/(pages-statiques)/questions-frequentes/_components/FAQListItem.tsx
+++ src/app/[locale]/(narrow-layout)/(pages-statiques)/questions-frequentes/_components/FAQListItem.tsx
@@ -1,7 +1,4 @@
-'use client'
-
 import Card from '@/design-system/layout/Card'
-import { onKeyDownHelper } from '@/helpers/accessibility/onKeyDownHelper'
 
 export default function FAQListItem({
   id,
@@ -12,41 +9,13 @@ export default function FAQListItem({
   question: string
   answer: string
 }) {
-  const handleDetailsToggle = (id: string, isOpen: boolean) => {
-    let newURL = window.location.pathname
-    if (!isOpen) {
-      newURL = window.location.pathname + `#${id}`
-    }
-    window.history.pushState(null, '', newURL)
-  }
-
   return (
     <li key={id} className="whitespace-wrap mb-2 list-none font-bold">
       <details id={id}>
         <summary
           role="button"
           tabIndex={0}
-          className="cursor-pointer border-none bg-transparent text-left text-base"
-          onClick={(e) =>
-            handleDetailsToggle(
-              id,
-              (
-                e?.currentTarget?.parentElement as HTMLElement & {
-                  open: boolean
-                }
-              )?.open ?? false
-            )
-          }
-          onKeyDown={onKeyDownHelper((e) =>
-            handleDetailsToggle(
-              id,
-              (
-                e?.currentTarget?.parentElement as HTMLElement & {
-                  open: boolean
-                }
-              )?.open ?? false
-            )
-          )}>
+          className="cursor-pointer border-none bg-transparent text-left text-base">
           <h3 className="inline text-black">{question}</h3>
         </summary>
 
