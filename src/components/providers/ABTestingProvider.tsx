c5c45c5f1227310d5db4fd6717d9787393a1f8a2
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-06-16 12:27:30 +0200

    ✨ Add ABTestingProvider

---
+++ src/components/providers/ABTestingProvider.tsx
@@ -0,0 +1,66 @@
+import { createContext, PropsWithChildren, useEffect, useState } from 'react'
+
+const ABTestingContext = createContext({})
+
+export const ABTestingProvider = ({ children }: PropsWithChildren) => {
+  const [abTests, setABTests] = useState<Record<string, boolean>>({})
+
+  useEffect(() => {
+    handleInitABTesting()
+  }, [])
+
+  const handleInitABTesting = () => {
+    // Vérifier si Matomo est activé
+    if (
+      process.env.NEXT_PUBLIC_MATOMO_ID !== '1' &&
+      process.env.NEXT_PUBLIC_MATOMO_ID !== '2'
+    ) {
+      console.log('Matomo not enabled in this environment')
+      return
+    }
+
+    const _paq = window._paq || undefined
+
+    if (!_paq) {
+      console.log('Matomo not initialized, retrying in 500ms.')
+      setTimeout(() => handleInitABTesting(), 500)
+      return
+    }
+
+    _paq.push([
+      'AbTesting::create',
+      {
+        name: '10',
+        includedTargets: [
+          { attribute: 'url', inverted: '0', type: 'any', value: '' },
+        ],
+        excludedTargets: [],
+        variations: [
+          {
+            name: 'original',
+            activate: function (event: any) {
+              // usually nothing needs to be done here
+            },
+          },
+          {
+            name: '17',
+            activate: function (event: any) {
+              setABTests({
+                'hide-tutorial': true,
+              })
+            },
+          },
+        ],
+      },
+    ])
+  }
+
+  return (
+    <ABTestingContext
+      value={{
+        abTests,
+      }}>
+      {children}
+    </ABTestingContext>
+  )
+}
