1da2aa0b108e5ee33ce5e3a8bb4882d1e828a69b
Benjamin Arias <bjlaa@proton.me>
2025-06-18 17:23:45 +0200

    ✨ Add Matomo AB testing system, test hiding /tutoriel (#1091)
    Co-authored-by: Benjamin Arias <12382534+bjlaa@users.noreply.github.com>

---
+++ src/components/providers/ABTestingProvider.tsx
@@ -0,0 +1,102 @@
+import {
+  abTestingVisitOriginal,
+  getAbTestingVisitVariation,
+} from '@/constants/tracking/ab-testing'
+import { trackEvent } from '@/utils/analytics/trackEvent'
+import {
+  createContext,
+  type PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
+
+type ABTestingContextType = {
+  abTests: Record<string, boolean>
+}
+
+const ABTestingContext = createContext<ABTestingContextType>({
+  abTests: {},
+})
+
+export const AB_TESTS_LABELS = {
+  hideTutorial: 'hide tutorial',
+}
+
+export const ABTestingProvider = ({ children }: PropsWithChildren) => {
+  const [abTests, setABTests] = useState<Record<string, boolean>>({})
+
+  const handleInitABTesting = () => {
+    console.log('[AB Testing] in handleInitABTesting')
+    // Vérifier si Matomo est activé
+    if (
+      process.env.NEXT_PUBLIC_MATOMO_ID !== '1' &&
+      process.env.NEXT_PUBLIC_MATOMO_ID !== '2'
+    ) {
+      console.log('[AB Testing] Matomo not enabled in this environment')
+      return
+    }
+
+    const _paq = window._paq || undefined
+    const Matomo = window.Matomo
+
+    if (!Matomo) {
+      setTimeout(() => handleInitABTesting(), 500)
+      return
+    }
+
+    if (!Matomo.AbTesting) return
+
+    _paq.push([
+      'AbTesting::create',
+      {
+        name: 'AvecOuSansTutoriel',
+        includedTargets: [
+          { attribute: 'url', inverted: '0', type: 'any', value: '' },
+        ],
+        excludedTargets: [],
+        variations: [
+          {
+            name: 'original',
+            activate: function (event: any) {
+              // usually nothing needs to be done here
+              console.log('[AB Testing] Original version')
+
+              trackEvent(abTestingVisitOriginal)
+            },
+          },
+          {
+            name: 'Variation1',
+            activate: function (event: any) {
+              console.log('[AB Testing] Hide tutorial version')
+
+              setABTests({
+                [AB_TESTS_LABELS.hideTutorial]: true,
+              })
+
+              trackEvent(
+                getAbTestingVisitVariation(AB_TESTS_LABELS.hideTutorial)
+              )
+            },
+          },
+        ],
+      },
+    ])
+  }
+
+  useEffect(() => {
+    handleInitABTesting()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <ABTestingContext
+      value={{
+        abTests,
+      }}>
+      {children}
+    </ABTestingContext>
+  )
+}
+
+export const useABTesting = () => useContext(ABTestingContext)
