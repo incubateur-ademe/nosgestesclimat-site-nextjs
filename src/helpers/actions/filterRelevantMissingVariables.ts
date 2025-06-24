68e97dad4a8cd5e57aa38cc81848e489f20268df
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-06-24 17:50:17 +0200

    ♻️ Factorize

--- src/helpers/actions/filterRelevantMissingVariables.ts
+++ src/helpers/actions/filterRelevantMissingVariables.ts
@@ -4,29 +4,6 @@ import type { MissingVariables } from '@/publicodes-state/types'
 import type { DottedName } from '@incubateur-ademe/nosgestesclimat'
 import type { EvaluatedNode, PublicodesExpression } from 'publicodes'
 
-// Gathered from nosgestesclimat-site
-const filteredDottedNames: DottedName[] = [
-  'divers . publicité',
-  'services sociétaux . pression locale',
-  'services sociétaux . voter',
-  'divers . aider les autres',
-  'divers . partage NGC',
-  'transport . infolettre',
-  'métrique',
-  'déforestation',
-  'logement . électricité verte',
-  'logement . chauffage . électricité',
-  'futureco-data . transport . ferry . distance aller . orthodromique',
-  'futureco-data . transport . ferry . durée du voyage',
-  'futureco-data . transport . ferry . vitesse en kmh',
-  'futureco-data . transport . ferry . cabine',
-  'futureco-data . transport . ferry . groupe',
-  'futureco-data . transport . ferry . consommation de services',
-  'futureco-data . transport . ferry . voiture',
-  'services sociétaux . devenir producteur photovoltaique',
-  'services sociétaux . bien placer argent',
-]
-
 export const filterRelevantMissingVariables = ({
   missingVariables,
   extendedFoldedSteps,
@@ -42,7 +19,6 @@ export const filterRelevantMissingVariables = ({
 }) => {
   return missingVariables.filter((dottedName: DottedName) => {
     const isFolded = extendedFoldedSteps.indexOf(dottedName) >= 0
-    const isManuallyExcluded = filteredDottedNames?.includes(dottedName)
     const isMustNotAskQuestion = MUST_NOT_ASK_QUESTIONS?.has(dottedName)
     const isRelevantQuestion = everyQuestions.includes(dottedName)
 
@@ -53,11 +29,7 @@ export const filterRelevantMissingVariables = ({
     })
 
     return (
-      !isManuallyExcluded &&
-      !isFolded &&
-      !isMustNotAskQuestion &&
-      isRelevantQuestion &&
-      isApplicable
+      !isFolded && !isMustNotAskQuestion && isRelevantQuestion && isApplicable
     )
   })
 }
