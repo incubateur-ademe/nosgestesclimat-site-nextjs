caeaf3d6a8dfc979ea1412b408af7b1916cdeece
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-07-01 17:55:33 +0200

    ♻️ Remove logs

--- src/utils/analytics/trackGTMEvent.ts
+++ src/utils/analytics/trackGTMEvent.ts
@@ -11,26 +11,5 @@ export const trackGTMEvent = (event: { event: string }) => {
     return
   }
 
-  // Log avant l'envoi
-  console.debug(`GTM Event about to be sent:`, event)
-  console.debug(`Current dataLayer length:`, window.dataLayer.length)
-
   window.dataLayer.push(event)
-
-  // Log après l'envoi
-  console.debug(`GTM Event sent: ${event.event}`)
-  console.debug(`New dataLayer length:`, window.dataLayer.length)
-  console.debug(
-    `Latest dataLayer entry:`,
-    window.dataLayer[window.dataLayer.length - 1]
-  )
-
-  // Vérifier si GTM est bien configuré
-  if (window.gtag) {
-    console.debug('GTM gtag function is available')
-  } else {
-    console.debug(
-      'GTM gtag function not yet available (this is normal during loading)'
-    )
-  }
 }
