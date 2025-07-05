c962f102a9be23f4cad396f38f5b95c60eda42a8
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-07-05 17:29:31 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -98,17 +98,12 @@ const nextConfig: NextConfig = {
 }
 
 const sentryConfig = {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-
   // Suppresses source map uploading logs during build
   silent: true,
   org: 'incubateur-ademe',
   project: 'nosgestesclimat-nextjs',
 
   authToken: process.env.SENTRY_AUTH_TOKEN,
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: false,
