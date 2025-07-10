4ff1518cf182044bea6803ebf7c9129d6ef78f87
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-07-10 18:02:44 +0200

    💄 Improve legend style

--- src/components/landing-pages/Legend.tsx
+++ src/components/landing-pages/Legend.tsx
@@ -1,7 +1,6 @@
 export default function Legend({ htmlLegend }: { htmlLegend: string }) {
-  console.log(htmlLegend)
   return (
-    <div className="mx-auto w-full max-w-screen px-2 pt-18 pb-12 md:max-w-5xl md:px-0">
+    <div className="mx-auto flex w-full max-w-screen justify-center px-2 pt-18 pb-12 md:max-w-5xl md:px-0">
       <div
         className="markdown mt-12 text-sm"
         dangerouslySetInnerHTML={{ __html: htmlLegend }}
