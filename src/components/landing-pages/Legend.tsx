c3522035a6fa929034d233690e3df90a33847f8d
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-07-10 09:21:39 +0200

    💄 Improve list style

--- src/components/landing-pages/Legend.tsx
+++ src/components/landing-pages/Legend.tsx
@@ -1,8 +1,11 @@
 export default function Legend({ htmlLegend }: { htmlLegend: string }) {
+  console.log(htmlLegend)
   return (
-    <div
-      className="mt-12 text-sm italic"
-      dangerouslySetInnerHTML={{ __html: htmlLegend }}
-    />
+    <div className="mx-auto w-full max-w-screen px-2 py-12 md:max-w-5xl md:px-0">
+      <div
+        className="markdown mt-12 text-sm"
+        dangerouslySetInnerHTML={{ __html: htmlLegend }}
+      />
+    </div>
   )
 }
