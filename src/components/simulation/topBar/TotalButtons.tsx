552c0c6a5e175bd22aa7bce5e0a2e730b0ec1131
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-07-10 10:35:10 +0200

    ♻️ Display question list only when debugging

--- src/components/simulation/topBar/TotalButtons.tsx
+++ src/components/simulation/topBar/TotalButtons.tsx
@@ -5,6 +5,7 @@ import SaveCheckIcon from '@/components/icons/SaveCheckIcon'
 import SaveIcon from '@/components/icons/SaveIcon'
 import Trans from '@/components/translation/trans/TransClient'
 import Button from '@/design-system/buttons/Button'
+import { useDebug } from '@/hooks/useDebug'
 import { useCurrentSimulation } from '@/publicodes-state'
 
 type Props = { toggleQuestionList: () => void; toggleSaveModal?: () => void }
@@ -15,26 +16,30 @@ export default function TotalButtons({
 }: Props) {
   const { savedViaEmail } = useCurrentSimulation()
 
+  const isDebug = useDebug()
+
   return (
     <div className="flex">
-      <Button
-        color="text"
-        size="sm"
-        className="h-10 w-10 p-0! font-medium lg:w-auto lg:gap-2 lg:px-4! lg:py-2!"
-        onClick={() => {
-          toggleQuestionList()
-        }}>
-        <ListIcon className="fill-primary-700 h-6 w-6" />
-        <span className="hidden lg:inline">
-          <Trans>Liste des questions</Trans>
-        </span>
-      </Button>
+      {isDebug && (
+        <Button
+          color="text"
+          size="sm"
+          className="h-10 w-10 p-0! font-medium lg:w-auto lg:gap-2 lg:px-4! lg:py-2!"
+          onClick={() => {
+            toggleQuestionList()
+          }}>
+          <ListIcon className="fill-primary-700 h-6 w-6" />
+          <span className="hidden lg:inline">
+            <Trans>Liste des questions</Trans>
+          </span>
+        </Button>
+      )}
 
       {toggleSaveModal ? (
         <Button
           color="text"
           size="sm"
-          className="h-10 w-10 p-0! font-medium lg:w-auto lg:gap-2 lg:px-4! lg:py-2!"
+          className="h-10 w-10 gap-2 p-0! font-medium sm:w-auto lg:px-4! lg:py-2!"
           onClick={() => {
             toggleSaveModal()
           }}>
@@ -43,7 +48,7 @@ export default function TotalButtons({
           ) : (
             <SaveIcon className="fill-primary-700 h-6 w-6" />
           )}
-          <span className="hidden lg:inline">
+          <span className="hidden sm:inline">
             <Trans>Reprendre plus tard</Trans>
           </span>
         </Button>
