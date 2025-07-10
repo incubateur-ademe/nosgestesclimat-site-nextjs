042fd2ccdfd0894409164272ee91620bcfb8482f
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-07-10 09:47:09 +0200

    Update preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,7 +3,14 @@ import { initialize, mswLoader } from 'msw-storybook-addon'
 import '../src/app/[locale]/globals.css'
 
 // Initialize MSW
-initialize()
+initialize({
+  serviceWorker: {
+    url:
+      process.env.NODE_ENV === 'production'
+        ? '/nosgestesclimat-site-nextjs/mockServiceWorker.js'
+        : '/mockServiceWorker.js',
+  },
+})
 
 const preview: Preview = {
   parameters: {
