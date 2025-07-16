bc4679b2f7c8a26535d19539af1e14a57c47e7a3
Benjamin Arias <bjlaa@proton.me>
2025-07-16 08:59:41 +0200

    🐛 Fix display of content in MotivationSection component (#1170)
    Co-authored-by: Benjamin Arias <12382534+bjlaa@users.noreply.github.com>

--- src/design-system/layout/Separator.tsx
+++ src/design-system/layout/Separator.tsx
@@ -1,12 +1,13 @@
 import { twMerge } from 'tailwind-merge'
 
-export default function Separator({ className = '' }) {
+export default function Separator({ className = '', ...props }) {
   return (
     <div
       className={twMerge(
         'bg-secondary-600 my-8 h-[3px] w-12 rounded-full md:w-20',
         className
       )}
+      {...props}
     />
   )
 }
