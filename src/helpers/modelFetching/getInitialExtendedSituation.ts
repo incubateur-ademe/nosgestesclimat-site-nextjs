c0bb0cecc612e4ef60f585fdee10d2826b53e71b
Clément <clem.auger@hotmail.fr>
2025-07-31 10:45:35 +0200

    :label: fix typing

--- src/helpers/modelFetching/getInitialExtendedSituation.ts
+++ src/helpers/modelFetching/getInitialExtendedSituation.ts
@@ -1,9 +1,10 @@
 import type { ExtendedSituation } from '@incubateur-ademe/nosgestesclimat'
 import initialExtendedSituation from '@incubateur-ademe/nosgestesclimat/public/initialExtendedSituation.json'
-
 /**
  * This function is used to get the initial extended situation. It can be called directly from a server component.
  */
 export function getInitialExtendedSituation(): ExtendedSituation {
-  return initialExtendedSituation
+  // Fixme: shouldn't be necessary to explicitly cast this to ExtendedSituation
+  // but the type is not correctly inferred from the JSON import.
+  return initialExtendedSituation as ExtendedSituation
 }
