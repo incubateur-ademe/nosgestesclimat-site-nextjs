0f375710e4b9e8168d16f684393d61adfc54f5e2
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-08-04 12:51:51 +0200

    Update DidYouKnowSlider.tsx

--- src/components/landing-pages/DidYouKnowSlider.tsx
+++ src/components/landing-pages/DidYouKnowSlider.tsx
@@ -6,8 +6,8 @@ import {
   getLandingDidYouKnowSlider,
   getLandingDidYouKnowSliderValue,
 } from '@/helpers/tracking/landings'
-// @ts-expect-error package types are wrongly exported
 import { useClientTranslation } from '@/hooks/useClientTranslation'
+// @ts-expect-error package types are wrongly exported
 import { Splide, SplideSlide } from '@splidejs/react-splide'
 import '@splidejs/react-splide/css'
 import dynamic from 'next/dynamic'
