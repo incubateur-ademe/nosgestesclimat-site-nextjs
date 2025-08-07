d47ff60a031b3d41509d8cd51086de3a9ed93e17
Benjamin Arias <bjlaa@proton.me>
2025-08-07 16:46:21 +0200

    ♻️ Improve iframes accessibility (#1219)
    Co-authored-by: Benjamin Arias <12382534+bjlaa@users.noreply.github.com>

--- src/app/[locale]/(narrow-layout)/(pages-statiques)/contact/page.tsx
+++ src/app/[locale]/(narrow-layout)/(pages-statiques)/contact/page.tsx
@@ -60,11 +60,11 @@ export default async function Contact({ params }: DefaultPageProps) {
       </div>
       <Card>
         <iframe
+          title={t('Formulaire - nous faire part de vos remarques sur le site')}
           data-tally-src="https://tally.so/embed/w59G1Z?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
           loading="lazy"
           width="100%"
-          height="1306"
-          title="Vous avez un retour sur Nos Gestes Climat ?"></iframe>
+          height="1306"></iframe>
       </Card>
 
       <Script id="tally">{`var d=document,w="https://tally.so/widgets/embed.js",v=function(){"undefined"!=typeof Tally?Tally.loadEmbeds():d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((function(e){e.src=e.dataset.tallySrc}))};if("undefined"!=typeof Tally)v();else if(d.querySelector('script[src="'+w+'"]')==null){var s=d.createElement("script");s.src=w,s.onload=v,s.onerror=v,d.body.appendChild(s);}`}</Script>
