d19af6c9ce0c72c743d5875be65b7989a72541f1
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-08-26 16:59:08 +0200

    ♻️ Include iframes in AB tests

--- src/hooks/abTesting/useIsTestVersion.ts
+++ src/hooks/abTesting/useIsTestVersion.ts
@@ -1,12 +1,8 @@
 import { DEFAULT_TEST_VARIANT_KEY } from '@/constants/ab-test'
-import { getIsIframe } from '@/utils/getIsIframe'
 import { useFeatureFlagVariantKey } from 'posthog-js/react'
 
 export const useIsTestVersion = (variantKey: string) => {
-  // Always exclude iframes from AB testing
-  const isIframe = getIsIframe()
-
   const flagValue = useFeatureFlagVariantKey(variantKey)
 
-  return !isIframe && flagValue === DEFAULT_TEST_VARIANT_KEY
+  return flagValue && flagValue === DEFAULT_TEST_VARIANT_KEY
 }
