e5895e800417d67d7c041a09f97fc59d36ffa1fa
Clément <clem.auger@hotmail.fr>
2025-08-27 15:02:05 +0200

    feat: use rulesToHide option from publicodes/react-ui

--- src/app/[locale]/documentation/[...slug]/_components/documentationRouter/documentationServer/calculDetail/RuleDetail.tsx
+++ src/app/[locale]/documentation/[...slug]/_components/documentationRouter/documentationServer/calculDetail/RuleDetail.tsx
@@ -1,4 +1,5 @@
 import Link from '@/components/Link'
+import { KEYS_TO_OMIT, RULES_TO_HIDE } from '@/constants/documentation'
 import { capitalizeString } from '@/utils/capitalizeString'
 import { encodeRuleName } from '@/utils/publicodes/encodeRuleName'
 import type {
@@ -7,25 +8,6 @@ import type {
   NGCRules,
 } from '@incubateur-ademe/nosgestesclimat'
 import { utils } from 'publicodes'
-
-const KEYS_TO_OMIT = [
-  'titre',
-  'couleur',
-  'abréviation',
-  'icônes',
-  'description',
-  'résumé',
-  'exposé',
-  'unité',
-  'question',
-  'note',
-  'références',
-  // specific to NGC actions
-  'effort',
-  'inactive',
-  // specific to NGC form generation, could be cool to visualize, but in a <details> tag, since it's big
-  'mosaique',
-]
 
 const getRuleFormatted = (rule: NGCRule): NGCRule => {
   const ruleFormatted = { ...rule }
@@ -84,8 +66,14 @@ export default function RuleDetail({
     }
   }
 
+  const rulesToHideSet = new Set(RULES_TO_HIDE)
+
   if (typeof ruleFormatted === 'number') {
-    return <span>{ruleFormatted}</span>
+    if (rulesToHideSet.has(context.dottedName)) {
+      return <span>masquée par l'intégrateur</span>
+    } else {
+      return <span>{ruleFormatted}</span>
+    }
   }
 
   if (isArray) {
