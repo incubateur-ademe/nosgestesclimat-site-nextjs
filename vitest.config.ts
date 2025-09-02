89873b9d0dbcc34b31274ce8dd1a1e2113f40ca5
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-09-02 17:01:10 +0200

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import * as path from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  // @ts-expect-error - Conflict between vite and vitest versions
   plugins: [react()],
   test: {
     globals: true,
