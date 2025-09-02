c7605cfaca36a63d7e56d52191df58719519ffc8
Benjamin Arias <bjlaa@proton.me>
2025-09-02 11:16:09 +0200

    ♻️ Exclude review apps and preprod from AB tests (#1262)
    Co-authored-by: Benjamin Arias <12382534+bjlaa@users.noreply.github.com>

--- src/hooks/abTesting/useIsTestVersion.ts
+++ src/hooks/abTesting/useIsTestVersion.ts
@@ -2,6 +2,14 @@ import { DEFAULT_TEST_VARIANT_KEY } from '@/constants/ab-test'
 import { useFeatureFlagVariantKey } from 'posthog-js/react'
 
 export const useIsTestVersion = (variantKey: string) => {
+  // Disable the AB testing in the preview environments
+  if (
+    process.env.NEXT_PUBLIC_ENV !== 'production' &&
+    process.env.NEXT_PUBLIC_ENV !== 'development'
+  ) {
+    return false
+  }
+
   const flagValue = useFeatureFlagVariantKey(variantKey)
 
   return flagValue && flagValue === DEFAULT_TEST_VARIANT_KEY
