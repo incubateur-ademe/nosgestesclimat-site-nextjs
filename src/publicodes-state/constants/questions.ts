4d0b0e45bf7962a51d0e9b7138ca8d247c7022e2
Clément <clem.auger@hotmail.fr>
2025-09-08 17:16:29 +0200

    feat: hide mosaiques meubles & electromenager

--- src/publicodes-state/constants/questions.ts
+++ src/publicodes-state/constants/questions.ts
@@ -3,6 +3,8 @@ import type { DottedName } from '@incubateur-ademe/nosgestesclimat'
 export const MUST_NOT_ASK_QUESTIONS: Set<DottedName> = new Set([
   'divers . textile . empreinte précise',
   'divers . textile . choix précis',
+  'divers . ameublement . meubles',
+  'divers . électroménager . appareils',
 ])
 
 // We use the DottedName type from nosgestesclimat to make sure the build will break when using rules that are not in the model.
