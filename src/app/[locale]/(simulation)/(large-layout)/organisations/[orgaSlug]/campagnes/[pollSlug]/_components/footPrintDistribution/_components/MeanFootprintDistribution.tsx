e76cab0fbd2b25ca5a5bfd18c026c51f17044370
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-09-09 16:12:23 +0200

    ✨ Add radar chart

--- src/app/[locale]/(simulation)/(large-layout)/organisations/[orgaSlug]/campagnes/[pollSlug]/_components/footPrintDistribution/_components/MeanFootprintDistribution.tsx
+++ src/app/[locale]/(simulation)/(large-layout)/organisations/[orgaSlug]/campagnes/[pollSlug]/_components/footPrintDistribution/_components/MeanFootprintDistribution.tsx
@@ -1,6 +1,8 @@
 import Trans from '@/components/translation/trans/TransClient'
 import Card from '@/design-system/layout/Card'
+import { useIsOrganisationAdmin } from '@/hooks/organisations/useIsOrganisationAdmin'
 import CategoryDistribution from './CategoryDistribution'
+import CategoryRadarChart from './CategoryRadarChart'
 import FootprintBarChart from './FootprintBarChart'
 
 type Props = {
@@ -14,6 +16,8 @@ export default function MeanFootprintDistribution({
   groupFootprint,
   userFootprint,
 }: Props) {
+  const { isAdmin } = useIsOrganisationAdmin()
+
   return (
     <div>
       <div className="flex flex-col gap-8 md:flex-row">
@@ -33,15 +37,34 @@ export default function MeanFootprintDistribution({
           </Card>
         </div>
         <div className="w-full">
-          <CategoryDistribution
-            categoryValues={{
-              transport: 1,
-              alimentation: 2,
-              logement: 3,
-              divers: 4,
-              'services sociétaux': 5,
-            }}
-          />
+          {isAdmin ? (
+            <CategoryDistribution
+              categoryValues={{
+                transport: 1,
+                alimentation: 2,
+                logement: 3,
+                divers: 4,
+                'services sociétaux': 5,
+              }}
+            />
+          ) : (
+            <CategoryRadarChart
+              userValues={{
+                transport: 2.5,
+                alimentation: 3.8,
+                logement: 1.2,
+                divers: 4.5,
+                'services sociétaux': 5.2,
+              }}
+              averageValues={{
+                transport: 0.8,
+                alimentation: 1.5,
+                logement: 0.3,
+                divers: 0.6,
+                'services sociétaux': 1.2,
+              }}
+            />
+          )}
         </div>
       </div>
     </div>
