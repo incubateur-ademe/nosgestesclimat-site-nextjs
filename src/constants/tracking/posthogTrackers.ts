bffd0199e48cf4082bcfb2d59281824a080599a6
Clément <clem.auger@hotmail.fr>
2025-09-10 14:27:09 +0200

    📈 send anonymized url only to matomo
    ♻️ rename variable
    
    :chart_with_upwards_trend: send orga events to posthog
    
    📈 send region and language events to posthog
    
    :white_check_mark: fix test

--- src/constants/tracking/posthogTrackers.ts
+++ src/constants/tracking/posthogTrackers.ts
@@ -10,7 +10,11 @@ type Props = {
   bilanEau?: number
   actionType?: 'précédent' | 'suivant' | 'passer'
   state?: 'opened' | 'closed'
+  locale?: string //should be Locale
+  region?: string
 }
+
+// Form
 
 export const captureClickFormNav = ({
   question,
@@ -83,6 +87,8 @@ export const captureSimulationCompleted = ({
   }
 }
 
+// Iframe
+
 export const captureIframeVisit = (url: string) => {
   return {
     eventName: 'iframe visit',
@@ -97,6 +103,118 @@ export const captureIframeInteraction = (url: string) => {
     eventName: 'iframe interaction',
     properties: {
       referrerUrl: url,
+    },
+  }
+}
+
+// Orga
+
+export const captureDownloadKit = () => {
+  return {
+    eventName: 'click download kit',
+    properties: {
+      module: 'organisation board',
+    },
+  }
+}
+
+export const captureClickAteliers = () => {
+  return {
+    eventName: 'click ateliers',
+    properties: {
+      module: 'organisation board',
+    },
+  }
+}
+
+export const captureClickImpactCo2 = () => {
+  return {
+    eventName: 'click impact co2',
+    properties: {
+      module: 'organisation board',
+    },
+  }
+}
+
+export const captureCopyPollLink = () => {
+  return {
+    eventName: 'copy poll link',
+    properties: {
+      module: 'poll dashboard',
+    },
+  }
+}
+
+export const captureClickFunFactsPlus = () => {
+  return {
+    eventName: 'click poll fun facts plus',
+    properties: {
+      module: 'poll board',
+    },
+  }
+}
+
+export const captureDownloadFunFactsPlus = () => {
+  return {
+    eventName: 'download poll fun facts plus',
+    properties: {
+      module: 'poll board',
+    },
+  }
+}
+
+export const captureDownloadPollQRCode = () => {
+  return {
+    eventName: 'download poll QR code',
+    properties: {
+      module: 'poll dashboard',
+    },
+  }
+}
+
+export const captureClickPollSettings = () => {
+  return {
+    eventName: 'click poll settings',
+    properties: {
+      module: 'poll dashboard',
+    },
+  }
+}
+
+export const captureExportPollData = () => {
+  return {
+    eventName: 'export poll data',
+    properties: {
+      module: 'poll dashboard',
+    },
+  }
+}
+
+// Language / Region
+
+export const captureLocale = ({ locale }: Props) => {
+  return {
+    eventName: 'Locale used',
+    properties: {
+      language: locale,
+    },
+  }
+}
+
+export const captureFooterSwitchLanguage = (locale: string) => {
+  return {
+    eventName: 'Switch Language in Footer',
+    properties: {
+      language: locale,
+    },
+  }
+}
+
+export const captureRegion = ({ region }: Props) => {
+  return {
+    eventName: 'Region used',
+    properties: {
+      region,
     },
   }
 }
