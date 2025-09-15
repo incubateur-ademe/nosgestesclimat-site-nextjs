fe3b2dda7dd83379ae616fa41de96d407c3e0595
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-09-15 11:59:58 +0200

    Revert "Update i18nMiddleware.ts"
    This reverts commit c82c85cf92e1b2993c9d261f3afe48aaae79afed.

--- src/middlewares/i18nMiddleware.ts
+++ src/middlewares/i18nMiddleware.ts
@@ -3,37 +3,7 @@ import { i18nRouter } from 'next-i18n-router'
 import { type NextRequest } from 'next/server'
 
 function i18nMiddleware(request: NextRequest) {
-  const response = i18nRouter(request, i18nConfig)
-
-  const detectedLocale =
-    response.headers.get('x-next-i18n-router-locale') ||
-    i18nConfig.defaultLocale
-
-  const existingCookie = request.cookies.get('NEXT_LOCALE')?.value
-
-  if (!existingCookie || existingCookie !== detectedLocale) {
-    const days = 30
-    const date = new Date()
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
-    const expires = '; expires=' + date.toUTCString()
-
-    const cookieString = `NEXT_LOCALE=${detectedLocale};expires=${expires}; path=/; SameSite=None; Secure`
-
-    response.cookies.set('NEXT_LOCALE', detectedLocale, {
-      expires: date,
-      path: '/',
-      sameSite: 'none',
-      secure: true,
-    })
-
-    console.log('�� Setting NEXT_LOCALE cookie in middleware:', {
-      detectedLocale,
-      existingCookie,
-      cookieString,
-    })
-  }
-
-  return response
+  return i18nRouter(request, i18nConfig)
 }
 
 export default i18nMiddleware
