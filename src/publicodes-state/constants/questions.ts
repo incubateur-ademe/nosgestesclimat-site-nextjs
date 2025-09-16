6cff3218c232df64d39c36491e47e6a9bd40dc01
Clément <clem.auger@hotmail.fr>
2025-09-16 16:50:20 +0200

    🐛 ameublement and electromenager mosaic children must not be asked within actions

--- src/publicodes-state/constants/questions.ts
+++ src/publicodes-state/constants/questions.ts
@@ -1,10 +1,34 @@
 import type { DottedName } from '@incubateur-ademe/nosgestesclimat'
 
 export const MUST_NOT_ASK_QUESTIONS: Set<DottedName> = new Set([
+  // Mosaic "Textile" is hidden but can be answered is "choix précis" is set to "oui".
   'divers . textile . empreinte précise',
   'divers . textile . choix précis',
+  // Mosaic "Ameublement" and "Électroménager" questions must not be asked, as their children.
   'divers . ameublement . meubles',
   'divers . électroménager . appareils',
+  'divers . ameublement . meubles . armoire . nombre',
+  'divers . ameublement . meubles . canapé . nombre',
+  'divers . ameublement . meubles . matelas . nombre',
+  'divers . ameublement . meubles . lit . nombre',
+  'divers . ameublement . meubles . table . nombre',
+  'divers . ameublement . meubles . chaise . nombre',
+  'divers . ameublement . meubles . petit meuble . nombre',
+  'divers . ameublement . meubles . grand meuble . nombre',
+  'divers . électroménager . appareils . réfrigérateur . nombre',
+  'divers . électroménager . appareils . petit réfrigérateur . nombre',
+  'divers . électroménager . appareils . congélateur . nombre',
+  'divers . électroménager . appareils . lave-linge . nombre',
+  'divers . électroménager . appareils . sèche-linge . nombre',
+  'divers . électroménager . appareils . lave-vaisselle . nombre',
+  'divers . électroménager . appareils . four . nombre',
+  'divers . électroménager . appareils . micro-onde . nombre',
+  'divers . électroménager . appareils . plaques . nombre',
+  'divers . électroménager . appareils . hotte . nombre',
+  'divers . électroménager . appareils . bouilloire . nombre',
+  'divers . électroménager . appareils . cafetière . nombre',
+  'divers . électroménager . appareils . aspirateur . nombre',
+  'divers . électroménager . appareils . robot cuisine . nombre',
 ])
 
 // We use the DottedName type from nosgestesclimat to make sure the build will break when using rules that are not in the model.
