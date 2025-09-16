ffceda5f336d926a9452dc7fda67a5c8716895e1
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-09-16 17:47:49 +0200

    Update getPersonas.ts

--- src/helpers/modelFetching/getPersonas.ts
+++ src/helpers/modelFetching/getPersonas.ts
@@ -5,15 +5,16 @@ import personasFR from '@incubateur-ademe/nosgestesclimat/public/personas-fr.jso
 import { importPreviewFile } from './importPreviewFile'
 
 const personasByLocale: Record<string, Personas> = {
-  /* TypeScript widens JSON string literals to `string`, so values like "answered"or "default" don't match the expected union type. We import the JSON as `unknown` and assert it to `Personas` to bypass this limitation.*/
-  fr: personasFR as unknown as Personas,
-  en: personasEN as unknown as Personas,
-  es: personasES as unknown as Personas,
+  fr: personasFR as Personas,
+  en: personasEN as Personas,
+  es: personasES as Personas,
 }
+
 type Props = {
   locale?: string
   PRNumber?: string
 }
+
 /*
  * This function is used to get the personas. It can be called directly from a server component.
  */
