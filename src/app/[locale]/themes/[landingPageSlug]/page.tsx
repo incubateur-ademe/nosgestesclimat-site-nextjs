51e8df5911ba3d51e63d13eec37378dc5454af6c
Benjamin Arias <bjlaa@proton.me>
2025-09-17 15:53:38 +0200

    ♻️  SEO fixes (#1312)
    Co-authored-by: Benjamin Arias <12382534+bjlaa@users.noreply.github.com>

--- src/app/[locale]/themes/[landingPageSlug]/page.tsx
+++ src/app/[locale]/themes/[landingPageSlug]/page.tsx
@@ -196,7 +196,7 @@ export default async function ThematicLandingPage({
         {block4 && (
           <DidYouKnowSlider
             slides={block4?.map(({ text, image, pinkText }) => ({
-              content: text,
+              content: <div dangerouslySetInnerHTML={{ __html: text}} />,
               illustration: image?.url ?? '',
               highlight: pinkText ?? '',
             }))}
