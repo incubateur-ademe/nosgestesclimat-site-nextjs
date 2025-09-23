afb3d459b9c82d0df7c80519e3d5998f9ca61228
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-09-23 10:55:17 +0200

    ♻️ Iframes

---
+++ src/app/[locale]/(narrow-layout)/(pages-statiques)/contact/_components/ContactIframe.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { useClientTranslation } from "@/hooks/useClientTranslation"
+
+export function ContactIframe() {
+  const { t } = useClientTranslation()
+
+  return (
+    <iframe
+    data-tally-src="https://tally.so/embed/w59G1Z?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+    loading="lazy"
+    id="iframe-contact"
+    onLoad={() => {
+      if (typeof window === 'undefined') return 
+      
+      const iframe = document.querySelector('#iframe-contact')
+      if (iframe) {
+        iframe.setAttribute('title', t('Formulaire - nous faire part de vos remarques sur le site'))
+      }
+    }}
+    ></iframe>
+  )
+}
