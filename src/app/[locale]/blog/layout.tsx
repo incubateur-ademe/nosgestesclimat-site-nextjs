1596683b5be83c68fa58b24a73b9f61081dc1c7b
Paul Souche <paul@bettercallpaul.fr>
2025-10-02 15:18:00 +0200

    :recycle: extract client part of the root layout (#1335)
    Co-authored-by: Benjamin Arias <bjlaa@proton.me>

--- src/app/[locale]/blog/layout.tsx
+++ src/app/[locale]/blog/layout.tsx
@@ -1,14 +1,20 @@
 import Header from '@/components/layout/Header'
+import type { DefaultPageProps } from '@/types'
 import type { PropsWithChildren } from 'react'
+import { ClientLayout } from '../_components/ClientLayout'
 
-export default function BlogLayout({ children }: PropsWithChildren) {
+type LayoutProps = PropsWithChildren & DefaultPageProps
+
+export default async function BlogLayout({ children, params }: LayoutProps) {
+  const { locale } = await params
+
   return (
-    <>
+    <ClientLayout locale={locale}>
       <Header isSticky={false} />
 
       <main className="flex w-full flex-1 flex-col overflow-visible lg:mx-auto lg:px-0">
         {children}
       </main>
-    </>
+    </ClientLayout>
   )
 }
