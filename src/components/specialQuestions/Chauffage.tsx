efb1c40c6d0a0bd36a6c0de7842e0a3ace0746ac
Clément <clem.auger@hotmail.fr>
2025-10-09 11:39:05 +0200

    ♻️ 'bouteille gaz' appears more often than 'fioul'

--- src/components/specialQuestions/Chauffage.tsx
+++ src/components/specialQuestions/Chauffage.tsx
@@ -4,8 +4,8 @@ import type { DottedName } from '@incubateur-ademe/nosgestesclimat'
 export default function Chauffage({ question }: { question: DottedName }) {
   const secondaryQuestionsOfMosaic: DottedName[] = [
     'logement . chauffage . réseau de chaleur . présent',
-    'logement . chauffage . fioul . présent',
     'logement . chauffage . bouteille gaz . présent',
+    'logement . chauffage . fioul . présent',
     'logement . chauffage . citerne propane . présent',
   ]
 
