9a435e04384e50acd0edc4c9d2c3540c2cdfc311
Clément <clem.auger@hotmail.fr>
2025-10-13 11:39:03 +0200

    ♻️ hide mosaic description

--- src/components/form/question/Label.tsx
+++ src/components/form/question/Label.tsx
@@ -54,7 +54,6 @@ export default function Label({
   const mustShowDescriptionQuestion: DottedName[] = [
     'transport . voiture . utilisateur',
     'logement . âge',
-    'logement . chauffage',
   ]
 
   if (!label) return
