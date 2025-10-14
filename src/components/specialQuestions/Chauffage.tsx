8c25f60b0b4128575522e686b815fa6f699cfb7f
Clément <clem.auger@hotmail.fr>
2025-10-14 15:07:17 +0200

    ♻️ better order in mosaic "chauffage"

--- src/components/specialQuestions/Chauffage.tsx
+++ src/components/specialQuestions/Chauffage.tsx
@@ -2,14 +2,15 @@ import Question from '@/components/form/Question'
 import type { DottedName } from '@incubateur-ademe/nosgestesclimat'
 
 export default function Chauffage({ question }: { question: DottedName }) {
+  // Order is defined here.
   const secondaryQuestionsOfMosaic: DottedName[] = [
+    'logement . chauffage . chauffe eau solaire . présent',
+    'logement . climatisation . présent',
+    'logement . électricité . photovoltaique . présent',
     'logement . chauffage . réseau de chaleur . présent',
     'logement . chauffage . bouteille gaz . présent',
     'logement . chauffage . fioul . présent',
     'logement . chauffage . citerne propane . présent',
-    'logement . chauffage . chauffe eau solaire . présent',
-    'logement . climatisation . présent',
-    'logement . électricité . photovoltaique . présent',
   ]
 
   return (
