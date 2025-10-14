6ec39b1af1e2f3e8dd35925330e4225dca0c0d75
Benjamin Arias <bjlaa@proton.me>
2025-10-14 12:17:10 +0200

    ♻️ Add log for AB test (#1372)
    Co-authored-by: Benjamin Arias <12382534+bjlaa@users.noreply.github.com>

--- src/hooks/abTesting/useIsTestVersion.ts
+++ src/hooks/abTesting/useIsTestVersion.ts
@@ -4,6 +4,8 @@ import { useFeatureFlagVariantKey } from 'posthog-js/react'
 export const useIsTestVersion = (variantKey: string) => {
   const flagValue = useFeatureFlagVariantKey(variantKey)
 
+  console.log('AB test : ', flagValue, ' selected for', variantKey)
+
   // Disable the AB testing in the preview environments
   if (
     process.env.NEXT_PUBLIC_ENV !== 'production' &&
