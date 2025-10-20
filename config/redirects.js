1d654c4edd3868cea3956d6191805daf9a4d5d3b
Benjamin Arias <bjlaa@proton.me>
2025-10-20 14:55:55 +0200

    🔧 Fix themes pages EN redirections (#1396)
    Co-authored-by: Benjamin Arias <12382534+bjlaa@users.noreply.github.com>

--- config/redirects.js
+++ config/redirects.js
@@ -299,13 +299,23 @@ const redirects = [
     permanent: true,
   },
   {
-    source: '/empreinte-carbone-transport',
-    destination: '/themes/empreinte-carbone-transport',
+    source: '/empreinte-carbone-logement',
+    destination: '/themes/empreinte-carbone-logement',
     permanent: true,
   },
   {
-    source: '/empreinte-carbone-logement',
-    destination: '/themes/empreinte-carbone-logement',
+    source: '/en/themes/empreinte-carbone-alimentation',
+    destination: '/en',
+    permanent: true,
+  },
+  {
+    source: '/en/themes/empreinte-carbone-logement',
+    destination: '/en',
+    permanent: true,
+  },
+  {
+    source: '/en/themes/empreinte-carbone-transport',
+    destination: '/en',
     permanent: true,
   },
   {
