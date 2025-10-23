692942f4c589a807a8351681bc248938612dec68
Clément <clem.auger@hotmail.fr>
2025-10-23 10:56:22 +0200

    ♻️ use reduce instead of 3 loops

--- src/helpers/publicodes/getGroupedQuestionsFromSubcat.ts
+++ src/helpers/publicodes/getGroupedQuestionsFromSubcat.ts
@@ -5,39 +5,39 @@ export function getGroupedQuestionsFromSubcat(
   subcategory: DottedName,
   missingVariables: Record<DottedName, number>
 ): DottedName[] {
-  // We only keep questions that belong to the subcategory: exemple "logement . chauffage"
-  const questionsOfSubcat = questions?.filter(
-    (question) =>
-      question.startsWith(subcategory) && question.split(' . ').length > 2
-  )
-
   // We make sure that questions with common root (third level) stay together and we sort questions within these groups by their missingVariables score
-  const groupedQuestions: Record<string, DottedName[]> = {}
-  for (const question of questionsOfSubcat) {
-    const splitted = question.split(' . ')
-    const parent = splitted.slice(0, 3).join(' . ')
-    if (!groupedQuestions[parent]) {
-      groupedQuestions[parent] = []
-    }
-    groupedQuestions[parent].push(question)
-  }
+  const groupedQuestions: Record<string, DottedName[]> = questions.reduce(
+    (accGroupedQuestions, question) => {
+      const splittedQuestion = question.split(' . ')
 
-  // Now we sort each group and questions within each group by missingVariables score descending. If a question has already been answered, its missingVariables score is undefined, so we need to handle that case to make sure it appears first.
-  for (const parent in groupedQuestions) {
-    groupedQuestions[parent].sort((a, b) => {
-      const aIsAnswered = missingVariables[a] === undefined
-      const bIsAnswered = missingVariables[b] === undefined
+      // We only keep questions that belong to the subcategory: exemple "logement . chauffage"
+      if (!(question.startsWith(subcategory) && splittedQuestion.length > 2))
+        return accGroupedQuestions
 
-      if (aIsAnswered && !bIsAnswered) {
-        return -1
-      }
-      if (!aIsAnswered && bIsAnswered) {
-        return 1
-      }
+      const parent = splittedQuestion.slice(0, 3).join(' . ')
 
-      return (missingVariables[b] ?? 0) - (missingVariables[a] ?? 0)
-    })
-  }
+      const newGroupedQuestions = [...(accGroupedQuestions[parent] ?? [])]
+      newGroupedQuestions.push(question)
+
+      newGroupedQuestions.sort((a, b) => {
+        const aIsAnswered = missingVariables[a] === undefined
+        const bIsAnswered = missingVariables[b] === undefined
+
+        if (aIsAnswered && !bIsAnswered) {
+          return -1
+        }
+        if (!aIsAnswered && bIsAnswered) {
+          return 1
+        }
+
+        return (missingVariables[b] ?? 0) - (missingVariables[a] ?? 0)
+      })
+
+      accGroupedQuestions[parent] = newGroupedQuestions
+      return accGroupedQuestions
+    },
+    {} as Record<string, DottedName[]>
+  )
 
   // Finally we flatten the result preserving the grouping order
   const result: DottedName[] = []
