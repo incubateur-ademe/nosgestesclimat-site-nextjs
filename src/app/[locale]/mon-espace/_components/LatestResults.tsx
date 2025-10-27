8f74dfab474d1072ab1cf2e79c31df09da722820
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-10-27 16:31:03 +0100

    :sparkles: Add instructions banner

--- src/app/[locale]/mon-espace/_components/LatestResults.tsx
+++ src/app/[locale]/mon-espace/_components/LatestResults.tsx
@@ -1,31 +1,18 @@
-'use client'
-
 import Trans from '@/components/translation/trans/TransClient'
-import BlockSkeleton from '@/design-system/layout/BlockSkeleton'
 import Separator from '@/design-system/layout/Separator'
-import { fetchUserSimulations } from '@/helpers/user/fetchUserSimulations'
 import type { Locale } from '@/i18nConfig'
-import { useCurrentSimulation, useUser } from '@/publicodes-state'
-import { useQuery } from '@tanstack/react-query'
+import type { Simulation } from '@/publicodes-state/types'
 import ResultsBanner from './latestResults/ResultsBanner'
 
-export default function LatestResults({ locale }: { locale: Locale }) {
-  const {
-    user: { userId },
-  } = useUser()
-
-  const currentSimulation = useCurrentSimulation()
-
-  const { data: simulations, isLoading } = useQuery({
-    queryKey: ['simulations', userId],
-    queryFn: () => fetchUserSimulations({ userId }),
-    enabled: !!userId && !currentSimulation,
-  })
-
-  const latestSimulation = currentSimulation || simulations?.[0]
-
-  if (isLoading || !latestSimulation) {
-    return <BlockSkeleton />
+export default function LatestResults({
+  locale,
+  simulation,
+}: {
+  locale: Locale
+  simulation: Simulation
+}) {
+  if (!simulation) {
+    return null
   }
 
   return (
@@ -37,16 +24,14 @@ export default function LatestResults({ locale }: { locale: Locale }) {
 
         <Separator />
 
-        {latestSimulation && (
-          <p className="mb-0 font-bold">
-            <Trans>Test effectué le</Trans>{' '}
-            {new Date(latestSimulation?.date)?.toLocaleDateString(locale, {
-              day: 'numeric',
-              month: 'long',
-              year: 'numeric',
-            })}
-          </p>
-        )}
+        <p className="mb-0 font-bold">
+          <Trans>Test effectué le</Trans>{' '}
+          {new Date(simulation?.date)?.toLocaleDateString(locale, {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+          })}
+        </p>
       </div>
 
       <ResultsBanner />
--- src/app/[locale]/mon-espace/page.tsx
+++ src/app/[locale]/mon-espace/page.tsx
@@ -2,11 +2,11 @@ import ContentLarge from '@/components/layout/ContentLarge'
 import { SHOW_WELCOME_BANNER_QUERY_PARAM } from '@/constants/urls/params'
 import { CONNEXION_PATH, MON_ESPACE_PATH } from '@/constants/urls/paths'
 import { getIsUserAuthenticated } from '@/helpers/authentication/getIsUserAuthenticated'
-import { UserProvider } from '@/publicodes-state'
+import { fetchUserSimulations } from '@/helpers/user/fetchUserSimulations'
 import type { DefaultPageProps } from '@/types'
-import migrationInstructions from '@incubateur-ademe/nosgestesclimat/public/migration.json'
 import { redirect } from 'next/navigation'
 import QueryClientProviderWrapper from '../_components/mainLayoutProviders/QueryClientProviderWrapper'
+import InstructionsBanner from './_components/InstructionsBanner'
 import LatestResults from './_components/LatestResults'
 import ProfileTab from './_components/ProfileTabs'
 import WelcomeBanner from './_components/WelcomeBanner'
@@ -25,17 +25,25 @@ export default async function MonEspacePage({
     redirect(CONNEXION_PATH)
   }
 
+  const simulations = await fetchUserSimulations({
+    userId: authenticatedUser.id,
+  })
+
+  const latestSimulation = simulations?.[0]
+
   return (
     <ContentLarge className="mt-4 px-4 md:mt-10 lg:px-0">
-      {showWelcomeBanner && <WelcomeBanner locale={locale} />}
+      {showWelcomeBanner && !!latestSimulation && (
+        <WelcomeBanner locale={locale} />
+      )}
+
+      {!latestSimulation && <InstructionsBanner locale={locale} />}
 
       <ProfileTab locale={locale} activePath={MON_ESPACE_PATH} />
 
-      <UserProvider migrationInstructions={migrationInstructions}>
-        <QueryClientProviderWrapper>
-          <LatestResults locale={locale} />
-        </QueryClientProviderWrapper>
-      </UserProvider>
+      <QueryClientProviderWrapper>
+        <LatestResults locale={locale} simulation={latestSimulation} />
+      </QueryClientProviderWrapper>
     </ContentLarge>
   )
 }
