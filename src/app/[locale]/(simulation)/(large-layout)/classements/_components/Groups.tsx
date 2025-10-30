f50738c4daa0dfe98d13a1feda5584612bdad07b
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-10-30 15:39:51 +0100

    ✨ Add group section

--- src/app/[locale]/(simulation)/(large-layout)/classements/_components/Groups.tsx
+++ src/app/[locale]/(simulation)/(large-layout)/classements/_components/Groups.tsx
@@ -1,7 +1,16 @@
+import PlusIcon from '@/components/icons/PlusIcon'
 import Trans from '@/components/translation/trans/TransServer'
+import CTACard from '@/design-system/actions/CTACard'
+import {
+  baseClassNames,
+  colorClassNames,
+  sizeClassNames,
+} from '@/design-system/buttons/Button'
 import Title from '@/design-system/layout/Title'
 import type { Locale } from '@/i18nConfig'
 import type { Group } from '@/types/groups'
+import Link from 'next/link'
+import { twMerge } from 'tailwind-merge'
 import GroupContent from './groups/GroupContent'
 
 type Props = {
@@ -20,7 +29,40 @@ export default function Groups({ groups, locale }: Props) {
           </Trans>
         }
       />
-      <GroupContent groups={groups} />
+      <div className="flex flex-col gap-6 md:flex-row md:gap-8">
+        <div className="flex-1">
+          <GroupContent groups={groups} />
+        </div>
+
+        <CTACard
+          href="/mon-espace/groupes/creer"
+          className="hidden md:block"
+          label={
+            <Trans locale={locale} i18nKey="mon-espace.groups.create">
+              Créer un groupe
+            </Trans>
+          }
+          color="secondary"
+          imageAlt=""
+          imageSrc="https://nosgestesclimat-prod.s3.fr-par.scw.cloud/cms/medium_people_raising_arm_fe915601cd.png"
+          icon={<PlusIcon className="stroke-primary-700 min-w-8" />}
+          ctaClassName="min-w-64"
+        />
+
+        <Link
+          href="/mon-espace/groupes/creer"
+          className={twMerge(
+            'block md:hidden',
+            baseClassNames,
+            sizeClassNames.md,
+            colorClassNames.secondary
+          )}>
+          <PlusIcon className="stroke-primary-700 min-w-8" />
+          <Trans locale={locale} i18nKey="mon-espace.groups.create">
+            Créer un groupe
+          </Trans>
+        </Link>
+      </div>
     </>
   )
 }
--- src/app/[locale]/(simulation)/(large-layout)/organisations/[orgaSlug]/_components/myPolls/AddPollCard.tsx
+++ src/app/[locale]/(simulation)/(large-layout)/organisations/[orgaSlug]/_components/myPolls/AddPollCard.tsx
@@ -1,17 +1,10 @@
 'use client'
 
-import Link from '@/components/Link'
 import PlusIcon from '@/components/icons/PlusIcon'
 import Trans from '@/components/translation/trans/TransClient'
-import {
-  baseClassNames,
-  colorClassNames,
-  sizeClassNames,
-} from '@/design-system/buttons/Button'
+import CTACard from '@/design-system/actions/CTACard'
 import { useClientTranslation } from '@/hooks/useClientTranslation'
-import Image from 'next/image'
 import { useParams } from 'next/navigation'
-import { twMerge } from 'tailwind-merge'
 
 type Props = {
   hasNoPollsYet?: boolean
@@ -24,41 +17,23 @@ export default function AddPollCard({ hasNoPollsYet }: Props) {
 
   return (
     <li>
-      <Link
+      <CTACard
         href={`/organisations/${orgaSlug}/creer-campagne`}
-        className={twMerge(
-          'bg-primary-50 flex min-h-64 flex-col rounded-xl p-6 no-underline',
-          hasNoPollsYet ? 'rainbow-border' : ''
-        )}>
-        <div className="mb-6 flex flex-1 items-center justify-center">
-          <Image
-            className="self-start"
-            src="https://nosgestesclimat-prod.s3.fr-par.scw.cloud/cms/medium_people_raising_arm_fe915601cd.png"
-            width="200"
-            height="400"
-            alt={t(
-              'Deux personnes se faisant une accolade en levant le bras en signe de victoire.'
-            )}
-          />
-        </div>
-
-        <div
-          className={twMerge(
-            'w-full! text-sm',
-            baseClassNames,
-            sizeClassNames.md,
-            hasNoPollsYet ? colorClassNames.primary : colorClassNames.secondary
-          )}
-          color={hasNoPollsYet ? 'primary' : 'secondary'}>
+        highlight={!!hasNoPollsYet}
+        color={hasNoPollsYet ? 'primary' : 'secondary'}
+        imageSrc="https://nosgestesclimat-prod.s3.fr-par.scw.cloud/cms/medium_people_raising_arm_fe915601cd.png"
+        imageAlt=""
+        label={<Trans>Créer une campagne</Trans>}
+        icon={
           <PlusIcon
-            className={twMerge(
-              'min-w-8',
-              hasNoPollsYet ? 'stroke-white' : 'stroke-primary-700'
-            )}
+            className={
+              hasNoPollsYet
+                ? 'min-w-8 stroke-white'
+                : 'stroke-primary-700 min-w-8'
+            }
           />
-          <Trans>Créer une campagne</Trans>
-        </div>
-      </Link>
+        }
+      />
     </li>
   )
 }
