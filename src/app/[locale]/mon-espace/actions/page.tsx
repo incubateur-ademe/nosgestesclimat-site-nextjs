5d8991b4c83df16bee9821efbc72ed3fdb5c7614
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-10-30 12:12:08 +0100

    ✨ Move actions page logic to new account page

--- src/app/[locale]/mon-espace/actions/page.tsx
+++ src/app/[locale]/mon-espace/actions/page.tsx
@@ -1,17 +1,36 @@
 import ContentLarge from '@/components/layout/ContentLarge'
+import TopBar from '@/components/simulation/TopBar'
 import Trans from '@/components/translation/trans/TransServer'
+import { STORAGE_KEY } from '@/constants/storage'
 import { CONNEXION_PATH, MON_ESPACE_ACTIONS_PATH } from '@/constants/urls/paths'
 import { getIsUserAuthenticated } from '@/helpers/authentication/getIsUserAuthenticated'
+import { getRules } from '@/helpers/modelFetching/getRules'
+import { fetchUserSimulations } from '@/helpers/user/fetchUserSimulations'
+import { EngineProvider, FormProvider, UserProvider } from '@/publicodes-state'
 import type { DefaultPageProps } from '@/types'
+import migrationInstructions from '@incubateur-ademe/nosgestesclimat/public/migration.json'
 import { redirect } from 'next/navigation'
+import ActionsContent from '../../(simulation)/(large-layout)/actions/_components/ActionsContent'
+import ActionsTutorial from '../../(simulation)/(large-layout)/actions/_components/ActionsTutorial'
+import QueryClientProviderWrapper from '../../_components/mainLayoutProviders/QueryClientProviderWrapper'
 import ProfileTab from '../_components/ProfileTabs'
+import ActionAutoSave from './_components/ActionAutoSave'
+import JagisActionBanner from './_components/JagisActionBanner'
 
 export default async function MonEspaceActionsPage({
   params,
 }: DefaultPageProps) {
   const { locale } = await params
 
+  const rules = await getRules()
+
   const authenticatedUser = await getIsUserAuthenticated()
+
+  const simulations = await fetchUserSimulations({
+    userId: authenticatedUser.id,
+  })
+
+  const latestSimulation = simulations?.[0]
 
   if (!authenticatedUser) {
     redirect(CONNEXION_PATH)
@@ -27,6 +46,26 @@ export default async function MonEspaceActionsPage({
         </h1>
 
         <ProfileTab locale={locale} activePath={MON_ESPACE_ACTIONS_PATH} />
+
+        <UserProvider
+          storageKey={STORAGE_KEY}
+          migrationInstructions={migrationInstructions}>
+          <QueryClientProviderWrapper>
+            <EngineProvider rules={rules}>
+              <FormProvider>
+                <ActionAutoSave />
+
+                <TopBar className="mb-6" simulationMode={false} showTotal />
+
+                <ActionsTutorial />
+
+                <JagisActionBanner />
+
+                <ActionsContent />
+              </FormProvider>
+            </EngineProvider>
+          </QueryClientProviderWrapper>
+        </UserProvider>
       </div>
     </ContentLarge>
   )
