dfd5a63e5575144c64aac57a4fb9a8d2ff153969
Johan Girod <dev@johangirod.com>
2025-11-10 18:41:08 +0100

    🐛 send login email with proper locale

--- src/hooks/authentication/useLogin.ts
+++ src/hooks/authentication/useLogin.ts
@@ -2,11 +2,14 @@ import { AUTHENTICATION_URL } from '@/constants/urls/main'
 import { useUser } from '@/publicodes-state'
 import { useMutation } from '@tanstack/react-query'
 import axios from 'axios'
+import { useLocale } from '../useLocale'
 
 export default function useLogin() {
   const {
     user: { userId },
   } = useUser()
+  const locale = useLocale()
+
   return useMutation({
     mutationFn: ({ email, code }: { email: string; code: string }) =>
       axios
@@ -18,6 +21,9 @@ export default function useLogin() {
             userId,
           },
           {
+            params: {
+              locale,
+            },
             withCredentials: true,
           }
         )
