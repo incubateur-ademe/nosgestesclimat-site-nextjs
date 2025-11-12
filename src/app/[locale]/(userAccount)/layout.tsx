04888a3f93ff6233dcff0e3a4b8deb5dc00c8daf
Johan Girod <dev@johangirod.com>
2025-11-12 15:15:28 +0100

    ♻️ Move redirect to mon-espace in layout

--- src/app/[locale]/(userAccount)/layout.tsx
+++ src/app/[locale]/(userAccount)/layout.tsx
@@ -1,7 +1,16 @@
 import HeaderServer from '@/components/layout/HeaderServer'
+import { MON_ESPACE_PATH } from '@/constants/urls/paths'
+import { getAuthentifiedUser } from '@/helpers/authentication/getAuthentifiedUser'
+import { redirect } from 'next/navigation'
 import type { PropsWithChildren } from 'react'
 
-export default function Layout({ children }: PropsWithChildren) {
+export default async function Layout({ children }: PropsWithChildren) {
+  const authenticatedUser = await getAuthentifiedUser()
+
+  if (authenticatedUser) {
+    redirect(MON_ESPACE_PATH)
+  }
+
   return (
     <>
       <HeaderServer />
