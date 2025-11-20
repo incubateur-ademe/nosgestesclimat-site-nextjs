79c06b45e6381529cc67ee60e610b773726a8fb6
Benjamin Arias <bjlaa@proton.me>
2025-11-20 14:23:40 +0100

    🔧 Update E2E CI config (#1464)
    Co-authored-by: Benjamin Arias <12382534+bjlaa@users.noreply.github.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,12 +4,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
@@ -19,7 +13,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
