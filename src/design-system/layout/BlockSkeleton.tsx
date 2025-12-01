3e1961f33d6ab33decae7ebc167536f7b4069f02
Benjamin Arias <bjlaa@proton.me>
2025-12-01 15:21:01 +0100

    🐛 Fix 404 page, improve loading fallback (#1487)
    * 🐛 Re-add 404 page, improve loading fallback
    
    * ♻️ Cancel re-adding 404 page, add html and body tag to not-found files
    
    * ♻️ Add notFound catch all page
    
    * 🔥 Remove useless component
    
    ---------
    
    Co-authored-by: Benjamin Arias <12382534+bjlaa@users.noreply.github.com>

--- src/design-system/layout/BlockSkeleton.tsx
+++ src/design-system/layout/BlockSkeleton.tsx
@@ -1,6 +1,10 @@
-export default function BlockSkeleton() {
+import { twMerge } from 'tailwind-merge'
+
+export default function BlockSkeleton({ className }: { className?: string }) {
   return (
-    <div className="mt-4 mb-8 animate-pulse" data-testid="block-skeleton">
+    <div
+      className={twMerge('mt-4 mb-8 animate-pulse', className)}
+      data-testid="block-skeleton">
       <div className="bg-primary-100 h-[100px] w-full rounded-md"></div>
     </div>
   )
