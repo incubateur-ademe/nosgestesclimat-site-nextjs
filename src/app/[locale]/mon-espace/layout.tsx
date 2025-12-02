05c6861df0d8f75899777a0f9e2ac6b7c57a1104
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-12-02 10:59:31 +0100

    🎨 Add missing ServerLayout

--- src/app/[locale]/mon-espace/layout.tsx
+++ src/app/[locale]/mon-espace/layout.tsx
@@ -1,20 +1,27 @@
 import Footer from '@/components/layout/Footer'
 import HeaderServer from '@/components/layout/HeaderServer'
+import { ServerLayout } from '@/components/layout/ServerLayout'
 import { isUserAuthenticated } from '@/helpers/server/model/user'
+import type { Locale } from '@/i18nConfig'
 import { redirect } from 'next/navigation'
-import type { PropsWithChildren } from 'react'
 
-export default async function Layout({ children }: PropsWithChildren) {
+/* global LayoutProps */
+export default async function Layout({
+  children,
+  params,
+}: LayoutProps<'/[locale]/mon-espace'>) {
+  const { locale } = await params
+
   if (!(await isUserAuthenticated())) {
     redirect('/connexion')
   }
   return (
-    <>
+    <ServerLayout locale={locale as Locale}>
       <HeaderServer isSticky />
 
       {children}
 
       <Footer className="mt-16" />
-    </>
+    </ServerLayout>
   )
 }
