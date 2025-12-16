c8c8d6823b22778cca140edf5c38a83b0fb0f95f
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2025-12-16 05:39:41 +0100

    🎨 Use `use` instead of useEffect

--- src/design-system/cms/Banner.tsx
+++ src/design-system/cms/Banner.tsx
@@ -1,22 +1,12 @@
 'use client'
 
-import type { BannerType } from '@/adapters/cmsClient'
 import type { Locale } from '@/i18nConfig'
 import { fetchBanner } from '@/services/cms/fetchBanner'
-import { useEffect, useState } from 'react'
+import { use } from 'react'
 import { BannerContent } from './BannerContent'
 
 export default function Banner({ locale }: { locale: Locale }) {
-  const [banner, setBanner] = useState<BannerType | null>(null)
-
-  useEffect(() => {
-    const fetchBannerData = async () => {
-      const banner = await fetchBanner(locale)
-      setBanner(banner)
-    }
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    fetchBannerData()
-  }, [locale])
+  const banner = use(fetchBanner(locale))
 
   if (!banner) return null
 
