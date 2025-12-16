402ed2b6fa09e434afecb08a3c4807903f6e1530
Clément <55186402+Clemog@users.noreply.github.com>
2025-12-16 11:57:14 +0100

    🐛 iframe integrator url error (#1514)
    * fix: iframe integrator url error
    
    * ♻️ sentry is not defined within iframe scripts
    
    * 🐛 try to fix URL parsing

--- src/app/[locale]/_components/mainLayoutProviders/IframeOptionsContext.tsx
+++ src/app/[locale]/_components/mainLayoutProviders/IframeOptionsContext.tsx
@@ -5,16 +5,37 @@ import { getIsFrenchRegion } from '@/helpers/regions/getIsFrenchRegion'
 import { useTrackIframe } from '@/hooks/tracking/useTrackIframe'
 import { useUser } from '@/publicodes-state'
 import { getIsIframe } from '@/utils/getIsIframe'
+import * as Sentry from '@sentry/nextjs'
 import { useSearchParams } from 'next/navigation'
 import { createContext, useEffect, useState } from 'react'
 
 const getIsAllowedToBypassConsentDataShare = () => {
   if (typeof window === 'undefined') return false
   // https://stackoverflow.com/questions/6531534/document-location-parent-location-can-they-be-blocked
+
+  const windowLocation = window.location
+  const windowParentLocation = window.parent.location
+
+  if (!windowLocation) {
+    console.error('Iframe Nos Gestes Climat: window.location is undefined')
+    Sentry.captureMessage(
+      `Iframe Nos Gestes Climat: window.location is undefined`
+    )
+  }
+
+  if (!windowParentLocation) {
+    console.error(
+      'Iframe Nos Gestes Climat: window.parent.location is undefined'
+    )
+    Sentry.captureMessage(
+      `Iframe Nos Gestes Climat: window.parent.location is undefined`
+    )
+  }
+
   const integratorUrl = new URL(
     window.location != window.parent.location
-      ? document.referrer
-      : document.location.href
+      ? (document.referrer ?? 'about:blank')
+      : (document.location.href ?? 'about:blank')
   ).origin
 
   return verifyIfIntegratorBypassRights(integratorUrl)
