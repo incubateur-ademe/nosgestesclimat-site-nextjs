00b69c351b1d90f978f1e2a72f3fea6c93237695
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-01-05 16:20:16 +0100

    Update Banner.tsx

--- src/design-system/cms/Banner.tsx
+++ src/design-system/cms/Banner.tsx
@@ -5,9 +5,7 @@ import { BannerContent } from './BannerContent'
 export default async function Banner({ locale }: { locale: Locale }) {
   const banner = await fetchBanner(locale)
 
-  if (!banner) {
-    return null
-  }
+  if (!banner) return null
 
   return <BannerContent banner={banner} />
 }
