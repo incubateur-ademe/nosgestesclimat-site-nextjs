f7020b08fb6ebfed56538705569ebe19bc238087
Clément <clem.auger@hotmail.fr>
2026-01-06 11:10:45 +0100

    ♻️ use `sharelink_NGC` only as medium

--- src/constants/urls/utm.ts
+++ src/constants/urls/utm.ts
@@ -1,2 +1,3 @@
 export const MATOMO_CAMPAIGN_KEY = 'mtm_campaign'
+export const UTM_SOURCE_KEY = 'utm_source'
 export const UTM_MEDIUM_KEY = 'utm_medium'
--- src/hooks/useEndPageSharedUrl.ts
+++ src/hooks/useEndPageSharedUrl.ts
@@ -1,4 +1,5 @@
 import { eauMetric } from '@/constants/model/metric'
+import { UTM_MEDIUM_KEY, UTM_SOURCE_KEY } from '@/constants/urls/utm'
 import { useRule } from '@/publicodes-state'
 
 export const useEndPageSharedUrl = () => {
@@ -6,7 +7,7 @@ export const useEndPageSharedUrl = () => {
 
   const { numericValue: waterNumericValue } = useRule('bilan', eauMetric)
 
-  const params = `?total=${Math.round(numericValue)}&watertotal=${Math.round(waterNumericValue)}`
+  const params = `?total=${Math.round(numericValue)}&watertotal=${Math.round(waterNumericValue)}&${UTM_MEDIUM_KEY}=sharelink&${UTM_SOURCE_KEY}=NGC`
 
   const sharedUrl = `${window.location.origin}/partage${params}`
 
