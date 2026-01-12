ee9afc9ffaa6bb088896b7cacd460f4c5b95dee7
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-01-12 13:34:26 +0100

    ✨ Add new newsletter block in footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -31,6 +31,7 @@ import Link from '../Link'
 import LogoLink from '../misc/LogoLink'
 import LanguageSwitchButton from '../translation/LanguageSwitchButton'
 import Trans from '../translation/trans/TransClient'
+import WantToActBlock from './footer/WantToActBlock'
 import ThematicPagesSection from './ThematicPagesSection'
 
 const WHITE_BACKGROUND_PATHS = new Set([
@@ -75,161 +76,165 @@ export default function Footer({
         shouldUseWhiteBackground ? 'bg-white' : ''
       )}>
       <div className="md:mx-auto md:max-w-5xl">
-        <LogoLink
-          className="mb-8"
-          onClick={() => trackEvent(footerClickLogo)}
-        />
+        <div className="mb-8 flex items-center justify-between">
+          <LogoLink onClick={() => trackEvent(footerClickLogo)} />
 
-        <div className="mb-10 flex flex-col flex-wrap justify-start gap-x-16 gap-y-8 pt-4 md:flex-row lg:flex-nowrap">
-          <div className="flex flex-col gap-y-2">
-            <p
-              id="about-section"
-              className="text-default mb-0 text-sm font-bold">
-              <Trans>À propos</Trans>
-            </p>
-            <ul aria-labelledby="about-section">
-              <li>
-                <InlineLink
-                  href="https://beta.gouv.fr/startups/nosgestesclimat.html"
-                  target="_blank"
-                  aria-label={t('Qui sommes-nous - Nouvelle fenêtre')}
-                  rel="noopener noreferrer"
-                  onClick={() => trackEvent(footerClickQuiSommesNous)}
-                  className="text-default text-sm no-underline hover:underline">
-                  <Trans>Qui sommes-nous</Trans>
-                </InlineLink>
-              </li>
-              <li>
-                <InlineLink
-                  href="/plan-du-site"
-                  onClick={() => trackEvent(footerClickPlanSite)}
-                  className="text-default text-sm no-underline hover:underline">
-                  <Trans>Plan du site</Trans>
-                </InlineLink>
-              </li>
-              <li>
-                <InlineLink
-                  href="/contact"
-                  onClick={() => trackEvent(footerClickContact)}
-                  className="text-default text-sm no-underline hover:underline">
-                  <Trans>Contact</Trans>
-                </InlineLink>
-              </li>
-              <li>
-                <InlineLink
-                  href="/international"
-                  onClick={() => trackEvent(footerClickInternational)}
-                  className="text-default text-sm no-underline hover:underline">
-                  <Trans>International</Trans>
-                </InlineLink>
-              </li>
-              <li>
-                <InlineLink
-                  href="/stats"
-                  onClick={() => trackEvent(footerClickStats)}
-                  className="text-default text-sm no-underline hover:underline">
-                  <Trans>Statistiques</Trans>
-                </InlineLink>
-              </li>
-            </ul>
-          </div>
-
-          <div className="flex flex-col gap-y-2">
-            <p
-              id="diffusion-section"
-              className="text-default mb-0 text-sm font-bold">
-              <Trans>Diffusion</Trans>
-            </p>
-            <ul aria-labelledby="diffusion-section">
-              <li>
-                <InlineLink
-                  href="/diffuser"
-                  onClick={() => trackEvent(footerClickDiffusion)}
-                  className="text-default text-sm no-underline hover:underline">
-                  <Trans>Diffuser Nos Gestes Climat</Trans>
-                </InlineLink>
-              </li>
-              <li>
-                <InlineLink
-                  href="/organisations"
-                  onClick={() => trackEvent(footerClickOrganisations)}
-                  className="text-default text-sm no-underline hover:underline">
-                  <Trans>Organisations</Trans>
-                </InlineLink>
-              </li>
-              <li>
-                <InlineLink
-                  href="/nos-relais"
-                  onClick={() => trackEvent(footerClickAmbassadeurs)}
-                  className="text-default text-sm no-underline hover:underline">
-                  <Trans>Relais et partenaires</Trans>
-                </InlineLink>
-              </li>
-            </ul>
-          </div>
-
-          <div className="flex flex-col gap-y-2">
-            <p
-              id="resources-section"
-              className="text-default mb-0 text-sm font-bold">
-              <Trans>Ressources</Trans>
-            </p>
-            <ul aria-labelledby="resources-section">
-              <li>
-                <InlineLink
-                  href="/blog"
-                  onClick={() => trackEvent(footerClickBlog)}
-                  className="text-default text-sm no-underline hover:underline">
-                  <Trans>Blog</Trans>
-                </InlineLink>
-              </li>
-              <li>
-                <InlineLink
-                  href="/documentation"
-                  onClick={() => trackEvent(footerClickDocumentation)}
-                  className="text-default text-sm no-underline hover:underline">
-                  <Trans>Documentation</Trans>
-                </InlineLink>
-              </li>
-              <li>
-                <InlineLink
-                  href="/questions-frequentes"
-                  onClick={() => trackEvent(footerClickFAQ)}
-                  className="text-default text-sm no-underline hover:underline">
-                  <Trans>FAQ</Trans>
-                </InlineLink>
-              </li>
-              <li>
-                <InlineLink
-                  href="/nouveautes"
-                  onClick={() => trackEvent(footerClickNouveautes)}
-                  className="text-default text-sm no-underline hover:underline">
-                  <Trans>Nouveautés</Trans>
-                </InlineLink>
-              </li>
-              <li>
-                <InlineLink
-                  href="https://impactco2.fr"
-                  target="_blank"
-                  className="text-default text-sm no-underline hover:underline"
-                  onClick={() => trackEvent(footerClickImpactco2)}>
-                  Impact CO2
-                </InlineLink>
-              </li>
-            </ul>
-          </div>
-
-          <ThematicPagesSection />
+          <LanguageSwitchButton langButtonsDisplayed={langButtonsDisplayed} />
         </div>
 
-        <div className="flex flex-wrap justify-between gap-8 md:flex-row md:flex-nowrap">
-          <div>
-            <div className="mt-6 flex flex-wrap items-start justify-between gap-10">
-              <LanguageSwitchButton
-                langButtonsDisplayed={langButtonsDisplayed}
-              />
+        <div className="my-4 block md:hidden">
+          <WantToActBlock />
+        </div>
+
+        <div className="flex flex-col md:flex-row">
+          <div className="mb-10 flex flex-col flex-wrap justify-start gap-x-16 gap-y-8 pt-4 md:flex-row">
+            <div className="flex flex-col gap-y-2">
+              <p
+                id="about-section"
+                className="text-default mb-0 text-sm font-bold">
+                <Trans>À propos</Trans>
+              </p>
+              <ul aria-labelledby="about-section">
+                <li>
+                  <InlineLink
+                    href="https://beta.gouv.fr/startups/nosgestesclimat.html"
+                    target="_blank"
+                    aria-label={t('Qui sommes-nous - Nouvelle fenêtre')}
+                    rel="noopener noreferrer"
+                    onClick={() => trackEvent(footerClickQuiSommesNous)}
+                    className="text-default text-sm no-underline hover:underline">
+                    <Trans>Qui sommes-nous</Trans>
+                  </InlineLink>
+                </li>
+                <li>
+                  <InlineLink
+                    href="/plan-du-site"
+                    onClick={() => trackEvent(footerClickPlanSite)}
+                    className="text-default text-sm no-underline hover:underline">
+                    <Trans>Plan du site</Trans>
+                  </InlineLink>
+                </li>
+                <li>
+                  <InlineLink
+                    href="/contact"
+                    onClick={() => trackEvent(footerClickContact)}
+                    className="text-default text-sm no-underline hover:underline">
+                    <Trans>Contact</Trans>
+                  </InlineLink>
+                </li>
+                <li>
+                  <InlineLink
+                    href="/international"
+                    onClick={() => trackEvent(footerClickInternational)}
+                    className="text-default text-sm no-underline hover:underline">
+                    <Trans>International</Trans>
+                  </InlineLink>
+                </li>
+                <li>
+                  <InlineLink
+                    href="/stats"
+                    onClick={() => trackEvent(footerClickStats)}
+                    className="text-default text-sm no-underline hover:underline">
+                    <Trans>Statistiques</Trans>
+                  </InlineLink>
+                </li>
+              </ul>
             </div>
 
+            <div className="flex flex-col gap-y-2">
+              <p
+                id="diffusion-section"
+                className="text-default mb-0 text-sm font-bold">
+                <Trans>Diffusion</Trans>
+              </p>
+              <ul aria-labelledby="diffusion-section">
+                <li>
+                  <InlineLink
+                    href="/diffuser"
+                    onClick={() => trackEvent(footerClickDiffusion)}
+                    className="text-default text-sm no-underline hover:underline">
+                    <Trans>Diffuser Nos Gestes Climat</Trans>
+                  </InlineLink>
+                </li>
+                <li>
+                  <InlineLink
+                    href="/organisations"
+                    onClick={() => trackEvent(footerClickOrganisations)}
+                    className="text-default text-sm no-underline hover:underline">
+                    <Trans>Organisations</Trans>
+                  </InlineLink>
+                </li>
+                <li>
+                  <InlineLink
+                    href="/nos-relais"
+                    onClick={() => trackEvent(footerClickAmbassadeurs)}
+                    className="text-default text-sm no-underline hover:underline">
+                    <Trans>Relais et partenaires</Trans>
+                  </InlineLink>
+                </li>
+              </ul>
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <p
+                id="resources-section"
+                className="text-default mb-0 text-sm font-bold">
+                <Trans>Ressources</Trans>
+              </p>
+              <ul aria-labelledby="resources-section">
+                <li>
+                  <InlineLink
+                    href="/blog"
+                    onClick={() => trackEvent(footerClickBlog)}
+                    className="text-default text-sm no-underline hover:underline">
+                    <Trans>Blog</Trans>
+                  </InlineLink>
+                </li>
+                <li>
+                  <InlineLink
+                    href="/documentation"
+                    onClick={() => trackEvent(footerClickDocumentation)}
+                    className="text-default text-sm no-underline hover:underline">
+                    <Trans>Documentation</Trans>
+                  </InlineLink>
+                </li>
+                <li>
+                  <InlineLink
+                    href="/questions-frequentes"
+                    onClick={() => trackEvent(footerClickFAQ)}
+                    className="text-default text-sm no-underline hover:underline">
+                    <Trans>FAQ</Trans>
+                  </InlineLink>
+                </li>
+                <li>
+                  <InlineLink
+                    href="/nouveautes"
+                    onClick={() => trackEvent(footerClickNouveautes)}
+                    className="text-default text-sm no-underline hover:underline">
+                    <Trans>Nouveautés</Trans>
+                  </InlineLink>
+                </li>
+                <li>
+                  <InlineLink
+                    href="https://impactco2.fr"
+                    target="_blank"
+                    className="text-default text-sm no-underline hover:underline"
+                    onClick={() => trackEvent(footerClickImpactco2)}>
+                    Impact CO2
+                  </InlineLink>
+                </li>
+              </ul>
+            </div>
+
+            <ThematicPagesSection />
+          </div>
+          <div className="hidden md:block">
+            <WantToActBlock />
+          </div>
+        </div>
+
+        <div className="flex flex-wrap justify-between gap-8 border-t border-gray-200 pt-6 md:flex-row md:flex-nowrap">
+          <div>
             <div className="mt-4 text-xs">
               <ul>
                 <li className="block md:inline">
