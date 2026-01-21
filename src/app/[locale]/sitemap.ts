d21d759d755318c4fbcf82eb54bea502b8c71c9b
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-01-21 14:47:07 +0100

    🎨 Improve sitemap

--- src/app/[locale]/sitemap.ts
+++ src/app/[locale]/sitemap.ts
@@ -11,33 +11,75 @@ import rules from '@incubateur-ademe/nosgestesclimat/public/co2-model.FR-lang.fr
 import type { MetadataRoute } from 'next'
 import { utils } from 'publicodes'
 
+// Documentation rules to exclude from sitemap (redirections or invalid pages)
+const excludedDocumentationPatterns = [
+  // Redirections - divers
+  'divers . nombre par tranche de x m2 . nombre de tranches',
+  'divers . nombre par tranche de x m2 . tranche',
+  'divers . textile . indice de durabilité moyen . nombre total',
+  // Redirections - logement vacances résidence secondaire
+  'logement . vacances . résidence secondaire . localisation . littoral nord-ouest . facteur saison . facteur surtout été',
+  'logement . vacances . résidence secondaire . localisation . montagne . facteur saison . facteur hiver',
+  'logement . vacances . résidence secondaire . localisation . montagne . facteur saison . facteur été',
+  'logement . vacances . résidence secondaire . localisation . littoral méditerranéen . facteur saison . facteur été',
+  'logement . vacances . résidence secondaire . localisation . montagne . facteur saison . facteur surtout été',
+  'logement . vacances . résidence secondaire . localisation . campagne . facteur saison . facteur surtout hiver',
+  'logement . vacances . résidence secondaire . localisation . ville . facteur saison . facteur surtout été',
+  'logement . vacances . résidence secondaire . localisation . littoral méditerranéen . facteur saison . facteur surtout hiver',
+  'logement . vacances . résidence secondaire . localisation . littoral méditerranéen . facteur saison . facteur hiver',
+  'logement . vacances . résidence secondaire . localisation . campagne . facteur saison . facteur été',
+  'logement . vacances . résidence secondaire . localisation . montagne . facteur saison . facteur surtout hiver',
+  'logement . vacances . résidence secondaire . localisation . ville . facteur saison . facteur hiver',
+  'logement . vacances . résidence secondaire . localisation . ville . facteur saison . facteur été',
+  'logement . vacances . résidence secondaire . localisation . littoral nord-ouest . facteur saison . facteur surtout hiver',
+  'logement . vacances . résidence secondaire . localisation . littoral nord-ouest . facteur saison . facteur été',
+  'logement . vacances . résidence secondaire . localisation . campagne . facteur saison . facteur hiver',
+  'logement . vacances . résidence secondaire . localisation . littoral méditerranéen . facteur saison . facteur surtout été',
+  'logement . vacances . résidence secondaire . localisation . ville . facteur saison . facteur surtout hiver',
+  'logement . vacances . résidence secondaire . localisation . littoral nord-ouest . facteur saison . facteur hiver',
+  'logement . vacances . résidence secondaire . localisation . campagne . facteur saison . facteur surtout été',
+  // Redirections - logement chauffage
+  'logement . chauffage . précision consommation . saisie DPE . F',
+  'logement . chauffage . précision consommation . saisie DPE . G',
+  'logement . chauffage . précision consommation . saisie DPE . B',
+  'logement . chauffage . précision consommation . saisie DPE . D',
+  'logement . chauffage . précision consommation . saisie DPE . A',
+  'logement . chauffage . précision consommation . saisie DPE . E',
+  'logement . chauffage . précision consommation . saisie DPE . C',
+  'logement . chauffage-collectif . individuel',
+  'logement . chauffage-collectif . collectif',
+  // Redirections - transport
+  'transport . voiture . boulot . sans voiture . type . TER',
+  'transport . voiture . boulot . sans voiture . type . trotinette électrique',
+  'transport . voiture . boulot . sans voiture . type . métro ou tramway',
+  'transport . voiture . boulot . sans voiture . type . bus',
+  'transport . voiture . boulot . sans voiture . type . vélo électrique',
+  'transport . voiture . boulot . sans voiture . type . vélo classique',
+  // Redirections - métrique
+  'métrique . carbone',
+  'métrique . eau',
+]
+
+const excludedDocumentationSet = new Set(excludedDocumentationPatterns)
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const staticPages = new Set([
     '',
-    'accessibilité',
-    'a-propos',
     'blog',
     'budget',
     'cgu',
     'contact',
     'diffuser',
-    'documentation/guide',
-    'documentation/modele',
     'documentation',
     'empreinte-carbone',
     'empreinte-eau',
-    'fin',
-    'guide',
     'international',
     'mentions-legales',
     'mentions-legales-base-empreinte',
     'modele',
     'nos-relais',
     'nouveautes',
-    'northstar',
     'organisations',
-    'partage',
-    'partenaire',
     'personas',
     'plan-du-site',
     'politique-de-confidentialite',
@@ -56,13 +98,15 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     priority: 0.8,
   }))
 
-  const documentationUrls = Object.keys(rules).map((dottedName) => ({
-    url: `https://nosgestesclimat.fr/documentation/${utils.encodeRuleName(
-      dottedName
-    )}`,
-    lastModified: new Date(),
-    priority: 0.8,
-  }))
+  const documentationUrls = Object.keys(rules)
+    .filter((dottedName) => !excludedDocumentationSet.has(dottedName))
+    .map((dottedName) => ({
+      url: `https://nosgestesclimat.fr/documentation/${utils.encodeRuleName(
+        dottedName
+      )}`,
+      lastModified: new Date(),
+      priority: 0.8,
+    }))
 
   const blogPosts = await fetchAllArticleTitlesAndSlugs({ locale: 'fr' })
 
