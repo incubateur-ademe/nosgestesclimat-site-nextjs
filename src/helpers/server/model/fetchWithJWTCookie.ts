a4a9f7cf16491bbf47bdcd17922caf2cd16ecad0
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-01-21 17:44:28 +0100

    :bug: Rename cookie to avoid buggy behaviour

--- src/helpers/server/model/fetchWithJWTCookie.ts
+++ src/helpers/server/model/fetchWithJWTCookie.ts
@@ -1,4 +1,5 @@
 'use server'
+import { AUTHENTICATION_COOKIE_NAME } from '@/constants/authentication/cookie'
 import { cookies } from 'next/headers'
 import {
   InternalServerError,
@@ -13,7 +14,7 @@ export async function fetchWithJWTCookie(
   { method = 'GET' }: { method?: 'GET' | 'POST'; setCookies?: boolean } = {}
 ) {
   const cookieStore = await cookies()
-  const ngcCookie = cookieStore.get('ngcjwt')
+  const ngcCookie = cookieStore.get(AUTHENTICATION_COOKIE_NAME)
 
   if (!ngcCookie) {
     return null
