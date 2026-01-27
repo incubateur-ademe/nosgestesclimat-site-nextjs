46a244f2262b01a1e39d002e1ecd8654675bfd9a
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-01-27 09:52:55 +0100

    move items to constant

--- src/components/user/NewsletterManagement.tsx
+++ src/components/user/NewsletterManagement.tsx
@@ -1,12 +1,6 @@
 'use client'
 
 import {
-  LIST_MAIN_NEWSLETTER,
-  LIST_NOS_GESTES_ALIMENTATION_NEWSLETTER,
-  LIST_NOS_GESTES_LOGEMENT_NEWSLETTER,
-  LIST_NOS_GESTES_TRANSPORT_NEWSLETTER,
-} from '@/constants/brevo'
-import {
   captureClickUpdateUserNewsletters,
   clickUpdateUserNewsletters,
 } from '@/constants/tracking/user-account'
@@ -14,7 +8,6 @@ import Alert from '@/design-system/alerts/alert/Alert'
 import Button from '@/design-system/buttons/Button'
 import EmailInput from '@/design-system/inputs/EmailInput'
 import Emoji from '@/design-system/utils/Emoji'
-import { t } from '@/helpers/metadata/fakeMetadataT'
 import type { UserServer } from '@/helpers/server/model/user'
 import { fetchUser } from '@/helpers/user/fetchUser'
 import { useGetNewsletterSubscriptions } from '@/hooks/settings/useGetNewsletterSubscriptions'
@@ -27,6 +20,7 @@ import { useQuery } from '@tanstack/react-query'
 import { useEffect } from 'react'
 import { useForm as useReactHookForm, useWatch } from 'react-hook-form'
 import Trans from '../translation/trans/TransClient'
+import { NEWSLETTERS } from './_constants/newsletters'
 import NewsletterCheckbox from './newsletterManagement/NewsletterCheckbox'
 
 const getSelectedNewsletterIds = (
@@ -36,77 +30,6 @@ const getSelectedNewsletterIds = (
     .filter(([, isSelected]) => isSelected)
     .map(([id]) => Number(id))
 }
-
-const NEWSLETTERS = [
-  {
-    id: LIST_MAIN_NEWSLETTER,
-    title: (
-      <>
-        {t(
-          'newsletterManagement.mainNewsletter.title',
-          'Les actualités de Nos Gestes Climat'
-        )}{' '}
-        <Emoji>🌱</Emoji>
-      </>
-    ),
-    description: t(
-      'newsletterManagement.mainNewsletter.description',
-      "Nos dernières évolutions et nos recommandations d'actions, les articles récents, les nouvelles formations, et plus encore. Une fois par mois"
-    ),
-    'data-testid': 'newsletter-saisonniere-checkbox',
-  },
-  {
-    id: LIST_NOS_GESTES_TRANSPORT_NEWSLETTER,
-    title: (
-      <>
-        {t(
-          'newsletterManagement.transportNewsletter.title',
-          'Nos Gestes Transports'
-        )}{' '}
-        <Emoji>🚗</Emoji>
-      </>
-    ),
-    description: t(
-      'newsletterManagement.transportNewsletter.description',
-      "4 infolettres (sur 4 semaines), pour comprendre l'impact de nos déplacements et agir concrètement. Pour avancer vers des trajets plus légers"
-    ),
-    'data-testid': 'newsletter-transports-checkbox',
-  },
-  {
-    id: LIST_NOS_GESTES_LOGEMENT_NEWSLETTER,
-    title: (
-      <>
-        {t(
-          'newsletterManagement.logementNewsletter.title',
-          'Nos Gestes Logement'
-        )}{' '}
-        <Emoji>🏠</Emoji>
-      </>
-    ),
-    description: t(
-      'newsletterManagement.logementNewsletter.description',
-      "3 infolettres (sur 3 semaines), pour découvrir comment nos choix d'habitat influencent notre empreinte carbone. Pour un logement plus sobre et confortable."
-    ),
-    'data-testid': 'newsletter-logement-checkbox',
-  },
-  {
-    id: LIST_NOS_GESTES_ALIMENTATION_NEWSLETTER,
-    title: (
-      <>
-        {t(
-          'newsletterManagement.alimentationNewsletter.title',
-          'Nos Gestes Alimentation'
-        )}{' '}
-        <Emoji>🍎</Emoji>
-      </>
-    ),
-    description: t(
-      'newsletterManagement.alimentationNewsletter.description',
-      "4 infolettres (sur 4 semaines), pour découvrir l'empreinte de notre alimentation, et comprendre comment aligner son assiette avec les enjeux planétaires."
-    ),
-    'data-testid': 'newsletter-alimentation-checkbox',
-  },
-]
 
 interface Props {
   hasEmailField?: boolean
