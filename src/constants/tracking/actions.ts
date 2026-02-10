217cadd2ce3ba7d31945a639860b7b8ba921891b
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-02-10 09:26:58 +0100

    Update actions.ts

--- src/constants/tracking/actions.ts
+++ src/constants/tracking/actions.ts
@@ -6,7 +6,6 @@ export const trackingActionClickCTA = 'CTA Click'
 
 export const trackingActionClickPageBottom = 'Click CTA bas de page'
 
-// Post thumbnail
 export const trackingActionClickPostThumbnail = 'Click article'
 
 export const trackActionClickSortingButton = () => {
