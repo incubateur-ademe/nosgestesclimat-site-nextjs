aba5f20f551c03e8534716caa8b6ec396ffdd75b
Benjamin Arias <bjlaa@proton.me>
2026-02-12 15:19:15 +0100

    🐛 Re-hide lang buttons dynamically (#1649)
    * 🐛 Re-hide lang buttons dynamically
    
    * fix lint error
    
    * Update FooterServer.tsx
    
    * improve getLangButtonsDisplayed
    
    * 🎨 Simplify `getShouldDisplayLangButtons`
    
    * 🎨 Improve func logic
    
    ---------
    
    Co-authored-by: Benjamin Arias <12382534+bjlaa@users.noreply.github.com>

--- src/app/[locale]/themes/[landingPageSlug]/page.tsx
+++ src/app/[locale]/themes/[landingPageSlug]/page.tsx
@@ -44,7 +44,7 @@ export async function generateMetadata({
   const { thematicLandingPageMetadata } =
     (await fetchThematicLandingPageMetadata({
       landingPageSlug,
-    })) || {}
+    })) ?? {}
 
   return getMetadataObject({
     locale,
@@ -302,7 +302,7 @@ export default async function ThematicLandingPage({
         {htmlLegend && <Legend htmlLegend={htmlLegend} />}
       </LandingPage>
 
-      <Footer locale={locale} />
+      <Footer params={{ landingPageSlug }} locale={locale} />
     </ClientLayout>
   )
 }
