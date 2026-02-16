364d8c8accca40bcd17caa6da7a6ac2800e1ff5c
Clément <clem.auger@hotmail.fr>
2026-02-16 16:31:34 +0100

    ⬆️ upgrade posthog pkg and check config

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -24,19 +24,18 @@ export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
 if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: 'identified_only',
-    // @TODO: update this date when able to install a newer version of posthog-js
-    defaults: '2025-05-24',
-    autocapture: false,
-    capture_pageview: false,
-    capture_pageleave: true,
+    person_profiles: 'identified_only', // we only identify users if they explicitly give their consent trough the cookie banner
+    defaults: '2025-11-30', // @TODO: last available date for the SDK we use. To be updated when being able to install a newer version of posthog-js (today restricted by minimumReleaseAge protection).
+    autocapture: false, // Disable automatic event capture, as we capture manually. So we can control the events we send (and reduce our billing)
+    capture_pageview: false, // Disable automatic pageview capture, as we capture manually to be more precise on iframe detections and referrer sources
+    capture_pageleave: true, // Enable pageleave capture
     custom_campaign_params: [
       'mtm_campaign',
       'mtm_kwd',
       'mtm_keyword',
       'organisation',
       'poll',
-    ],
+    ], // Enable to set query parameters as properties on the events
   })
 
   // Expose posthog globally for inline scripts
