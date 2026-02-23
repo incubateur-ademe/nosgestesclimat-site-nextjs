cfcb33fce1805e9de6fafb1eb9eb09968ff8bc97
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-02-23 11:29:30 +0100

    Update page.tsx

--- src/app/[locale]/(server)/(large)/simulation/[simulationId]/resultats/page.tsx
+++ src/app/[locale]/(server)/(large)/simulation/[simulationId]/resultats/page.tsx
@@ -1,6 +1,7 @@
 import SimulationResults from '@/components/results/SimulationResults'
 import { getUser } from '@/helpers/server/model/user'
 import type { Locale } from '@/i18nConfig'
+import { notFound } from 'next/navigation'
 
 export default async function SimulationPage({
   params,
@@ -8,6 +9,10 @@ export default async function SimulationPage({
   const { simulationId, locale } = await params
 
   const user = await getUser()
+
+  if (!user) {
+    notFound()
+  }
 
   return (
     <SimulationResults
