b5cc6018e361829f97379fc6bc769c0f3d5597a4
Benjamin Arias <bjlaa@proton.me>
2026-02-26 11:13:47 +0100

    🐛 Update GTM cookie acceptance state for returning users (#1682)
    * :bug: Update GTM cookie acceptance state for returning users
    
    * ♻️ make GTM comp server side
    
    ---------
    
    Co-authored-by: Benjamin Arias <12382534+bjlaa@users.noreply.github.com>

--- src/components/googleTagManager/GoogleTagIframe.tsx
+++ src/components/googleTagManager/GoogleTagIframe.tsx
@@ -1,15 +1,9 @@
-'use client'
-
-import { useClientTranslation } from '@/hooks/useClientTranslation'
-
 export function GoogleTagIframe() {
-  const { t } = useClientTranslation()
-
   return (
     <noscript>
       <iframe
         id="google-tag-iframe"
-        title={t('Iframe - Google Tag Manager')}
+        title="Iframe - Google Tag Manager"
         src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_ID}`}
         height="0"
         width="0"
