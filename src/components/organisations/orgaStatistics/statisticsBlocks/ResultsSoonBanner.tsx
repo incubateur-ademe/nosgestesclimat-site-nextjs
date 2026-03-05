54ddc26cb7ebca08c9d0ddccae70e6fca820512f
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-03-05 12:03:41 +0100

    🔥 Remove water bloc on poll results view

--- src/components/organisations/orgaStatistics/statisticsBlocks/ResultsSoonBanner.tsx
+++ src/components/organisations/orgaStatistics/statisticsBlocks/ResultsSoonBanner.tsx
@@ -4,7 +4,6 @@ import HourglassIcon from '@/components/icons/HourglassIcon'
 import Trans from '@/components/translation/trans/TransClient'
 import Card from '@/design-system/layout/Card'
 import { useIsOrganisationAdmin } from '@/hooks/organisations/useIsOrganisationAdmin'
-import Wave from 'react-wavify'
 
 interface Props {
   hasLessThan3Participants: boolean
@@ -56,37 +55,13 @@ export default function ResultsSoonBanner({ hasLessThan3Participants }: Props) {
         </Card>
       </div>
 
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-        <div className="bg-rainbow-rotation overflow-hidden rounded-xl bg-gray-100 p-8">
-          <p className="text-primary-700 text-4xl font-bold">
-            8,0 <span className="text-base font-normal">t CO₂e</span>
-          </p>
-          <p className="text-xl">
-            <Trans>Empreinte moyenne</Trans>
-          </p>
-        </div>
-
-        <div className="relative hidden overflow-hidden rounded-xl bg-gray-100 p-8 lg:block">
-          <Wave
-            fill="#5152D0"
-            className="pointer-events-none absolute right-0 bottom-0 left-0 h-full w-full rounded-b-xl"
-            options={{ height: 10, amplitude: 20, speed: 0.11, points: 3 }}
-          />
-          <div className="relative z-10">
-            <p className="text-3xl font-bold text-white">
-              10 000{' '}
-              <span className="text-base font-normal">
-                <Trans>litres</Trans>
-              </span>
-            </p>
-
-            <p className="text-xl text-white">
-              <Trans>
-                <strong>Empreinte eau</strong> moyenne
-              </Trans>
-            </p>
-          </div>
-        </div>
+      <div className="bg-rainbow-rotation overflow-hidden rounded-xl bg-gray-100 p-8">
+        <p className="text-primary-700 text-4xl font-bold">
+          8,0 <span className="text-base font-normal">t CO₂e</span>
+        </p>
+        <p className="text-xl">
+          <Trans>Empreinte moyenne</Trans>
+        </p>
       </div>
     </div>
   )
