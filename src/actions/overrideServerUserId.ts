e485ec935de52795b122a0b922abbf963cc0d0db
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-03-09 15:02:55 +0100

    ♻️ Move userId sync logic to hook inside user provider

---
+++ src/actions/overrideServerUserId.ts
@@ -0,0 +1,15 @@
+'use server'
+
+import {
+  USER_ID_COOKIE_NAME,
+  USER_ID_COOKIE_OPTIONS,
+} from '@/constants/authentication/cookie'
+import { cookies } from 'next/headers'
+
+export async function overrideServerUserId(userId: string) {
+  const serverUserId = (await cookies()).get(USER_ID_COOKIE_NAME)?.value
+
+  if (!serverUserId || serverUserId !== userId) {
+    ;(await cookies()).set(USER_ID_COOKIE_NAME, userId, USER_ID_COOKIE_OPTIONS)
+  }
+}
