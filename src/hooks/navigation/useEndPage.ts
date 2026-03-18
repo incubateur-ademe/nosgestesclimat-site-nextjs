8b440325e66963222a158b755a10f015f0c8d5cc
Johan Girod <dev@johangirod.com>
2026-03-18 09:09:12 +0100

    ♻️ Remove auto-save in end page navigation

--- src/hooks/navigation/useEndPage.ts
+++ src/hooks/navigation/useEndPage.ts
@@ -3,11 +3,9 @@ import { END_PAGE_PATH } from '@/constants/urls/paths'
 import { useCurrentSimulation } from '@/publicodes-state'
 import { useRouter } from 'next/navigation'
 import { useCallback } from 'react'
-import { useSaveSimulation } from '../simulation/useSaveSimulation'
 
 export function useGoToEndPage() {
   const router = useRouter()
-  const { saveSimulation } = useSaveSimulation()
   const currentSimulation = useCurrentSimulation()
 
   const { progression, groups, polls } = currentSimulation
@@ -16,13 +14,12 @@ export function useGoToEndPage() {
     if (progression !== 1) {
       return
     }
-    void saveSimulation({ simulation: currentSimulation })
     if ((polls && polls.length > 0) || groups) {
       router.push(EMAIL_PAGE)
       return
     }
     router.push(END_PAGE_PATH)
-  }, [progression, saveSimulation, currentSimulation, polls, groups, router])
+  }, [progression, polls, groups, router])
 
   return { goToEndPage }
 }
