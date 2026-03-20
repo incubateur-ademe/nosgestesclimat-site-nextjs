49490e07b83d16323aa7759b2cdab0bd6134697a
Johan Girod <dev@johangirod.com>
2026-03-20 10:27:24 +0100

    ✍ Change collective effort layout and content

--- src/components/results/objective/_components/CollectiveEffort.tsx
+++ src/components/results/objective/_components/CollectiveEffort.tsx
@@ -20,46 +20,78 @@ export default function CollectiveEfforts({ locale, className }: Props) {
         </Trans>
       </Badge>
 
-      <Card className="mb-12 flex-col gap-4 md:flex-row">
-        <article>
-          <h3 className="mb-5">
-            <Badge color="purple" className="border-none">
+      <Card className="mb-12">
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+          <article>
+            <h3 className="mb-5">
+              <Badge color="blue" className="border-none">
+                <Trans
+                  locale={locale}
+                  i18nKey="results.objective.collectiveEffort.state">
+                  L'État français
+                </Trans>
+              </Badge>
+            </h3>
+            <p>
               <Trans
                 locale={locale}
-                i18nKey="results.objective.collectiveEffort.state">
-                L'État français
+                i18nKey="results.objective.collectiveEffort.stateDescription">
+                Il peut accélérer la <strong>décarbonation</strong> de la
+                société en <strong>votant des lois</strong> et{' '}
+                <strong>des aides financières ciblées.</strong> Grâce à ses
+                investissements, il réduit progressivement l'empreinte des
+                services sociétaux.
               </Trans>
-            </Badge>
-          </h3>
-          <p>
-            <Trans
-              locale={locale}
-              i18nKey="results.objective.collectiveEffort.stateDescription">
-              Réduction des services sociétaux (aujourd’hui à 1,55T) et
-              décarbonation des infrastructures.
-            </Trans>
-          </p>
-        </article>
+            </p>
+          </article>
 
-        <article>
-          <h3 className="mb-5">
-            <Badge color="yellow" className="border-none">
+          <article>
+            <h3 className="mb-5">
+              <Badge color="blue" className="border-none">
+                <Trans
+                  locale={locale}
+                  i18nKey="results.objective.collectiveEffort.collectivities">
+                  Les collectivités
+                </Trans>
+              </Badge>
+            </h3>
+            <p>
               <Trans
                 locale={locale}
-                i18nKey="results.objective.collectiveEffort.society">
-                Toute la société
+                i18nKey="results.objective.collectiveEffort.collectivitiesDescription">
+                Par leurs choix d'aménagements du territoire, elles participent
+                à{' '}
+                <strong>l'adoption de nouvelles pratiques plus durables</strong>{' '}
+                (mobilités, logement, alimentation...) à l'échelle locale.
               </Trans>
-            </Badge>
-          </h3>
-          <p>
-            <Trans
-              locale={locale}
-              i18nKey="results.objective.collectiveEffort.societyDescription">
-              Diminution de l’empreinte des biens et des services par l’effort
-              combiné des entreprises et collectivités.{' '}
-            </Trans>
-          </p>
-        </article>
+            </p>
+          </article>
+
+          <article>
+            <h3 className="mb-5">
+              <Badge color="blue" className="border-none">
+                <Trans
+                  locale={locale}
+                  i18nKey="results.objective.collectiveEffort.companies">
+                  Les entreprises
+                </Trans>
+              </Badge>
+            </h3>
+            <p>
+              <Trans
+                locale={locale}
+                i18nKey="results.objective.collectiveEffort.companiesDescription">
+                Sous l'impulsion de l'État et des citoyens, les entreprises
+                travaillent à la{' '}
+                <strong>
+                  réduction progressive de l'empreinte des bien et services.
+                </strong>{' '}
+                Cela contribue à réduire, à la fin, l'empreinte de chacun de nos
+                achats et usages.
+              </Trans>
+            </p>
+          </article>
+        </div>
       </Card>
     </div>
   )
