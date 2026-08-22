f688f63a8b3ef4ad635199459c4bfc0c392fa692
Andréa Maugars <andrea.maugars@gmail.com>
2026-08-22 21:13:18 +0300

    ♻️  remove useless isModelSupported check

--- apps/server/src/features/simulations/handlers/program-computation.ts
+++ apps/server/src/features/simulations/handlers/program-computation.ts
@@ -1,13 +1,11 @@
 import { ComputationAlreadyExistsException } from '@nosgestesclimat/core/features/simulation-computation/exceptions/simulation-computation.exception'
 import { createProgramSimulationComputation } from '@nosgestesclimat/core/features/simulation-computation/services/program-simulation-computation'
-import { captureException } from '@sentry/node'
 import type { Handler } from '../../../core/event-bus/handler.ts'
 import logger from '../../../logger.ts'
 import type { SimulationUpsertedEvent } from '../events/SimulationUpserted.event.ts'
 
 const programSimulationComputation = createProgramSimulationComputation({
   logger,
-  captureException,
 })
 
 export const programComputation: Handler<SimulationUpsertedEvent> = async ({
--- packages/core/src/features/simulation-computation/services/program-simulation-computation.ts
+++ packages/core/src/features/simulation-computation/services/program-simulation-computation.ts
@@ -1,24 +1,18 @@
-import type { CaptureException, Logger } from '../../logger/index.ts'
+import type { Logger } from '../../logger/index.ts'
 import { getSimulationById } from '../../simulations/repository/simulation.repository.ts'
-import {
-  SimulationNotFinishedException,
-  UnsupportedModelException,
-} from '../exceptions/simulation-computation.exception.ts'
-import { isModelSupported } from '../model-support/is-model-supported.ts'
+import { SimulationNotFinishedException } from '../exceptions/simulation-computation.exception.ts'
 import { createSimulationComputation } from '../repositories/simulation-computations.repository.ts'
 
 interface ProgramSimulationComputationDeps {
   logger: Logger
-  captureException: CaptureException
 }
 
 export function createProgramSimulationComputation(
-  deps: ProgramSimulationComputationDeps
+  _deps: ProgramSimulationComputationDeps
 ) {
   return async function programSimulationComputation(
     simulationId: string
   ): Promise<void> {
-    const { logger, captureException } = deps
     const simulation = await getSimulationById(simulationId)
 
     if (simulation.progression !== 1) {
@@ -26,19 +20,6 @@ export function createProgramSimulationComputation(
         simulationId: simulation.id,
         progression: simulation.progression,
       })
-    }
-
-    if (!isModelSupported(simulation.model)) {
-      const exception = new UnsupportedModelException({
-        message: 'Unsupported model',
-        model: simulation.model,
-      })
-      logger.error(
-        `[program-simulation-computation] ${exception.message}`,
-        exception.payload
-      )
-      captureException(exception)
-      return
     }
 
     await createSimulationComputation(simulationId)
