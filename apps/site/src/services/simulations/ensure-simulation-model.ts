8479e20bb4d56db1c28f8098148f5f19dccf1a6b
Andréa Maugars <andrea.maugars@gmail.com>
2026-09-08 19:09:53 +0300

    simplify ensureSimulationModel type constraint

--- apps/site/src/services/simulations/ensure-simulation-model.ts
+++ apps/site/src/services/simulations/ensure-simulation-model.ts
@@ -15,7 +15,7 @@ import { resolveNewSimulationModelString } from './resolve-new-simulation-model'
  * what makes repairing here reliable.
  */
 export async function ensureSimulationModel<
-  Payload extends Pick<Simulation, 'id'> & Partial<Pick<Simulation, 'model'>>,
+  Payload extends { id: Simulation['id']; model?: Simulation['model'] },
 >(simulation: Payload): Promise<Payload> {
   if (simulation.model && parseModelString(simulation.model)) {
     return simulation
